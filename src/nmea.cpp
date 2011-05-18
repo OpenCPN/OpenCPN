a2519fb583022fa7e3bdad6d8eea6e050d38f397
David Register <bdbcat@users.sourceforge.net>
2011-05-17 20:45:03 -0400

    debug

--- src/nmea.cpp
+++ src/nmea.cpp
@@ -1064,7 +1064,7 @@ void NMEAHandler::OnTimerNMEA(wxTimerEvent& event)
             if(m_pShareMutex)
                   wxMutexLocker stateLocker(*m_pShareMutex) ;
             float kSog = 5.;//8.5;
-            float kCog = 41.;//gCog;  // 28.0;                // gCog to simulate, see hotkey arrows
+            float kCog = NAN;//41.;//gCog;  // 28.0;                // gCog to simulate, see hotkey arrows
 
             //    Kludge the startup case
             if(ThreadPositionData.kLat < 1.0)
