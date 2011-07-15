da2d531975b5e876a6d58ffe3b0526235ebbf5e9
David Register <bdbcat@users.sourceforge.net>
2011-07-14 22:46:56 -0400

    Implement multi-sentence ECRTE protocol.

--- src/nmea.cpp
+++ src/nmea.cpp
@@ -1342,6 +1342,10 @@ ret_point:
 
             int port_fd = g_pCommMan->OpenComPort ( com_name, 4800 );
 
+            int progress_stall = 500;
+            if(pr->pRoutePointList->GetCount() > 10)
+                  progress_stall = 500;
+
             //    Send out the waypoints, in order
             if ( bsend_waypoints )
             {
@@ -1369,6 +1373,14 @@ ret_point:
 
                         g_pCommMan->WriteComPort ( com_name, snt.Sentence );
 
+                        wxString msg(_T("-->GPS Port:"));
+                        msg += com_name;
+                        msg += _T("  Sentence: ");
+                        msg += snt.Sentence;
+                        msg.Trim();
+                        wxLogMessage(msg);
+
+
                         if ( pProgress )
                         {
                               pProgress->SetValue ( ( ip * 100 ) / nProg );
@@ -1376,7 +1388,7 @@ ret_point:
                               pProgress->Update();
                         }
 
-                        wxMilliSleep ( 1000 );
+                        wxMilliSleep ( progress_stall );
 
                         node = node->GetNext();
 
@@ -1406,11 +1418,147 @@ ret_point:
                   node = node->GetNext();
             }
 
-
             oNMEA0183.Rte.Write ( snt );
 
+            unsigned int max_length = 70;
+
+            if(snt.Sentence.Len() > max_length)
+            {
+                  //    Make a route with one waypoint to get tare load.
+                  NMEA0183    tNMEA0183;
+                  SENTENCE    tsnt;
+                  tNMEA0183.TalkerID = _T ( "EC" );
+
+                  tNMEA0183.Rte.Empty();
+                  tNMEA0183.Rte.TypeOfRoute = CompleteRoute;
+
+                  if ( pr->m_RouteNameString.IsEmpty() )
+                        tNMEA0183.Rte.RouteName = _T ( "1" );
+                  else
+                        tNMEA0183.Rte.RouteName = pr->m_RouteNameString;
+                  tNMEA0183.Rte.AddWaypoint ( _T("123456") );
+                  tNMEA0183.Rte.Write ( tsnt );
+
+                  unsigned int tare_length = tsnt.Sentence.Len();
+
+                  wxArrayString sentence_array;
+
+                  //    Trial balloon: add the waypoints, with length checking
+                  int n_total = 1;
+                  bool bnew_sentence = true;
+                  int sent_len=0;
+
+                  wxRoutePointListNode *node = pr->pRoutePointList->GetFirst();
+                  while ( node )
+                  {
+                        RoutePoint *prp = node->GetData();
+                        if(bnew_sentence)
+                        {
+                              sent_len = tare_length;
+                              sent_len += prp->GetName().Truncate ( 6 ).Len();
+                              bnew_sentence = false;
+                              node = node->GetNext();
+
+                        }
+                        else
+                        {
+                              if(sent_len + prp->GetName().Truncate ( 6 ).Len() > max_length)
+                              {
+                                    n_total ++;
+                                    bnew_sentence = true;
+                              }
+                              else
+                              {
+                                    sent_len += prp->GetName().Truncate ( 6 ).Len();
+                                    node = node->GetNext();
+                              }
+                        }
+                  }
+
+                  //    Now we have the sentence count, so make the real sentences using the same counting logic
+                  int final_total = n_total;
+                  int n_run = 1;
+                  bnew_sentence = true;
+
+                  node = pr->pRoutePointList->GetFirst();
+                  while ( node )
+                  {
+                        RoutePoint *prp = node->GetData();
+                        if(bnew_sentence)
+                        {
+                              sent_len = tare_length;
+                              sent_len += prp->GetName().Truncate ( 6 ).Len();
+                              bnew_sentence = false;
+
+                              oNMEA0183.Rte.Empty();
+                              oNMEA0183.Rte.TypeOfRoute = CompleteRoute;
+
+                              if ( pr->m_RouteNameString.IsEmpty() )
+                                    oNMEA0183.Rte.RouteName = _T ( "1" );
+                              else
+                                    oNMEA0183.Rte.RouteName = pr->m_RouteNameString;
+
+                              oNMEA0183.Rte.total_number_of_messages     = final_total;
+                              oNMEA0183.Rte.message_number               = n_run;
+                              snt.Sentence.Clear();
+
+                              oNMEA0183.Rte.AddWaypoint ( prp->GetName().Truncate ( 6 ) );
+                              node = node->GetNext();
+                        }
+                        else
+                        {
+                              if(sent_len + prp->GetName().Truncate ( 6 ).Len() > max_length)
+                              {
+                                    n_run ++;
+                                    bnew_sentence = true;
+
+                                    oNMEA0183.Rte.Write ( snt );
+//       printf("%s", snt.Sentence.mb_str());
+
+                                    sentence_array.Add(snt.Sentence);
+
+                              }
+                              else
+                              {
+                                    sent_len += prp->GetName().Truncate ( 6 ).Len();
+                                    oNMEA0183.Rte.AddWaypoint ( prp->GetName().Truncate ( 6 ) );
+                                    node = node->GetNext();
+                              }
+                        }
+                  }
+
+                  oNMEA0183.Rte.Write ( snt );              //last one...
+                  if(snt.Sentence.Len() > tare_length)
+                        sentence_array.Add(snt.Sentence);
+
+                  for(unsigned int ii=0 ; ii < sentence_array.GetCount(); ii++)
+                  {
+//                        printf("%s", sentence_array.Item(ii).mb_str());
+                        g_pCommMan->WriteComPort ( com_name, sentence_array.Item(ii) );
+
+                        wxString msg(_T("-->GPS Port:"));
+                        msg += com_name;
+                        msg += _T("  Sentence: ");
+                        msg += sentence_array.Item(ii);
+                        msg.Trim();
+                        wxLogMessage(msg);
+
+                        wxMilliSleep ( 500 );
+                  }
+
+            }
+            else
+            {
       //      printf("%s", snt.Sentence.mb_str());
-            g_pCommMan->WriteComPort ( com_name, snt.Sentence );
+                  g_pCommMan->WriteComPort ( com_name, snt.Sentence );
+
+                  wxString msg(_T("-->GPS Port:"));
+                  msg += com_name;
+                  msg += _T("  Sentence: ");
+                  msg += snt.Sentence;
+                  msg.Trim();
+                  wxLogMessage(msg);
+            }
 
 
             if ( pProgress )
