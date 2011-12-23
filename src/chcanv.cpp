3510fc28487265b5dc54ea7d3a285bc73636c4b0
David Register <bdbcat@users.sourceforge.net>
2011-12-23 10:20:56 -0500

    Use null name of WP drop with CTRL-M

--- src/chcanv.cpp
+++ src/chcanv.cpp
@@ -4141,7 +4141,8 @@ void ChartCanvas::OnKeyDown(wxKeyEvent &event)
 
                case 13:                     // Ctrl M                      //    Drop Marker;
                {
-                    RoutePoint *pWP = new RoutePoint ( m_cursor_lat, m_cursor_lon, wxString ( _T ( "triangle" ) ), wxString ( _( "New Mark" ) ), GPX_EMPTY_STRING );
+                    RoutePoint *pWP = new RoutePoint ( m_cursor_lat, m_cursor_lon,
+                                wxString ( _T ( "triangle" ) ), wxString ( _( "" ) ), GPX_EMPTY_STRING );
                     pWP->m_bIsolatedMark = true;                      // This is an isolated mark
                     pSelect->AddSelectableRoutePoint ( m_cursor_lat, m_cursor_lon, pWP );
                     pConfig->AddNewWayPoint ( pWP, -1 );    // use auto next num
