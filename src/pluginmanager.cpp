975b7a33887c20a74e3d2e5a5b57ba78bbe352e1
Pavel Kalian <pavel@kalian.cz>
2012-04-13 13:12:24 +0200

    Make plugins consistent with the core

--- src/pluginmanager.cpp
+++ src/pluginmanager.cpp
@@ -1368,16 +1368,7 @@ void SendPluginMessage( wxString message_id, wxString message_body )
 
 void DimeWindow(wxWindow *win)
 {
-      wxColour col,col1,gridline,uitext,udkrd,back_color,text_color;
-      col = GetGlobalColor(_T("DILG0"));       // Dialog Background white
-      col1 = GetGlobalColor(_T("DILG1"));      // Dialog Background
-      back_color = GetGlobalColor(_T("DILG2"));// Control Background
-      text_color = GetGlobalColor(_T("DILG3"));// Text
-      uitext = GetGlobalColor(_T("UITX1"));    // Menu Text, derived from UINFF
-      udkrd = GetGlobalColor(_T("UDKRD"));
-      gridline = GetGlobalColor(_T("GREY2"));
-
-      DimeControl(win, col, col1, back_color, text_color, uitext, udkrd);
+      DimeControl(win);
 }
 
 //-----------------------------------------------------------------------------------------
