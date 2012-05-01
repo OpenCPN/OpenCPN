be54c0cc6c23e0fc0d2a75693a00eba8f0c5b852
David Register <bdbcat@users.sourceforge.net>
2012-04-30 21:13:56 -0400

    Correct CMakeLists for Mac 64 bit system builds

--- build_mac_64.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#
-if [[ ! -d build_osx ]];then
-	mkdir build_osx
-fi
-cd build_osx
-CC=`wx-config --cc` CXX=`wx-config --cxx` cmake .. && make
-#cmake -DCMAKE_OSX_ARCHITECTURES=i386 .. && make
