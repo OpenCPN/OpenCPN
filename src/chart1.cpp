c133e98a4f28e76425c8103d062d74ec9f69c190
David Register <bdbcat@users.sourceforge.net>
2012-06-25 22:03:53 -0400

    Correct Toolbar tooltip behavior if application is not active

--- src/chart1.cpp
+++ src/chart1.cpp
@@ -10627,6 +10627,8 @@ void ocpnToolBarSimple::OnKillFocus(wxFocusEvent& WXUNUSED(event))
 
 void ocpnToolBarSimple::OnToolTipTimerEvent(wxTimerEvent& event)
 {
+	if(!gFrame->IsActive()) return;
+
       if( m_btooltip_show && IsShown() && m_pToolTipWin && (!m_pToolTipWin->IsShown()))
       {
             if(m_last_ro_tool)
