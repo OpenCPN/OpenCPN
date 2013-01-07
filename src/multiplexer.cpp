301e27ab6667166273a278fe92217fed0f5cad0a
David Register <bdbcat@users.sourceforge.net>
2013-01-06 20:22:51 -0500

    Remove AIVDO processing from AIS consumer, move to core GPS consumer.

--- src/multiplexer.cpp
+++ src/multiplexer.cpp
@@ -183,7 +183,6 @@ void Multiplexer::OnEvtStream(OCPN_DataStreamEvent& event)
             
         if( bpass ) {
             if( message.Mid(3,3).IsSameAs(_T("VDM")) ||
-                message.Mid(3,3).IsSameAs(_T("VDO")) ||
                 message.Mid(1,5).IsSameAs(_T("FRPOS")) ||
                 message.Mid(1,2).IsSameAs(_T("CD")) )
             {
