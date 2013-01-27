10708ae0c128e1705ffb9b2aa17b861cc0a06e4b
Pavel Kalian <pavel@kalian.cz>
2013-01-27 14:47:02 -0600

    Replace tabs with spaces

--- src/undo.cpp
+++ src/undo.cpp
@@ -403,7 +403,7 @@ UndoAction::~UndoAction()
                         break;
                     case Undo_DeleteWaypoint: break;
                     case Undo_CreateWaypoint: break;
-		    case Undo_AppendWaypoint: break;
+                    case Undo_AppendWaypoint: break;
                 }
                 break;
             }
