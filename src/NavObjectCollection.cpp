42aafe5c02c10f0fcda0bcf78726ef11dc3a8b22
David Register <bdbcat@users.sourceforge.net>
2013-09-15 20:29:39 -0400

    Correct potential fault on UTF8 encoding of XML hyperlinks attributes.

--- src/NavObjectCollection.cpp
+++ src/NavObjectCollection.cpp
@@ -583,18 +583,21 @@ bool GPXCreateWpt( pugi::xml_node node, RoutePoint *pr, unsigned int flags )
             while( linknode ) {
                 Hyperlink *link = linknode->GetData();
                 
-                pugi::xml_node child_link = node.append_child("link");
-                child_link.append_attribute("href") = link->Link.mb_str();
-                
-                wxCharBuffer buffer=link->DescrText.ToUTF8();
+                pugi::xml_node child_link = node.append_child("link");;
+                wxCharBuffer buffer=link->Link.ToUTF8();
+                if(buffer.data())
+                    child_link.append_attribute("href") = buffer.data();
+                                
+                buffer=link->DescrText.ToUTF8();
                 if(buffer.data()) {
                     child = child_link.append_child("text");
                     child.append_child(pugi::node_pcdata).set_value(buffer.data());
                 }
                 
-                if( link->LType.Len() ) {
+                buffer=link->LType.ToUTF8();
+                if(buffer.data()) {
                     child = child_link.append_child("type");
-                    child.append_child(pugi::node_pcdata).set_value(link->LType.mb_str());
+                    child.append_child(pugi::node_pcdata).set_value(buffer.data());
                 }
                 
                 linknode = linknode->GetNext();
@@ -679,17 +682,20 @@ bool GPXCreateTrk( pugi::xml_node node, Route *pRoute )
             Hyperlink *link = linknode->GetData();
         
             pugi::xml_node child_link = node.append_child("link");
-            child_link.append_attribute("href") = link->Link.mb_str();
-        
-            wxCharBuffer buffer=link->DescrText.ToUTF8();
+            wxCharBuffer buffer = link->Link.ToUTF8();
+            if(buffer.data())
+                child_link.append_attribute("href") = buffer.data();
+                        
+            buffer=link->DescrText.ToUTF8();
             if(buffer.data()) {
                 child = child_link.append_child("text");
                 child.append_child(pugi::node_pcdata).set_value(buffer.data());
             }
         
-            if( link->LType.Len() ) {
+            buffer=link->LType.ToUTF8();
+            if(buffer.data()) {
                 child = child_link.append_child("type");
-                child.append_child(pugi::node_pcdata).set_value(link->LType.mb_str());
+                child.append_child(pugi::node_pcdata).set_value(buffer.data());
             }
         
             linknode = linknode->GetNext();
@@ -793,19 +799,23 @@ bool GPXCreateRoute( pugi::xml_node node, Route *pRoute )
             Hyperlink *link = linknode->GetData();
         
             pugi::xml_node child_link = node.append_child("link");
-            child_link.append_attribute("href") = link->Link.mb_str();
+            wxCharBuffer buffer = link->Link.ToUTF8();
+            if(buffer.data())
+                child_link.append_attribute("href") = buffer.data();
+            
         
-            wxCharBuffer buffer=link->DescrText.ToUTF8();
+            buffer=link->DescrText.ToUTF8();
             if(buffer.data()) {
                 child = child_link.append_child("text");
                 child.append_child(pugi::node_pcdata).set_value(buffer.data());
             }
-        
-            if( link->LType.Len() ) {
+
+            buffer=link->LType.ToUTF8();
+            if(buffer.data()) {
                 child = child_link.append_child("type");
-                child.append_child(pugi::node_pcdata).set_value(link->LType.mb_str());
+                child.append_child(pugi::node_pcdata).set_value(buffer.data());
             }
-        
+            
             linknode = linknode->GetNext();
         }
     }
