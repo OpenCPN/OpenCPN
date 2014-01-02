144ad209d0c1d02c2a611b310b8c7412415a4dc6
David Register <bdbcat@users.sourceforge.net>
2014-01-02 12:15:39 -0500

    Correct roundint() function definition for Mac OSX

--- include/cutil.h
+++ include/cutil.h
@@ -62,11 +62,20 @@ typedef struct {
      extern  long  __stdcall MyUnhandledExceptionFilter( struct _EXCEPTION_POINTERS *ExceptionInfo );
 #endif
 #endif
+     
 
+     //      Replacement for round(x)???
+#ifdef __cplusplus
+     extern "C"  double     round_msvc (double flt);
+#else
+     extern double round_msvc (double flt);
+#endif /* __cplusplus */
+     
+     
 inline int roundint (double x)
 {
 #ifdef __WXOSX__
-    return (int)wxRound(x);     //FS#1278
+    return (int)round(x);     //FS#1278
 #else
     int tmp = static_cast<int> (x);
     tmp += (x-tmp>=.5) - (x-tmp<=-.5);
@@ -99,12 +108,6 @@ extern "C"  ClipResult cohen_sutherland_line_clip_i (int *x0, int *y0, int *x1, 
                                              int xmin_, int xmax_, int ymin_, int ymax_);
 
 #endif
-
-
-//      Replacement for round(x)???
-#ifdef __cplusplus
-extern "C"  double     round_msvc (double flt);
-#endif /* __cplusplus */
 
 
 
