99ed53d88aa3f24825160030ba98ac7f14dadd43
Sean D'Epagnier <sean@depagnier.com>
2014-05-13 11:20:43 +0800

    code formatting (whitespace change)

--- include/glChartCanvas.h
+++ include/glChartCanvas.h
@@ -39,87 +39,87 @@ WX_DECLARE_HASH_MAP( void*, ChartTextureHashType*, wxPointerHash, wxPointerEqual
 class glChartCanvas : public wxGLCanvas
 {
 public:
-      static void RotateToViewPort(const ViewPort &vp);
-      static void SetClipRegion(const ViewPort &vp, const OCPNRegion &region,
-                                bool apply_rotation=true, bool b_clear=false);
-      static void DisableClipRegion();
+    static void RotateToViewPort(const ViewPort &vp);
+    static void SetClipRegion(const ViewPort &vp, const OCPNRegion &region,
+                              bool apply_rotation=true, bool b_clear=false);
+    static void DisableClipRegion();
 
-      static bool         s_b_useScissorTest;
-      static bool         s_b_useStencil;
+    static bool         s_b_useScissorTest;
+    static bool         s_b_useStencil;
 
-      glChartCanvas(wxWindow *parent);
-      ~glChartCanvas();
+    glChartCanvas(wxWindow *parent);
+    ~glChartCanvas();
 
-      void SetContext(wxGLContext *pcontext) { m_pcontext = pcontext; }
+    void SetContext(wxGLContext *pcontext) { m_pcontext = pcontext; }
 
-      void OnPaint(wxPaintEvent& event);
-      void OnEraseBG(wxEraseEvent& evt);
-      void render();
-      void OnActivate ( wxActivateEvent& event );
-      void OnSize ( wxSizeEvent& event );
-      void MouseEvent(wxMouseEvent& event);
+    void OnPaint(wxPaintEvent& event);
+    void OnEraseBG(wxEraseEvent& evt);
+    void render();
+    void OnActivate ( wxActivateEvent& event );
+    void OnSize ( wxSizeEvent& event );
+    void MouseEvent(wxMouseEvent& event);
 
-      wxString GetRendererString(){ return m_renderer; }
-      void EnablePaint(bool b_enable){ m_b_paint_enable = b_enable; }
+    wxString GetRendererString(){ return m_renderer; }
+    void EnablePaint(bool b_enable){ m_b_paint_enable = b_enable; }
       
 
-      void Invalidate() { m_gl_cache_vp.Invalidate(); }
-      void RenderRasterChartRegionGL(ChartBase *chart, ViewPort &vp, OCPNRegion &region);
-      bool PurgeChartTextures(ChartBase *pc);
-      void ClearAllRasterTextures(void);
-      void DrawGLOverLayObjects(void);
+    void Invalidate() { m_gl_cache_vp.Invalidate(); }
+    void RenderRasterChartRegionGL(ChartBase *chart, ViewPort &vp, OCPNRegion &region);
+    bool PurgeChartTextures(ChartBase *pc);
+    void ClearAllRasterTextures(void);
+    void DrawGLOverLayObjects(void);
 
 protected:
-      void RenderQuiltViewGL(ViewPort &vp, OCPNRegion Region, bool b_clear = true);
-      void BuildFBO(void);
-      void ComputeRenderQuiltViewGLRegion( ViewPort &vp, OCPNRegion Region );
+    void RenderQuiltViewGL(ViewPort &vp, OCPNRegion Region, bool b_clear = true);
+    void BuildFBO(void);
+    void ComputeRenderQuiltViewGLRegion( ViewPort &vp, OCPNRegion Region );
 
-      wxGLContext       *m_pcontext;
+    wxGLContext       *m_pcontext;
 
-      int m_cacheinvalid;
-      int max_texture_dimension;
+    int m_cacheinvalid;
+    int max_texture_dimension;
 
-      unsigned char *m_data;
-      int m_datasize;
+    unsigned char *m_data;
+    int m_datasize;
 
-      bool m_bsetup;
+    bool m_bsetup;
 
-      wxString m_renderer;
+    wxString m_renderer;
 
-      void GrowData(int size);
+    void GrowData(int size);
 
-      ArrayOfTexDescriptors         m_tex_array;
+    ArrayOfTexDescriptors         m_tex_array;
 
-      //    This is a hash table
-      //    key is ChartBaseBSB pointer
-      //    Value is ChartTextureHashType*
-      ChartPointerHashType          m_chart_hash;
+    //    This is a hash table
+    //    key is ChartBaseBSB pointer
+    //    Value is ChartTextureHashType*
+    ChartPointerHashType          m_chart_hash;
 
-      ViewPort    m_gl_cache_vp;
+    ViewPort    m_gl_cache_vp;
 
 
-      bool m_bGenMM;
-      bool m_bGL_GEN_MM;
-      int  m_ntex;
-      int  m_tex_max_res;
-      int  m_tex_max_res_initial;
-      bool m_b_mem_crunch;
-      bool m_b_paint_enable;
+    bool m_bGenMM;
+    bool m_bGL_GEN_MM;
+    int  m_ntex;
+    int  m_tex_max_res;
+    int  m_tex_max_res_initial;
+    bool m_b_mem_crunch;
+    bool m_b_paint_enable;
 
-      //    For FBO(s)
-      bool         m_b_useFBO;
-      bool         m_b_useFBOStencil;
-      GLuint       m_fb0;
-      GLuint       m_depth_rb;
+    //    For FBO(s)
+    bool         m_b_useFBO;
+    bool         m_b_useFBOStencil;
+    GLuint       m_fb0;
+    GLuint       m_depth_rb;
 
-      GLenum       m_TEX_TYPE;
-      GLuint       m_cache_tex;
-      GLuint       m_blit_tex;
-      int          m_cache_tex_x;
-      int          m_cache_tex_y;
-      OCPNRegion     m_gl_rendered_region;
+    GLenum       m_TEX_TYPE;
+    GLuint       m_cache_tex;
+    GLuint       m_blit_tex;
+    int          m_cache_tex_x;
+    int          m_cache_tex_y;
+    OCPNRegion     m_gl_rendered_region;
 
-DECLARE_EVENT_TABLE()
+    DECLARE_EVENT_TABLE()
 };
 
 #endif
