ef2bcf319681369aac5987d47ee6f586de7038f5
David Register <bdbcat@users.sourceforge.net>
2014-06-24 12:39:53 -0400

    Enable higher quality compression when building cacheable raster textures

--- src/glTexCache.cpp
+++ src/glTexCache.cpp
@@ -316,7 +316,7 @@ void glTexFactory::PrepareTexture( int base_level, const wxRect &rect, ColorSche
         glTextureDescriptor *p = new glTextureDescriptor;
         p->x = rect.x;
         p->y = rect.y;
-        p->level_min = 100;  // default, crazy high
+        p->level_min = g_mipmap_max_level + 1;  // default, nothing loaded
         m_td_array[array_index] = p;
         
     }
@@ -629,9 +629,17 @@ unsigned char *glTexFactory::GetTextureLevel( const wxRect &rect, int level, Col
                     }
                 }
                 FlattenColorsForCompression(ptd->map_array[level], dim, first);
-                
+ 
                 // color range fit is worse quality but twice as fast
                 int flags = squish::kDxt1 | squish::kColourRangeFit;
+                
+                if( g_GLOptions.m_bTextureCompressionCaching) {
+                /* use slower cluster fit since we are building the cache for
+                 * better quality, this takes roughly 25% longer and uses about
+                 * 10% more disk space (result doesn't compress as well with lz4) */
+                    flags = squish::kDxt1 | squish::kColourClusterFit;
+                }
+                
                 squish::CompressImageRGB(ptd->map_array[level], dim, dim, tex_data, flags);
             } else if(m_raster_format == GL_ETC1_RGB8_OES) 
                 CompressDataETC(ptd->map_array[level], dim, size, tex_data);
