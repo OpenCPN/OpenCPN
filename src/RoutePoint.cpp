6a082ff275382e9e6f312be6f39d1c3876591652
David Register <bdbcat@users.sourceforge.net>
2014-07-27 12:02:27 -0400

    Improve Routepoint highlite on OpenGL

--- src/RoutePoint.cpp
+++ src/RoutePoint.cpp
@@ -305,7 +305,15 @@ void RoutePoint::Draw( ocpnDC& dc, wxPoint *rpn )
     hilitebox = r1;
     hilitebox.x -= r.x;
     hilitebox.y -= r.y;
-    hilitebox.Inflate( 2 );
+    float radius;
+    if( g_btouch ){
+        hilitebox.Inflate( 20 );
+        radius = 20.0f;
+    }
+    else{
+        hilitebox.Inflate( 4 );
+        radius = 4.0f;
+    }
 
     wxColour hi_colour = pen->GetColour();
     unsigned char transparency = 100;
@@ -317,7 +325,7 @@ void RoutePoint::Draw( ocpnDC& dc, wxPoint *rpn )
         
     //  Highlite any selected point
     if( m_bPtIsSelected || m_bIsBeingEdited) {
-        AlphaBlending( dc, r.x + hilitebox.x, r.y + hilitebox.y, hilitebox.width, hilitebox.height, 0.0,
+        AlphaBlending( dc, r.x + hilitebox.x, r.y + hilitebox.y, hilitebox.width, hilitebox.height, radius,
                 hi_colour, transparency );
     }
 
@@ -424,11 +432,16 @@ void RoutePoint::DrawGL( ViewPort &vp, OCPNRegion &region )
     hilitebox = r3;
     hilitebox.x -= r.x;
     hilitebox.y -= r.y;
-    if( g_btouch )
+    float radius;
+    if( g_btouch ){
         hilitebox.Inflate( 20 );
-    else
+        radius = 20.0f;
+    }
+    else{
         hilitebox.Inflate( 4 );
-
+        radius = 4.0f;
+    }
+    
     /* update bounding box */
     if(!m_wpBBox.GetValid() || vp.chart_scale != m_wpBBox_chart_scale || vp.rotation != m_wpBBox_rotation) {
         double lat1, lon1, lat2, lon2;
@@ -457,7 +470,7 @@ void RoutePoint::DrawGL( ViewPort &vp, OCPNRegion &region )
             hi_colour = GetGlobalColor( _T ( "YELO1" ) );
         }
         
-        AlphaBlending( dc, r.x + hilitebox.x, r.y + hilitebox.y, hilitebox.width, hilitebox.height, 0.0,
+        AlphaBlending( dc, r.x + hilitebox.x, r.y + hilitebox.y, hilitebox.width, hilitebox.height, radius,
                        hi_colour, transparency );
     }
     
