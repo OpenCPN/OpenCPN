1d155d3d3767f719544f6e823ed8b752ea894672
David Register <bdbcat@users.sourceforge.net>
2014-09-15 10:58:11 -0400

    Avoid possible NULL deref on image data.

--- src/TexFont.cpp
+++ src/TexFont.cpp
@@ -122,27 +122,30 @@ void TexFont::Build( wxFont &font, bool blur )
         image = image.Blur(1);
 
     unsigned char *imgdata = image.GetData();
-    unsigned char *teximage = (unsigned char *) malloc( stride * tex_w * tex_h );
+    
+    if(imgdata){
+        unsigned char *teximage = (unsigned char *) malloc( stride * tex_w * tex_h );
 
-    for( int j = 0; j < tex_w*tex_h; j++ )
-        for( int k = 0; k < stride; k++ )
-            teximage[j * stride + k] = imgdata[3*j];
+        for( int j = 0; j < tex_w*tex_h; j++ )
+            for( int k = 0; k < stride; k++ )
+                teximage[j * stride + k] = imgdata[3*j];
 
-    if(texobj)
-        Delete();
+        if(texobj)
+            Delete();
 
-    glGenTextures( 1, &texobj );
-    glBindTexture( GL_TEXTURE_2D, texobj );
+        glGenTextures( 1, &texobj );
+        glBindTexture( GL_TEXTURE_2D, texobj );
 
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
 
-    glTexImage2D( GL_TEXTURE_2D, 0, internalformat, tex_w, tex_h, 0,
-                  format, GL_UNSIGNED_BYTE, teximage );
+        glTexImage2D( GL_TEXTURE_2D, 0, internalformat, tex_w, tex_h, 0,
+                    format, GL_UNSIGNED_BYTE, teximage );
 
-    free(teximage);
+        free(teximage);
+    }
 }
 
 void TexFont::Delete( )
