5854b111b773e313777e5dac47a16f4d4f9f8321
Jon Gough <jon.gough@eclipsesystems.com.au>
2014-11-05 07:10:53 +1100

    Modified definition of g_b_legacy_input_filter_behaviour to have a comment to allow commiting the change and creating a diff file without whitespace changes

--- src/chart1.cpp
+++ src/chart1.cpp
@@ -424,7 +424,7 @@ bool                      g_bsmoothpanzoom;
 
 int                       g_nCOMPortCheck;
 
-bool                      g_b_legacy_input_filter_behaviour;
+bool                      g_b_legacy_input_filter_behaviour;  // Support original input filter process or new process
 
 bool                      g_bbigred;
 
