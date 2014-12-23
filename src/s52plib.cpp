fb94bf0f394aefd1d9726295e4d2a090e1b613f9
David Register <bdbcat@users.sourceforge.net>
2014-12-23 12:55:50 -0500

    Correct OpenGL symbol rendering for nPOT textures with FBO support.

--- src/s52plib.cpp
+++ src/s52plib.cpp
@@ -2581,10 +2581,19 @@ bool s52plib::RenderRasterSymbol( ObjRazRules *rzRules, Rule *prule, wxPoint &r,
             //  Since draw pixels is so slow, lets not draw anything we don't have to
             wxRect sym_rect(r.x - ddx, r.y - ddy, b_width, b_height);
             if(vp->rv_rect.Intersects(sym_rect) ) {
+                
+                glPushAttrib( GL_SCISSOR_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+                
+                glDisable( GL_SCISSOR_TEST );
+                glDisable( GL_STENCIL_TEST );
+                glDisable( GL_DEPTH_TEST );
+                
                 glRasterPos2f( r.x - ddx, r.y - ddy );
                 glPixelZoom( 1, -1 );
                 glDrawPixels( b_width, b_height, GL_RGBA, GL_UNSIGNED_BYTE, prule->pixelPtr );
                 glPixelZoom( 1, 1 );
+
+                glPopAttrib();
             }
         }
 
