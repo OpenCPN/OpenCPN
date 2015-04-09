5cc4100e37f25ce727dfefcb781dbcacfad196a3
David Register <bdbcat@users.sourceforge.net>
2015-04-08 20:07:10 -0400

    Improve options dialog screen size management.

--- src/options.cpp
+++ src/options.cpp
@@ -1950,7 +1950,8 @@ void options::CreatePanel_Advanced( size_t parent, int border_size, int group_it
     pRBSizeManual = new wxRadioButton( m_ChartDisplayPage, ID_SIZEMANUALRADIOBUTTON, _("Manual:") );
     pDPIRow->Add( pRBSizeManual, inputFlags );
 
-    pScreenMM = new wxTextCtrl( m_ChartDisplayPage, ID_TEXTCTRL, _T(""), wxDefaultPosition, wxSize( 50, -1 ), wxTE_RIGHT  );
+    pScreenMM = new wxTextCtrl( m_ChartDisplayPage, ID_TEXTCTRL, _T(""), wxDefaultPosition,
+                                wxSize( 3 * m_fontHeight, -1 ), wxTE_RIGHT  );
     pDPIRow->Add( pScreenMM, 0, wxALIGN_RIGHT | wxALL, group_item_spacing );
 
     pDPIRow->Add( new wxStaticText( m_ChartDisplayPage, wxID_ANY, _("mm") ), inputFlags );
@@ -3256,8 +3257,8 @@ void options::SetInitialSettings()
         pRBSizeManual->SetValue( true );
     }
     else{
-        screenmm = _("Auto");
         pRBSizeAuto->SetValue( true );
+        screenmm.Printf(_T("%d"), int(g_Platform->GetDisplaySizeMM()));
         pScreenMM->Disable();
     }
     
@@ -3388,6 +3389,9 @@ void options::UpdateOptionsUnits()
 void options::OnSizeAutoButton( wxCommandEvent& event )
 {
     pScreenMM->SetValue(_("Auto"));
+    wxString screenmm;
+    screenmm.Printf(_T("%d"), int(g_Platform->GetDisplaySizeMM()));
+    pScreenMM->SetValue(screenmm);
     pScreenMM->Disable();
 }
 
@@ -3396,6 +3400,9 @@ void options::OnSizeManualButton( wxCommandEvent& event )
     wxString screenmm;
     if(g_config_display_size_mm > 0){
         screenmm.Printf(_T("%d"), int(g_config_display_size_mm));
+    }
+    else {
+        screenmm.Printf(_T("%d"), int(g_Platform->GetDisplaySizeMM()));
     }
     
     pScreenMM->SetValue(screenmm);
