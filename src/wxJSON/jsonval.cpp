6685ec0535e333439af985ea288ba6a2ad049d9b
Pavel Kalian <pavel@kalian.cz>
2015-04-18 09:15:39 -0700

    Fix build errors

--- src/wxJSON/jsonval.cpp
+++ src/wxJSON/jsonval.cpp
@@ -962,7 +962,7 @@ wxJSONValue::AsString() const
             break;
         case wxJSONTYPE_INT :
             #if defined( wxJSON_64BIT_INT )
-                  s.Printf( _T("%") wxLongLongFmtSpec _T("i"),
+                  s.Printf( _T("%") _T(wxLongLongFmtSpec) _T("i"),
                         data->m_value.m_valInt64 );
             #else
             s.Printf( _T("%ld"), data->m_value.m_valLong );
@@ -970,7 +970,7 @@ wxJSONValue::AsString() const
             break;
         case wxJSONTYPE_UINT :
             #if defined( wxJSON_64BIT_INT )
-            s.Printf( _T("%") wxLongLongFmtSpec _T("u"),
+            s.Printf( _T("%") _T(wxLongLongFmtSpec) _T("u"),
                         data->m_value.m_valUInt64 );
             #else
             s.Printf( _T("%lu"), data->m_value.m_valULong );
