f71ec389a443d1e1fae3cfbccb907c07d36afce7
did-g <did-g@users.noreply.github.com>
2015-04-28 18:54:11 +0200

    enc: always display safety depth contour

--- src/s57chart.cpp
+++ src/s57chart.cpp
@@ -2305,6 +2305,7 @@ bool s57chart::DoRenderRegionViewOnDC( wxMemoryDC& dc, const ViewPort& VPoint,
         ClearRenderedTextCache();                       // and reset the text renderer,
                                                         //for the case where depth(height) units change
         ResetPointBBoxes( m_last_vp, VPoint );
+        SetSafetyContour();
     }
 
     if( VPoint.view_scale_ppm != m_last_vp.view_scale_ppm ) {
