d64dcd7e4e57b66630e2c5f535e88b1284b67a4e
David Register <bdbcat@users.sourceforge.net>
2015-05-06 22:18:06 -0400

    Correct inadvertent change on previous reversion.

--- src/chart1.cpp
+++ src/chart1.cpp
@@ -5179,6 +5179,7 @@ void MyFrame::ToggleQuiltMode( void )
         if( cur_mode != cc1->GetQuiltMode() ){
             SetupQuiltMode();
             DoChartUpdate();
+            cc1->InvalidateGL();
             Refresh();
         }
     }
