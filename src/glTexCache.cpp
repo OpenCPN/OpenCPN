11b80387b11d21c481070b9aa96a2b3291e79107
didier <dgautheron@magic.fr>
2015-06-22 14:52:59 +0200

    flush to disk buffered cache file when writing header

--- src/glTexCache.cpp
+++ src/glTexCache.cpp
@@ -1950,6 +1950,7 @@ bool glTexFactory::WriteCatalogAndHeader()
         hdr.chartdate = m_chart_date_binary;
         
         m_fs->Write( &hdr, sizeof(hdr));
+        m_fs->Flush();
         
         return true;
     }
