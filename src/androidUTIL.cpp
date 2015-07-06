628cb658c51fdc36c592e01cf01d15925cbd0cb9
David Register <bdbcat@users.sourceforge.net>
2015-07-05 22:27:44 -0400

    Update comments

--- src/androidUTIL.cpp
+++ src/androidUTIL.cpp
@@ -1040,6 +1040,28 @@ wxString androidGetHomeDir()
 wxString androidGetPrivateDir()                 // Used for logfile, config file, navobj, and the like
 {
     if(g_bExternalApp){
+        
+        // should check storage availability
+#if 0
+/* Checks if external storage is available for read and write */
+        public boolean isExternalStorageWritable() {
+            String state = Environment.getExternalStorageState();
+            if (Environment.MEDIA_MOUNTED.equals(state)) {
+                return true;
+            }
+            return false;
+        }
+        
+        /* Checks if external storage is available to at least read */
+        public boolean isExternalStorageReadable() {
+            String state = Environment.getExternalStorageState();
+            if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+                return true;
+                }
+                return false;
+        }
+#endif        
         if(g_androidExtFilesDir.Length())
             return g_androidExtFilesDir;
     }
