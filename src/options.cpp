748c05d0ce2cb944e2a9577b166ed83db9496760
David Register <bdbcat@users.sourceforge.net>
2015-09-15 22:23:23 -0400

    Correct initial setting of OpenGL options "Accelerated Panning" checkbox. (FS#1850)

--- src/options.cpp
+++ src/options.cpp
@@ -7236,6 +7236,11 @@ void OpenGLOptionsDlg::Populate( void )
             m_cbUseAcceleratedPanning->Disable();
         }
     }
+    else{
+        m_cbUseAcceleratedPanning->SetValue( g_GLOptions.m_bUseAcceleratedPanning );
+        m_cbUseAcceleratedPanning->Disable();
+    }
+        
 }
 
 void OpenGLOptionsDlg::OnButtonRebuild( wxCommandEvent& event )
