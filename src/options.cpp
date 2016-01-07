c11e7b71f84cfb96be577aebec9857a711782d25
Jon Gough <jon.gough@eclipsesystems.com.au>
2016-01-07 11:36:55 +1100

    Update to fix to totally remove SendIdleEvents for wxWidgets 2.8

--- src/options.cpp
+++ src/options.cpp
@@ -918,13 +918,13 @@ options::~options(void) {
 }
 
 // with AIS it's called very often
-bool options::SendIdleEvents(wxIdleEvent &event )  { 
 #if wxCHECK_VERSION(3,0,0)
+bool options::SendIdleEvents(wxIdleEvent &event )  { 
     if (IsShown())
        return wxDialog::SendIdleEvents(event);
-#endif    
    return false;
 }
+#endif    
 
 void options::RecalculateSize(void) {
   if (!g_bresponsive) {
