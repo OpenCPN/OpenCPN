d3f06a8c91cd1b6b3109c62a2b1ca5b7d791aa32
Jon Gough <jon.gough@eclipsesystems.com.au>
2016-01-13 09:29:36 +1100

    Allow scaling of icons irrespective of tablet or not. Removed the use of bResponsive as it is irrelevant here

--- src/OCPNPlatform.cpp
+++ src/OCPNPlatform.cpp
@@ -1462,66 +1462,57 @@ double OCPNPlatform::GetToolbarScaleFactor( int GUIScaleFactor )
     // We try to arrange matters so that at GUIScaleFactor=0, the tool icons are approximately 9 mm in size
     // and that the value may range from 0.5 -> 2.0
     
-    if(g_bresponsive ){
-        //  Get the basic size of a tool icon
-        ocpnStyle::Style* style = g_StyleManager->GetCurrentStyle();
-        wxSize style_tool_size = style->GetToolSize();
-        double tool_size = style_tool_size.x;
-        
-        // unless overridden by user, we declare the "best" tool size
-        // to be roughly the same as the ActionBar height.
-        //  This may be approximated in a device orientation-independent way as:
-        //   40pixels * DENSITY
-        double premult = 1.0;
-        if( g_config_display_size_manual && (g_config_display_size_mm > 0) ){
-            double target_size = 9.0;                // mm
-        
-            double basic_tool_size_mm = tool_size / GetDisplayDPmm();
-            premult = target_size / basic_tool_size_mm;
-            
-        }
-        else{
-            premult = wxMax(40 * getAndroidDisplayDensity(), 50) / tool_size;
-        }            
-        
-        //Adjust the scale factor using the global GUI scale parameter
-        double postmult =  exp( GUIScaleFactor * (0.693 / 5.0) );       //  exp(2)
-        
-//        qDebug() << "parmsF" << GUIScaleFactor << premult << postmult;
-        
-        rv = premult * postmult;
-        rv = wxMin(rv, 3.0);      //  Clamp at 3.0
-    }
-        
-        
+    //  Get the basic size of a tool icon
+    ocpnStyle::Style* style = g_StyleManager->GetCurrentStyle();
+    wxSize style_tool_size = style->GetToolSize();
+    double tool_size = style_tool_size.x;
     
-#else
-    if(g_bresponsive ){
-        
-        double premult = 1.0;
-        
-        //  Get the basic size of a tool icon
-        ocpnStyle::Style* style = g_StyleManager->GetCurrentStyle();
-        wxSize style_tool_size = style->GetToolSize();
-        double tool_size = style_tool_size.x;
-
-        // unless overridden by user, we declare the "best" tool size
-        // to be roughly 6 mm square.
-        double target_size = 6.0;                // mm
-
+    // unless overridden by user, we declare the "best" tool size
+    // to be roughly the same as the ActionBar height.
+    //  This may be approximated in a device orientation-independent way as:
+    //   40pixels * DENSITY
+    double premult = 1.0;
+    if( g_config_display_size_manual && (g_config_display_size_mm > 0) ){
+        double target_size = 9.0;                // mm
+    
         double basic_tool_size_mm = tool_size / GetDisplayDPmm();
         premult = target_size / basic_tool_size_mm;
-
-        //Adjust the scale factor using the global GUI scale parameter
-        double postmult =  exp( GUIScaleFactor * (0.693 / 5.0) );       //  exp(2)
-        
-        
-        rv = premult * postmult;
-        rv = wxMin(rv, 3.0);      //  Clamp at 3.0
-        rv = wxMax(rv, 1.0);
         
     }
+    else{
+        premult = wxMax(40 * getAndroidDisplayDensity(), 50) / tool_size;
+    }            
     
+    //Adjust the scale factor using the global GUI scale parameter
+    double postmult =  exp( GUIScaleFactor * (0.693 / 5.0) );       //  exp(2)
+    
+//        qDebug() << "parmsF" << GUIScaleFactor << premult << postmult;
+    
+    rv = premult * postmult;
+    rv = wxMin(rv, 3.0);      //  Clamp at 3.0
+    
+#else
+    double premult = 1.0;
+    
+    //  Get the basic size of a tool icon
+    ocpnStyle::Style* style = g_StyleManager->GetCurrentStyle();
+    wxSize style_tool_size = style->GetToolSize();
+    double tool_size = style_tool_size.x;
+
+    // unless overridden by user, we declare the "best" tool size
+    // to be roughly 6 mm square.
+    double target_size = 6.0;                // mm
+
+    double basic_tool_size_mm = tool_size / GetDisplayDPmm();
+    premult = target_size / basic_tool_size_mm;
+
+    //Adjust the scale factor using the global GUI scale parameter
+    double postmult =  exp( GUIScaleFactor * (0.693 / 5.0) );       //  exp(2)
+    
+    
+    rv = premult * postmult;
+    rv = wxMin(rv, 3.0);      //  Clamp at 3.0
+    rv = wxMax(rv, 1.0);
 
 #endif
 
