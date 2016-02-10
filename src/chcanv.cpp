39c0b7c3367181ed92adfe0e41d59b7fb65d3a9a
David Register <bdbcat@users.sourceforge.net>
2016-02-09 22:39:08 -0500

    Correct fault in DC mode during repaint of empty route while being created.

--- src/chcanv.cpp
+++ src/chcanv.cpp
@@ -10172,6 +10172,11 @@ void ChartCanvas::DrawAllRoutesInBBox( ocpnDC& dc, LLBBox& BltBBox, const wxRegi
                 continue;
             }
 
+            if ( 0 == pRouteDraw->GetnPoints() ) {
+                node = node->GetNext();
+                continue;
+            }
+            
             LLBBox test_box = pRouteDraw->GetBBox();
 
             if( b_run ) test_box.Expand( gLon, gLat );
