3aad41ff8ae2f4304899cd15a96aad96654e5e4a
transmitterdan <dldickey@ieee.org>
2016-07-13 21:30:13 -0500

    Added filterobj class to GIT

---
+++ include/filterobj.h
@@ -0,0 +1,98 @@
+/******************************************************************************
+* filterobj.h
+*
+* Project:  Many
+* Purpose:  Class that implements single order inifinite-impulse-response filter
+* Author:   Transmitterdan
+***************************************************************************
+*   Copyright (C) 2016                                                    *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+***************************************************************************
+*/
+
+/**************************************************************************
+ * How to use:                                                            *
+ *                                                                        *
+ * To create a filter object declare on instance of filterobj. There are  *
+ * 2 optional parameters to the constructor. The first (Fc) determines    *
+ * the filter cutoff frequency. A value of 0.5 is basically no filtering  *
+ * and smaller values decrease the cutoff frequency. If you think of the  *
+ * filter as being "fast" or "slow" then 0.5 is fastest and smaller values*
+ * are "slower". The second parameter (tp) selects whether the underlying *
+ * filtered values represent a linear value (such as speed) or a circular *
+ * angle such as direction. The angle can be either in radians or degrees.*
+ * These values can be changed on the "fly" with the setFC() and setType()*
+ * methods.                                                               *
+ * The main method is filter() which accepts a new unfiltered value and   *
+ * returns a fitered value. To obtain the most recent filter output use   *
+ * the get() method. Lesser used methods are getType (returns tp) and     *
+ * getFC() (returns FC). The reset() method resets the filter to zero.    *
+ **************************************************************************
+ */
+#if ! defined( FILTEROBJ_CLASS_HEADER )
+#define FILTEROBJ_CLASS_HEADER
+
+// Define filter types
+enum
+{
+    FILTEROBJ_TYPE_LINEAR = 1 << 0,
+    FILTEROBJ_TYPE_DEG = 1 << 1,
+    FILTEROBJ_TYPE_RAD = 1 << 2
+};
+
+class filterobj
+{
+public:
+
+//    filterobj() {setFC(0.5); type = FILTEROBJ_TYPE_LINEAR; reset();};
+
+    filterobj(double fc = 0.5, int tp = FILTEROBJ_TYPE_LINEAR);
+    ~filterobj(){};
+    double filter(double data); // Return filtered data given new data point
+    void reset(double a = 0.0); // Clear filter
+    void setFC(double fc = 0.1);// Set cutoff frequency
+    void setType(int tp);       // Set type of filter (linear or angle type)
+    double getFc(void) const;         // Return cutoff frequency
+    int getType(void) const;          // Return type of filter
+    double get(void) const;           // Return the current filtered data
+
+protected:
+
+    void unwrapDeg(double deg);
+    void unwrapRad(double rad);
+
+private:
+
+    double a0;
+    double b1;
+    double accum;
+    double oldDeg;
+    double oldRad;
+    int wraps;
+    int type;
+};
+
+// Operators
+inline bool operator==(const filterobj& lhs, const filterobj& rhs){ return (lhs.get() == rhs.get()); }
+inline bool operator!=(const filterobj& lhs, const filterobj& rhs){ return !(rhs == lhs); }
+
+inline bool operator<(const filterobj& lhs, const filterobj& rhs){ return (lhs.get() < rhs.get()); }
+inline bool operator>(const filterobj& lhs, const filterobj& rhs){ return rhs < lhs; }
+inline bool operator<=(const filterobj& lhs, const filterobj& rhs){ return !(lhs > rhs); }
+inline bool operator>=(const filterobj& lhs, const filterobj& rhs){ return !(lhs < rhs); }
+
+#endif
---
+++ src/filterobj.cpp
@@ -0,0 +1,114 @@
+#include "filterobj.h"
+
+#include <wx/math.h>
+
+filterobj::filterobj(double fc, int tp) {
+    wxASSERT(tp == FILTEROBJ_TYPE_DEG || tp == FILTEROBJ_TYPE_LINEAR || tp == FILTEROBJ_TYPE_RAD);
+    setFC(fc);
+    type = tp;
+    reset(0.0);
+}
+
+double filterobj::filter(double data) {
+    if (!wxIsNaN(data) && !wxIsNaN(b1)) {
+        if (wxIsNaN(accum))
+            accum = 0.0;
+        switch (type) {
+            case FILTEROBJ_TYPE_LINEAR:
+                accum = accum * b1 + a0 * data;
+                break;
+
+            case FILTEROBJ_TYPE_DEG:
+                unwrapDeg(data);
+                accum = accum * b1 + a0 * (oldDeg + 360.0*wraps);
+                break;
+                   
+            case FILTEROBJ_TYPE_RAD:
+                unwrapRad(data);
+                accum = accum * b1 + a0 * (oldRad + 2.0*M_PI*wraps);
+                break;
+                    
+            default:
+                wxASSERT(false);
+        }
+    }
+    else
+        accum = data;
+    return get();
+}
+
+void filterobj::reset(double a) {
+    accum = a;
+    oldDeg = NAN;
+    oldRad = NAN;
+    wraps = 0;
+}
+
+void filterobj::setFC(double fc) {
+    if (wxIsNaN(fc) || fc <= 0.0) {
+        a0 = b1 = NAN;  // NAN means no filtering will be done
+        reset();
+    }
+    else {
+        b1 = exp(-2.0 * 3.1415926535897932384626433832795 * fc);
+        a0 = 1 - b1;
+    }
+}
+
+void filterobj::setType(int tp)
+{
+    wxASSERT(tp == FILTEROBJ_TYPE_DEG || tp == FILTEROBJ_TYPE_LINEAR || tp == FILTEROBJ_TYPE_RAD);
+    type = tp;
+}
+
+double filterobj::getFc(void) const
+{
+    if (wxIsNaN(b1))
+        return 0.0;
+    double fc = log(b1) / (-2.0 * 3.1415926535897932384626433832795);
+    return fc;
+}
+
+int filterobj::getType(void) const
+{
+    return type;
+}
+
+double filterobj::get(void) const {
+    if (wxIsNaN(accum))
+        return accum;
+    double res = accum;
+    switch (type) {
+        case FILTEROBJ_TYPE_DEG:
+            while (res < 0) res += 360.0;
+            while (res > 360) res -= 360.0;
+            break;
+                
+        case FILTEROBJ_TYPE_RAD:
+            while (res < 0) res += 2.0*M_PI;
+            while (res > 2.0*M_PI) res -= 2.0*M_PI;
+            break;
+    }
+    return res;
+}
+
+void filterobj::unwrapDeg(double deg) {
+    if (deg - oldDeg > 180) {
+        wraps--;
+    }
+    else if (deg - oldDeg < -180) {
+        wraps++;
+    }
+    oldDeg = deg;
+}
+
+void filterobj::unwrapRad(double rad) {
+    if (rad - oldRad > M_PI) {
+        wraps--;
+    }
+    else if (rad - oldRad < M_PI) {
+        wraps++;
+    }
+    oldRad = rad;
+}
+
