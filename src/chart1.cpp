bf0b4e215592ac895a4e465652a03b771a1b3b50
Sean D'Epagnier <seandepagnier@gmail.com>
2016-11-12 19:42:50 -0400

    remove dead windows code

--- src/chart1.cpp
+++ src/chart1.cpp
@@ -11327,70 +11327,7 @@ void RestoreSystemColors()
 #endif
 
 void SetSystemColors( ColorScheme cs )
-{
-//---------------
-#if 0
-    //    This is the list of Color Types from winuser.h
-    /*
-     * Color Types
-     */
-#define CTLCOLOR_MSGBOX         0
-#define CTLCOLOR_EDIT           1
-#define CTLCOLOR_LISTBOX        2
-#define CTLCOLOR_BTN            3
-#define CTLCOLOR_DLG            4
-#define CTLCOLOR_SCROLLBAR      5
-#define CTLCOLOR_STATIC         6
-#define CTLCOLOR_MAX            7
-
-#define COLOR_SCROLLBAR         0         //??
-#define COLOR_BACKGROUND        1         //??
-#define COLOR_ACTIVECAPTION     2       //??
-#define COLOR_INACTIVECAPTION   3         //??
-#define COLOR_MENU              4         // Menu background
-#define COLOR_WINDOW            5         // default window background
-#define COLOR_WINDOWFRAME       6         // Sub-Window frames, like status bar, etc..
-#define COLOR_MENUTEXT          7         // Menu text
-#define COLOR_WINDOWTEXT        8         //??
-#define COLOR_CAPTIONTEXT       9         //??
-#define COLOR_ACTIVEBORDER      10        //??
-#define COLOR_INACTIVEBORDER    11       //??
-#define COLOR_APPWORKSPACE      12       //??
-#define COLOR_HIGHLIGHT         13       //Highlited text background  in query box tree
-#define COLOR_HIGHLIGHTTEXT     14        //??
-#define COLOR_BTNFACE           15        //??
-#define COLOR_BTNSHADOW         16        // Menu Frame
-#define COLOR_GRAYTEXT          17        // Greyed out text in menu
-#define COLOR_BTNTEXT           18        //??
-#define COLOR_INACTIVECAPTIONTEXT 19      //??
-#define COLOR_BTNHIGHLIGHT      20        //??
-#if(WINVER >= 0x0400)
-#define COLOR_3DDKSHADOW        21        //??
-#define COLOR_3DLIGHT           22        // Grid rule lines in list control
-#define COLOR_INFOTEXT          23        //??
-#define COLOR_INFOBK            24
-#endif /* WINVER >= 0x0400 */
-
-#if(WINVER >= 0x0500)
-#define COLOR_HOTLIGHT          26              //??
-#define COLOR_GRADIENTACTIVECAPTION 27        //??
-#define COLOR_GRADIENTINACTIVECAPTION 28        //??
-#if(WINVER >= 0x0501)
-#define COLOR_MENUHILIGHT       29              // Selected item in menu, maybe needs reset on popup menu?
-#define COLOR_MENUBAR           30              //??
-#endif /* WINVER >= 0x0501 */
-#endif /* WINVER >= 0x0500 */
-
-#if(WINVER >= 0x0400)
-#define COLOR_DESKTOP           COLOR_BACKGROUND
-#define COLOR_3DFACE            COLOR_BTNFACE
-#define COLOR_3DSHADOW          COLOR_BTNSHADOW
-#define COLOR_3DHIGHLIGHT       COLOR_BTNHIGHLIGHT
-#define COLOR_3DHILIGHT         COLOR_BTNHIGHLIGHT
-#define COLOR_BTNHILIGHT        COLOR_BTNHIGHLIGHT
-#endif /* WINVER >= 0x0400 */
-#endif
-
+{//---------------
 #ifdef __WXMSW__
     int element[NCOLORS];
     int rgbcolor[NCOLORS];
