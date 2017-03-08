a7cc46f8ec39c7a693147c5a4882e399ad7d1fa5
David Register <bdbcat@yahoo.com>
2017-03-07 21:36:47 -0500

    Improve line rendering for thin lines in HPGL renderer.

--- src/s52plib.cpp
+++ src/s52plib.cpp
@@ -9056,13 +9056,17 @@ void RenderFromHPGL::SetPen()
     }
 #ifdef ocpnUSE_GL
     if( renderToOpenGl ) {
-        glEnable( GL_LINE_SMOOTH );
+    //    glEnable( GL_LINE_SMOOTH );
         glEnable( GL_POLYGON_SMOOTH );
-        glEnable( GL_BLEND );
         
         glColor4ub( penColor.Red(), penColor.Green(), penColor.Blue(), transparency );
-        glLineWidth( wxMax(g_GLMinSymbolLineWidth, (float) penWidth * 0.7) );
+        int line_width = wxMax(g_GLMinSymbolLineWidth, (float) penWidth * 0.7);
+        glLineWidth( line_width );
 #ifndef __OCPN__ANDROID__
+        if(line_width >= 2)
+            glEnable( GL_LINE_SMOOTH );
+        else
+            glDisable( GL_LINE_SMOOTH );
         glEnable( GL_BLEND );
 #endif        
     }
