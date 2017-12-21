9759309de57f39a30a8fb2891f44eff0b39968fa
David Register <bdbcat@yahoo.com>
2017-12-21 11:44:24 -0500

    Revert "disable full-world grib overlay for texture rectangle (it doesn't work as expected)"
    This reverts commit d44168d7cd9e13ce0707cd0490ecf638c14d5d6e.

--- plugins/grib_pi/src/GribOverlayFactory.cpp
+++ plugins/grib_pi/src/GribOverlayFactory.cpp
@@ -522,10 +522,6 @@ bool GRIBOverlayFactory::CreateGribGLTexture( GribOverlay *pGO, int settings, Gr
 {
     bool repeat = pGR->getLonMin() == 0 && pGR->getLonMax() + pGR->getDi() == 360;
 
-    // texture rectangle doesn't repeat correctly
-    if( repeat && texture_format != GL_TEXTURE_2D )
-        return false;
-
     // create the texture to the size of the grib data plus a transparent border
     int tw = pGR->getNi()+2*!repeat, th = pGR->getNj()+2;
     
@@ -2140,8 +2136,8 @@ void GRIBOverlayFactory::drawLineBuffer(LineBuffer &buffer, int x, int y, double
 void GRIBOverlayFactory::texcoord(double u, double v, GribRecord *pGR)
 {
     if(texture_format != GL_TEXTURE_2D) {
-        u *= pGR->getNi() + 2;
-        v *= pGR->getNj() + 2;
+        u *= pGR->getNi();
+        v *= pGR->getNj();
     }
     glTexCoord2d(u, v);
 }
