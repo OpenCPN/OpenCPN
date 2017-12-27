8455c6d5dc0d4305431112149362f73ac465c509
Theeko <sylvain.carlioz@gmail.com>
2017-12-27 15:55:07 +0100

    Add default boat speed unit in knots

--- src/options.cpp
+++ src/options.cpp
@@ -3848,11 +3848,10 @@ void options::CreatePanel_Display(size_t parent, int border_size,
     // (for calculation, in case GPS speed is null)
     wxBoxSizer *defaultBoatSpeedSizer = new wxBoxSizer(wxHORIZONTAL);
     boxDispStatusBar->Add(defaultBoatSpeedSizer, wxALL, group_item_spacing);
-    defaultBoatSpeedSizer->Add(new wxStaticText(pDisplayPanel, wxID_ANY, _("Default Boat Speed")),
+    defaultBoatSpeedSizer->Add(new wxStaticText(pDisplayPanel, wxID_ANY, _("Default Boat Speed (knots)")),
                                groupLabelFlags);
     pSDefaultBoatSpeed = new wxTextCtrl(pDisplayPanel, ID_DEFAULT_BOAT_SPEED, _T(""), wxDefaultPosition,
                                         wxSize(50, -1), wxTE_RIGHT);
-    //pSDefaultBoatSpeed->Enable(g_bShowLiveETA);
     defaultBoatSpeedSizer->Add(pSDefaultBoatSpeed, 0, wxALIGN_CENTER_VERTICAL, group_item_spacing);
     
     // --------------------------------------
