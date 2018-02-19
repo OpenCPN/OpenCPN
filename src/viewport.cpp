88d6209793d7faf21239af8ee189d737f6c1d228
did-g <did-g@users.noreply.github.com>
2018-02-19 16:07:30 +0100

    GetVPRegionIntersect on error only delete points array if it was allocated by us
    This test is done every where but here. Trigger a core dump with cm93 in
    orthographic projection.

--- src/viewport.cpp
+++ src/viewport.cpp
@@ -561,7 +561,7 @@ OCPNRegion ViewPort::GetVPRegionIntersect( const OCPNRegion &Region, size_t nPoi
 
     if(!valid)
     {
-        delete[] pp;
+        if ( ppoints == NULL ) delete[] pp;
         return OCPNRegion(); //empty;
     }
  
