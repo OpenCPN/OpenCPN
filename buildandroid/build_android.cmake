62bcbb180a646421bfc853f17056b3c0f8ba4a03
= <bdbcat@yahoo.com>
2018-12-12 14:23:24 -0500

    Update cross-building tools definition for Android

--- buildandroid/build_android.cmake
+++ buildandroid/build_android.cmake
@@ -9,11 +9,12 @@ SET(CMAKE_SYSTEM_NAME Generic)
 #this one not so much
 SET(CMAKE_SYSTEM_VERSION 1)
 
-SET(TARGET_SUPPORTS_SHARED_LIBS TRUE)
 
 # specify the cross compiler
 SET(CMAKE_C_COMPILER     /home/dsr/Android/android-ndk-r10d/tools_19_48/bin/arm-linux-androideabi-gcc)
 SET(CMAKE_CXX_COMPILER   /home/dsr/Android/android-ndk-r10d/tools_19_48/bin/arm-linux-androideabi-g++)
+
+SET(TARGET_SUPPORTS_SHARED_LIBS TRUE)
 
 #Location of the root of the Android NDK
 SET(NDK_Base /home/dsr/Android/android-ndk-r10d )
