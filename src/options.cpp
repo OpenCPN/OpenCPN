7ff2751e840eb52ce479931e7a115da5dfe38fbd
= <bdbcat@yahoo.com>
2018-12-12 21:31:42 -0500

    Check for invalid list index on chart directory delete.

--- src/options.cpp
+++ src/options.cpp
@@ -7832,8 +7832,9 @@ void options::OnButtondeleteClick(wxCommandEvent& event) {
     pActiveChartsList->Delete(pListBoxSelections.Item((nSelections - i) - 1));
   }
 #else
-  int n = pActiveChartsList->GetSelection();
-  pActiveChartsList->Delete(n);
+  unsigned int n = pActiveChartsList->GetSelection();
+  if( n < pActiveChartsList->GetCount())        // Protect against invalid index
+    pActiveChartsList->Delete(n);
 #endif
 
   UpdateWorkArrayFromTextCtl();
