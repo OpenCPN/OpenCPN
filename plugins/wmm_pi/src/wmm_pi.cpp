c2b0ff48e389f841faf2475fd774648b86aae3e4
Pavel Kalian <pavel@kalian.cz>
2019-02-05 15:56:47 -0300

    Update the WMM plugin to WMM2015v2 data and code
    - The library updated to the latest code from https://www.ngdc.noaa.gov/geomag/WMM/soft.shtml#downloads
    - Removed data embedded in the code, the plugin now exclusively uses the WMM.COF file

--- plugins/wmm_pi/src/wmm_pi.cpp
+++ plugins/wmm_pi/src/wmm_pi.cpp
@@ -6,7 +6,7 @@
  * Author:   Pavel Kalian
  *
  ***************************************************************************
- *   Copyright (C) 2011 by Pavel Kalian   *
+ *   Copyright (C) 2011-2019 by Pavel Kalian   *
  *   $EMAIL$   *
  *                                                 *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -113,9 +113,9 @@ and extended by Sean D'Epagnier to support plotting."));
 wmm_pi::wmm_pi(void *ppimgr)
     : opencpn_plugin_18(ppimgr),
     m_bShowPlot(false), 
-    m_DeclinationMap(DECLINATION, MagneticModel, TimedMagneticModel, &Ellip),
-    m_InclinationMap(INCLINATION, MagneticModel, TimedMagneticModel, &Ellip),
-    m_FieldStrengthMap(FIELD_STRENGTH, MagneticModel, TimedMagneticModel, &Ellip),
+    m_DeclinationMap(DECLINATION_PLOT, MagneticModel, TimedMagneticModel, &Ellip),
+    m_InclinationMap(INCLINATION_PLOT, MagneticModel, TimedMagneticModel, &Ellip),
+    m_FieldStrengthMap(FIELD_STRENGTH_PLOT, MagneticModel, TimedMagneticModel, &Ellip),
     m_bComputingPlot(false)
 {
     // Create the PlugIn icons
@@ -130,6 +130,9 @@ int wmm_pi::Init(void)
     m_wmm_dialog_x = 0;
     m_wmm_dialog_y = 0;
 
+    MagneticModel = NULL;
+    TimedMagneticModel = NULL;
+    
     ::wxDisplaySize(&m_display_width, &m_display_height);
 
     //    Get a pointer to the opencpn display canvas, to use as a parent for the POI Manager dialog
@@ -152,47 +155,43 @@ int wmm_pi::Init(void)
     _T("data") + wxFileName::GetPathSeparator();
     
     //    WMM initialization
+    
     /* Memory allocation */
-    int NumTerms = ( ( WMM_MAX_MODEL_DEGREES + 1 ) * ( WMM_MAX_MODEL_DEGREES + 2 ) / 2 );    /* WMM_MAX_MODEL_DEGREES is defined in WMM_Header.h */
-
-    MagneticModel       = WMM_AllocateModelMemory(NumTerms);  /* For storing the WMM Model parameters */
-    TimedMagneticModel  = WMM_AllocateModelMemory(NumTerms);  /* For storing the time modified WMM Model parameters */
-    if(MagneticModel == NULL || TimedMagneticModel == NULL)
-    {
-        //WMM_Error(2); Nohal - We don't want the printf's
+    int NumTerms, epochs = 1, nMax = 0;
+    wxString cof_filename = m_shareLocn + "WMM.COF";
+    
+    if(!MAG_robustReadMagModels((const_cast<char*>((const char*)cof_filename.mb_str())), (MAGtype_MagneticModel *(*)[])&MagneticModels, epochs)) {
         WMMLogMessage1(_T("initialization error"));
         m_buseable = false;
-    }
-
-    if( m_buseable )
-    {
-        WMM_SetDefaults(&Ellip, MagneticModel, &Geoid); /* Set default values and constants */
+    } else {
+        WMMLogMessage1(wxString::Format(_T("WMM model data loaded from file %s."), cof_filename.c_str()));
+        for(int i = 0; i < epochs; i++) {
+            if(MagneticModels[i]->nMax > nMax) {
+                nMax = MagneticModels[i]->nMax;
+            }
+        }
+        NumTerms = ((nMax + 1) * (nMax + 2) / 2);
+        
+        TimedMagneticModel = MAG_AllocateModelMemory(NumTerms); /* For storing the time modified WMM Model parameters */
+        
+        for(int i = 0; i < epochs; i++) {
+            if(MagneticModels[i] == NULL || TimedMagneticModel == NULL) {
+                WMMLogMessage1(_T("initialization error MAG_Error(2)"));
+                m_buseable = false;
+            }
+        }
+        
+        MagneticModel = MagneticModels[0];
+        
+        MAG_SetDefaults(&Ellip, &Geoid); /* Set default values and constants */
         /* Check for Geographic Poles */
-        //WMM_readMagneticModel_Large(filename, MagneticModel); //Uncomment this line when using the 740 model, and comment out the  WMM_readMagneticModel line.
-
-        filename = m_wmm_dir + _T("/WMM.COF");
-        wxCharBuffer buf = filename.ToUTF8();
-
-        if (0 == WMM_readMagneticModel(buf.data(), MagneticModel))
-        {
-            WMMLogMessage1(wxString::Format(_T("Warning: WMM model data file %s can't be loaded, using the bundled data."), filename.c_str()));
-            WMM_setupMagneticModel(wmm_cof_data, MagneticModel);
-        }
-        else
-        {
-            WMMLogMessage1(wxString::Format(_T("WMM model data loaded from file %s."), filename.c_str()));
-        }
+        
+        /* Set EGM96 Geoid parameters */
+        Geoid.GeoidHeightBuffer = GeoidHeightBuffer;
+        Geoid.Geoid_Initialized = 1;
+        /* Set EGM96 Geoid parameters END */
     }
-
- //     filename = m_wmm_dir + _T("/EGM9615.BIN");
- //     strncpy(geoiddatapath, (const char*)filename.mb_str(wxConvUTF8), 1023);
- //     if (FALSE == WMM_InitializeGeoid(&Geoid))    /* Read the Geoid file */
- //     {
- //         wxLogMessage(wxString::Format(_T("Warning: WMM model data file %s can't be loaded. Switching off the geoid calculations. The accuracy will be reduced"), filename.c_str()));
- //         m_busegeoid = false;
- //     }
-    //WMM_GeomagIntroduction(MagneticModel);  /* Print out the WMM introduction */
-
+    
     int ret_flag =  (WANTS_OVERLAY_CALLBACK |
     WANTS_OPENGL_OVERLAY_CALLBACK |
     WANTS_CURSOR_LATLON     |
@@ -232,8 +231,12 @@ bool wmm_pi::DeInit(void)
          m_pWmmDialog = NULL;
      }
     SaveConfig();
-    WMM_FreeMagneticModelMemory(MagneticModel);
-    WMM_FreeMagneticModelMemory(TimedMagneticModel);
+    if(MagneticModel) {
+        MAG_FreeMagneticModelMemory(MagneticModel);
+    }
+    if(TimedMagneticModel) {
+        MAG_FreeMagneticModelMemory(TimedMagneticModel);
+    }
 
     RemovePlugInTool(m_leftclick_tool_id);
 
@@ -481,11 +484,11 @@ void wmm_pi::SetCursorLatLon(double lat, double lon)
     UserDate.Month = wxDateTime::GetCurrentMonth() + 1; //WHY is it 0 based????????
     UserDate.Day = wxDateTime::Now().GetDay();
     char err[255];
-    WMM_DateToYear(&UserDate, err);
-    WMM_GeodeticToSpherical(Ellip, CoordGeodetic, &CoordSpherical);    /*Convert from geodeitic to Spherical Equations: 17-18, WMM Technical report*/
-    WMM_TimelyModifyMagneticModel(UserDate, MagneticModel, TimedMagneticModel); /* Time adjust the coefficients, Equation 19, WMM Technical report */
-    WMM_Geomag(Ellip, CoordSpherical, CoordGeodetic, TimedMagneticModel, &GeoMagneticElements);   /* Computes the geoMagnetic field elements and their time change*/
-    WMM_CalculateGridVariation(CoordGeodetic,&GeoMagneticElements);
+    MAG_DateToYear(&UserDate, err);
+    MAG_GeodeticToSpherical(Ellip, CoordGeodetic, &CoordSpherical);    /*Convert from geodeitic to Spherical Equations: 17-18, WMM Technical report*/
+    MAG_TimelyModifyMagneticModel(UserDate, MagneticModel, TimedMagneticModel); /* Time adjust the coefficients, Equation 19, WMM Technical report */
+    MAG_Geomag(Ellip, CoordSpherical, CoordGeodetic, TimedMagneticModel, &GeoMagneticElements);   /* Computes the geoMagnetic field elements and their time change*/
+    MAG_CalculateGridVariation(CoordGeodetic,&GeoMagneticElements);
     //WMM_PrintUserData(GeoMagneticElements,CoordGeodetic, UserDate, TimedMagneticModel, &Geoid);     /* Print the results */
     m_pWmmDialog->m_tcF->SetValue(wxString::Format(_T("%-9.1lf nT"), GeoMagneticElements.F));
     m_pWmmDialog->m_tcH->SetValue(wxString::Format(_T("%-9.1lf nT"), GeoMagneticElements.H));
@@ -501,6 +504,9 @@ void wmm_pi::SetCursorLatLon(double lat, double lon)
 
 void wmm_pi::SetPositionFix(PlugIn_Position_Fix &pfix)
 {
+    if(!m_buseable) {
+        return;
+    }
     CoordGeodetic.lambda = pfix.Lon;
     CoordGeodetic.phi = pfix.Lat;
     CoordGeodetic.HeightAboveEllipsoid = 0;
@@ -509,11 +515,11 @@ void wmm_pi::SetPositionFix(PlugIn_Position_Fix &pfix)
     UserDate.Month = wxDateTime::GetCurrentMonth() + 1; //WHY is it 0 based????????
     UserDate.Day = wxDateTime::Now().GetDay();
     char err[255];
-    WMM_DateToYear(&UserDate, err);
-    WMM_GeodeticToSpherical(Ellip, CoordGeodetic, &CoordSpherical);    /*Convert from geodeitic to Spherical Equations: 17-18, WMM Technical report*/
-    WMM_TimelyModifyMagneticModel(UserDate, MagneticModel, TimedMagneticModel); /* Time adjust the coefficients, Equation 19, WMM Technical report */
-    WMM_Geomag(Ellip, CoordSpherical, CoordGeodetic, TimedMagneticModel, &GeoMagneticElements);   /* Computes the geoMagnetic field elements and their time change*/
-    WMM_CalculateGridVariation(CoordGeodetic,&GeoMagneticElements);
+    MAG_DateToYear(&UserDate, err);
+    MAG_GeodeticToSpherical(Ellip, CoordGeodetic, &CoordSpherical);    /*Convert from geodeitic to Spherical Equations: 17-18, WMM Technical report*/
+    MAG_TimelyModifyMagneticModel(UserDate, MagneticModel, TimedMagneticModel); /* Time adjust the coefficients, Equation 19, WMM Technical report */
+    MAG_Geomag(Ellip, CoordSpherical, CoordGeodetic, TimedMagneticModel, &GeoMagneticElements);   /* Computes the geoMagnetic field elements and their time change*/
+    MAG_CalculateGridVariation(CoordGeodetic,&GeoMagneticElements);
     //WMM_PrintUserData(GeoMagneticElements,CoordGeodetic, UserDate, TimedMagneticModel, &Geoid);     /* Print the results */
     
     m_boatVariation = GeoMagneticElements;
@@ -660,11 +666,11 @@ void wmm_pi::SendVariationAt(double lat, double lon, int year, int month, int da
     UserDate.Month = month;
     UserDate.Day = day;
     char err[255];
-    WMM_DateToYear(&UserDate, err);
-    WMM_GeodeticToSpherical(Ellip, CoordGeodetic, &CoordSpherical);    /*Convert from geodeitic to Spherical Equations: 17-18, WMM Technical report*/
-    WMM_TimelyModifyMagneticModel(UserDate, MagneticModel, TimedMagneticModel); /* Time adjust the coefficients, Equation 19, WMM Technical report */
-    WMM_Geomag(Ellip, CoordSpherical, CoordGeodetic, TimedMagneticModel, &GeoMagneticElements);   /* Computes the geoMagnetic field elements and their time change*/
-    WMM_CalculateGridVariation(CoordGeodetic,&GeoMagneticElements);
+    MAG_DateToYear(&UserDate, err);
+    MAG_GeodeticToSpherical(Ellip, CoordGeodetic, &CoordSpherical);    /*Convert from geodeitic to Spherical Equations: 17-18, WMM Technical report*/
+    MAG_TimelyModifyMagneticModel(UserDate, MagneticModel, TimedMagneticModel); /* Time adjust the coefficients, Equation 19, WMM Technical report */
+    MAG_Geomag(Ellip, CoordSpherical, CoordGeodetic, TimedMagneticModel, &GeoMagneticElements);   /* Computes the geoMagnetic field elements and their time change*/
+    MAG_CalculateGridVariation(CoordGeodetic,&GeoMagneticElements);
     v[_T("Decl")] = GeoMagneticElements.Decl;
     v[_T("Decldot")] = GeoMagneticElements.Decldot;
     v[_T("F")] = GeoMagneticElements.F;
@@ -914,71 +920,3 @@ void wmm_pi::ShowPlotSettings()
     }
     delete dialog;
 }
-
-/*!
- * \brief
- * sets up the magnetic model with our bundled data.
- * 
- * \param data
- * string containing the coefitient data
- * 
- * \param MagneticModel
- * Magnetic model
- * 
- * \returns
- * true on success
- * 
- * 
- * This is a modification of WMM_readMagneticModel() to set up the model in case the data files are missing.
- */
-int WMM_setupMagneticModel(char *data, WMMtype_MagneticModel * MagneticModel)
-{
-    char c_str[81], c_new[5];   /*these strings are used to read a line from coefficient file*/
-    int i, icomp, m, n, EOF_Flag = 0, index;
-    double epoch, gnm, hnm, dgnm, dhnm;
-    char *c_tmp;
-    char *tmp_data;
-
-    tmp_data = strdup(data);
-
-    MagneticModel->Main_Field_Coeff_H[0] = 0.0;
-    MagneticModel->Main_Field_Coeff_G[0] = 0.0;
-    MagneticModel->Secular_Var_Coeff_H[0] = 0.0;
-    MagneticModel->Secular_Var_Coeff_G[0] = 0.0;
-
-    c_tmp = strtok(tmp_data, "\n");
-    strncpy(c_str, c_tmp, 80);
-    c_str[80] = '\0';
-    sscanf(c_str,"%lf%s",&epoch, MagneticModel->ModelName);
-    MagneticModel->epoch = epoch;
-    while (EOF_Flag == 0)
-    {
-        c_tmp = strtok(NULL, "\n");
-        strncpy(c_str, c_tmp, 80);
-        /* CHECK FOR LAST LINE IN FILE */
-        for (i=0; i<4 && (c_str[i] != '\0'); i++)
-        {
-            c_new[i] = c_str[i];
-            c_new[i+1] = '\0';
-        }
-        icomp = strcmp("9999", c_new);
-        if (icomp == 0)
-        {
-            EOF_Flag = 1;
-            break;
-        }
-        /* END OF FILE NOT ENCOUNTERED, GET VALUES */
-        sscanf(c_str,"%d%d%lf%lf%lf%lf",&n,&m,&gnm,&hnm,&dgnm,&dhnm);
-        if (m <= n)
-        {
-            index = (n * (n + 1) / 2 + m);
-            MagneticModel->Main_Field_Coeff_G[index] = gnm;
-            MagneticModel->Secular_Var_Coeff_G[index] = dgnm;
-            MagneticModel->Main_Field_Coeff_H[index] = hnm;
-            MagneticModel->Secular_Var_Coeff_H[index] = dhnm;
-        }
-    }
-    
-    free(tmp_data);
-    return TRUE;
-} /*WMM_setupMagneticModel */
