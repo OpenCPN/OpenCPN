a971862b2d4259dc0c8ab4cad9ba2dac0c637198
Pavel Kalian <pavel@kalian.cz>
2019-03-04 19:11:29 -0300

    Guard against EVT_UPDATE while setting planned route speed programatically as it may lead to infinite loop

--- src/RoutePropDlgImpl.cpp
+++ src/RoutePropDlgImpl.cpp
@@ -271,7 +271,9 @@ void RoutePropDlgImpl::UpdatePoints()
     m_pRoute->UpdateSegmentDistances( m_pRoute->m_PlannedSpeed );           // to fix ETA properties
     m_tcDistance->SetValue(wxString::Format(wxT("%5.1f ") + getUsrDistanceUnit(), toUsrDistance(m_pRoute->m_route_length)));
     m_tcEnroute->SetValue(formatTimeDelta(wxLongLong(m_pRoute->m_route_time)));
+    m_tcPlanSpeed->SetEvtHandlerEnabled(false);
     m_tcPlanSpeed->SetValue(wxString::FromDouble(toUsrDistance(m_pRoute->m_PlannedSpeed)));
+    m_tcPlanSpeed->SetEvtHandlerEnabled(true);
     //  Iterate on Route Points, inserting blank fields starting with index 0
     wxRoutePointListNode *pnode = m_pRoute->pRoutePointList->GetFirst();
     int in = 0;
@@ -570,7 +572,9 @@ void RoutePropDlgImpl::PlanSpeedOnTextEnter( wxCommandEvent& event )
             UpdatePoints();
         }
     } else {
+        m_tcPlanSpeed->SetEvtHandlerEnabled(false);
         m_tcPlanSpeed->SetValue(wxString::FromDouble(toUsrSpeed(m_pRoute->m_PlannedSpeed)));
+        m_tcPlanSpeed->SetEvtHandlerEnabled(true);
     }
 }
 
@@ -585,7 +589,9 @@ void RoutePropDlgImpl::PlanSpeedOnKillFocus( wxFocusEvent& event )
             UpdatePoints();
         }
     } else {
+        m_tcPlanSpeed->SetEvtHandlerEnabled(false);
         m_tcPlanSpeed->SetValue(wxString::FromDouble(toUsrSpeed(m_pRoute->m_PlannedSpeed)));
+        m_tcPlanSpeed->SetEvtHandlerEnabled(true);
     }
     event.Skip();
 }
