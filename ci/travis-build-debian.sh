2254498d5beb077dbe0604098b479ecc9f27430e
Alec Leamas <leamas.alec@gmail.com>
2019-03-09 15:03:00 -0500

    build: Get rid of hardcoded WX_PKG_NAME
    Use a debian control file to describe the dependencies. The package
    depsolvers can easily handle all sorts of conditions like the
    one here (two alternative packages).

--- ci/travis-build-debian.sh
+++ ci/travis-build-debian.sh
@@ -5,34 +5,14 @@
 #
 set -xe
 sudo apt-get -qq update
-
-sudo apt-get --allow-unauthenticated install \
-  gettext                  \
-  libarchive-dev           \
-  libcairo2-dev            \
-  libcurl4-openssl-dev     \
-  libelf-dev               \
-  libexif-dev              \
-  libexpat1-dev            \
-  libglu1-mesa-dev         \
-  libgps-dev               \
-  libgtk2.0-dev libbz2-dev \
-  liblzma-dev              \
-  libportaudio2            \
-  libsqlite3-dev           \
-  libtinyxml-dev           \
-  libwxgtk3.0-dev          \
-  $WX_PKG_NAME             \
-  portaudio19-dev
-
-if [[ "$USE_WEBVIEW" == "ON" ]]; then 
-    sudo apt-get --allow-unauthenticated install \
-        libwxgtk-webview3.0-dev \
-        libgtk-3-dev
-fi
+sudo apt-get install devcscripts
 
 mkdir  build
 cd build
+mk-build-deps ../ci/control
+sudo dpkg -i *all.deb 
+sudo apt-get --allow-unauthenticated install -f
+
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make -sj2
 make package
