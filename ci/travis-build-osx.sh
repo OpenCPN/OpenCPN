214cafe195223e9ad077d968df0d1c67d622498b
Pavel Kalian <pavel@kalian.cz>
2019-04-08 16:52:04 -0300

    Implement macOS application bundle signing

--- ci/travis-build-osx.sh
+++ ci/travis-build-osx.sh
@@ -40,3 +40,25 @@ make install
 make install # Dunno why the second is needed but it is, otherwise
              # plugin data is not included in the bundle
 make create-dmg
+
+# Signing stuff
+f="$(ls OpenCPN*.dmg)"
+hdiutil attach -owners on "$f" -shadow
+echo "Decrypting Cert"
+openssl aes-256-cbc -K $encrypted_287837bd9398_key -iv $encrypted_287837bd9398_iv -in ../ci/Certificates.p12.enc -out Certificates.p12 -d
+echo "Creating Keychain"
+security create-keychain -p "$SECRET_PASSWORD" build.keychain
+security default-keychain -s build.keychain
+security unlock-keychain -p "$SECRET_PASSWORD" build.keychain
+security set-keychain-settings -t 3600 -u build.keychain
+echo "Importing certs into keychain"
+security import ./Certificates.p12 -k build.keychain -T "$(which codesign)" -P "$SECRET_PASSWORD"
+rm -f Certificates.p12
+# macOS 10.12+
+security set-key-partition-list -S apple-tool:,apple: -s -k "$SECRET_PASSWORD" build.keychain
+echo "Signing Package"
+codesign --deep --sign "Pavel Kalian - Code Signing" "/Volumes/OpenCPN Installer/OpenCPN.app"
+security delete-keychain build.keychain;
+hdiutil detach "/Volumes/OpenCPN Installer"
+hdiutil convert -format UDZO -o new.dmg "$f" -shadow
+mv new.dmg $f
