9d1ea5925bba9e9b1d53a7b6c0cafb148771abf2
Alec Leamas <leamas.alec@gmail.com>
2019-09-16 22:57:41 +0200

    Add a version datatype.
    Versions are typically used either as printed info or when comparing
    version levels. Add a datatype solving this once and for all.

---
+++ include/semantic_vers.h
@@ -0,0 +1,77 @@
+/******************************************************************************
+ *
+ * Project:  OpenCPN
+ *
+ ***************************************************************************
+ *   Copyright (C) 2019 Alec Leamas                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ ***************************************************************************
+ */
+
+#ifndef SEMANTIC_VERSION_H_GUARD
+#define SEMANTIC_VERSION_H_GUARD
+
+#include "config.h"
+#include <sstream>
+
+#undef major                // walk around gnu's major() and minor() macros.
+#undef minor
+
+
+/**
+ * Versions are simplified semantic versioning: major.minor.revision-tag+build
+ * for example 1.2.6+1-alfa+deadbee. The values major, minor and revision should
+ * be integers. The tag is a pre-release marker, the build is build info.
+ *
+ * Parsing and comparing follows the spec besides the pre-release tag which 
+ * is sorted strictly lexically (no dotted parts support).
+ *
+ * See: https://semver.org/
+ */
+struct SemanticVersion
+{
+    int major;
+    int minor;
+    int patch;
+    std::string pre;    // Pre-release tag like alfa
+    std::string build;  // Build info
+
+    /** Construct a "0.0.0" version. */ 
+    SemanticVersion();
+
+    /** Parse a version string, sets major == -1 on errors. */
+    SemanticVersion(std::string version_release);
+
+    SemanticVersion(int major, int minor, int rev = 0,
+                    std::string pre = "",  std::string build = "");
+
+    bool operator <  (const SemanticVersion& other);
+    bool operator == (const SemanticVersion& other);
+    bool operator >  (const SemanticVersion& other);
+    bool operator <= (const SemanticVersion& other);
+    bool operator >= (const SemanticVersion& other);
+    bool operator != (const SemanticVersion& other);
+
+    /** Return printable representation. */
+    std::string to_string();
+};
+
+/** Dump version string. */
+std::ostream& operator << (std::ostream& s, const SemanticVersion& v);
+
+
+#endif  // SEMANTIC_VERSION_H_GUARD
---
+++ src/semantic_vers.cpp
@@ -0,0 +1,133 @@
+/******************************************************************************
+ *
+ * Project:  OpenCPN
+ *
+ ***************************************************************************
+ *   Copyright (C) 2019 Alec Leamas                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ ***************************************************************************
+ */
+
+
+#include "config.h"
+
+#include <cstdio>
+#include <string>
+#include <sstream>
+
+#include "semantic_vers.h"
+
+#undef major                // walk around gnu's major() and minor() macros.
+#undef minor
+
+SemanticVersion::SemanticVersion()
+    :major(0), minor(0), patch(0), pre(""), build("")
+{}
+
+
+SemanticVersion::SemanticVersion(std::string s)
+    :SemanticVersion()
+{
+    using namespace std;
+    int pos = s.find('+');
+    if (pos != string::npos) {
+        build = s.substr(pos + 1);
+        s = s.substr(0, pos);
+    }
+    pos = s.find('-');
+    if (pos != string::npos) {
+        pre = s.substr(pos + 1);
+        s = s.substr(0, pos);
+    }
+    int r = sscanf(s.c_str(), "%d.%d.%d", &major, &minor, &patch);
+    if (r < 2) {
+        major = -1;
+    }
+}
+
+
+SemanticVersion::SemanticVersion(
+    int major, int minor, int patch, std::string pre, std::string build)
+{
+    this->major = major;
+    this->minor = minor;
+    this->patch = patch;
+    this->pre = pre;
+    this->build = build;
+}
+
+bool SemanticVersion::operator < (const SemanticVersion& other)
+{
+    if (major < other.major) return true;
+    if (major > other.major) return false;
+    if (minor < other.minor) return true;
+    if (minor > other.minor) return false;
+    if (patch < other.patch) return true;
+    if (patch > other.patch) return false;
+    int len = std::min(pre.length(), other.pre.length());
+    if (pre.substr(0, len) != other.pre.substr(0, len)) {
+        return pre.substr(0, len) < other.pre.substr(0, len);
+    }
+    return pre.length() > other.pre.length();
+}
+
+bool SemanticVersion::operator == (const SemanticVersion& other)
+{
+    return major == other.major
+        && minor == other.minor
+        && patch == other.patch
+        && pre == other.pre;
+}
+
+bool SemanticVersion::operator > (const SemanticVersion& other)
+{
+    return !(*this == other) && !(*this < other);
+}
+
+bool SemanticVersion::operator <= (const SemanticVersion& other)
+{
+    return (*this == other) || (*this < other);
+}
+
+bool SemanticVersion::operator >= (const SemanticVersion& other)
+{
+    return (*this == other) || (*this > other);
+}
+
+bool SemanticVersion::operator != (const SemanticVersion& other)
+{
+    return !(*this == other);
+}
+
+std::ostream& operator << (std::ostream& s, const SemanticVersion& v)
+{
+    s << v.major << '.' << v.minor << '.' << v.patch;
+    if (v.pre != "" ) { 
+        s << '-' << v.pre;
+    }
+    if (v.build != "" ) { 
+        s << '+' << v.build;
+    }
+    return s;
+}
+
+std::string SemanticVersion::to_string()
+{
+    std::ostringstream os;
+    os << *this;
+    return os.str();
+}
