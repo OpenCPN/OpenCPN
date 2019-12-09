28b51768bf5b2d92a1b9eae8e4b1208d799478c9
bdbcat <bdbcat@yahoo.com>
2019-12-08 19:02:40 -0500

    Merge pull request #1455 from leamas/mingw
    build: mingw: Bundle required runtime libs (closes: #1400).

--- ci/docker-build-mingw.sh
+++ ci/docker-build-mingw.sh
@@ -11,7 +11,9 @@ sudo dnf copr enable -y --nogpgcheck leamas/opencpn-mingw
 sudo dnf builddep  -y /opencpn-ci/mingw/fedora/opencpn-deps.spec
 cd /opencpn-ci
 rm -rf build; mkdir build; cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=../mingw/fedora/toolchain.cmake \
+cmake \
+    -DCMAKE_TOOLCHAIN_FILE=../mingw/fedora/toolchain.cmake \
+    -DOCPN_CI_BUILD:BOOL=ON \
     ..
 
 make -j2
