afd7f14136509c249a4895cad2005930e9b2c0aa
Florian La Roche <Florian.LaRoche@gmail.com>
2020-01-05 19:39:41 +0100

    code cleanup for Apple build

--- plugins/dashboard_pi/src/dial.cpp
+++ plugins/dashboard_pi/src/dial.cpp
@@ -99,12 +99,12 @@ wxSize DashboardInstrument_Dial::GetSize( int orient, wxSize hint )
 
 void DashboardInstrument_Dial::SetData(int st, double data, wxString unit)
 {
-      if ( (st == m_MainValueCap))
+      if (st == m_MainValueCap)
       {
             m_MainValue = data;
             m_MainValueUnit = unit;
       }
-      else if ( (st == m_ExtraValueCap))
+      else if (st == m_ExtraValueCap)
       {
             m_ExtraValue = data;
             m_ExtraValueUnit = unit;
