0e86bbdfb6c54759a99608139b82d18a967e20a8
Alec Leamas <leamas.alec@nowhere.net>
2020-03-01 16:48:27 +0100

    safe-mode: Use OCPNTimedMessageDialog instad of wxDialog.

--- src/safe_mode.cpp
+++ src/safe_mode.cpp
@@ -8,6 +8,7 @@
 
 #include "OCPNPlatform.h"
 #include "ocpn_utils.h"
+#include "chart1.h"
 
 #include "safe_mode.h"
 
@@ -45,76 +46,6 @@ static std::string check_file_path()
     return path;
 }
 
-
-static void LoadIcon(const char* plugin_name, wxBitmap& bitmap, int size=32)
-{
-    wxFileName path(g_Platform->GetSharedDataDir(), plugin_name);
-    path.AppendDir("uidata");
-    path.AppendDir("markicons");
-    bool ok = false;
-    path.SetExt("svg");
-    if (path.IsFileReadable()) {
-        wxImage img = LoadSVGIcon(path.GetFullPath(), size, size);
-        bitmap = wxBitmap(img);
-        ok = bitmap.IsOk();
-    }
-    if (!ok) {
-        wxLogMessage("Cannot load Symbol-Question-Black.svg icon");
-    }
-}
-
-
-class SafeModeDialog: public wxDialog
-{
-
-    public:
-        SafeModeDialog() :
-            wxDialog(0, wxID_ANY, "Safe restart") 
-        {
-            timer.Bind(wxEVT_TIMER, [&](wxTimerEvent) { EndModal(wxID_NO); });
-            timer.StartOnce(TIMEOUT_SECONDS * 1000);
-
-            wxBitmap bitmap;
-            LoadIcon("Symbol-Question-Black.svg", bitmap, 64);
-            auto static_bitmap = new wxStaticBitmap(this, wxID_ANY, bitmap);
-
-            auto hbox = new wxBoxSizer(wxHORIZONTAL);
-            hbox->Add(static_bitmap);
-            hbox->Add(new wxStaticText(this, wxID_ANY,  LAST_RUN_ERROR_MSG),
-                      wxSizerFlags().Expand());
-
-            auto no_btn = new wxButton(this, wxID_NO);
-            no_btn->SetFocus();
-            no_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED,
-                         [&](wxCommandEvent&) { EndModal(wxID_NO); } );
-
-            auto yes_btn = new wxButton(this, wxID_YES);
-            yes_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED,
-                          [&](wxCommandEvent&) { EndModal(wxID_YES); } );
-
-            auto buttons = new wxStdDialogButtonSizer();
-            buttons->Add(no_btn, wxSizerFlags().Border());
-            buttons->Add(yes_btn, wxSizerFlags().Border());
-
-            auto vbox = new wxBoxSizer(wxVERTICAL);
-            vbox->Add(hbox);
-            vbox->Add(buttons);
-
-            SetSizer(vbox);
-            auto size = GetTextExtent(
-                "in safe mode without plugins and other possibly problematic");
-            SetMinClientSize(wxSize(size.GetWidth() * 12 / 10,
-                             size.GetHeight() * 8));
-            SetMaxClientSize(wxSize(-1, size.GetHeight() * 8));
-            Layout();
-            Show();
-        };
-
-    private:
-        wxTimer timer;
-};
-
-
 void set_mode(bool mode)
 {
     safe_mode = mode;
@@ -138,7 +69,14 @@ void check_last_start()
         dest.close();
         return;
     }
-    auto dlg = new SafeModeDialog();
+    long style = wxYES | wxNO | wxNO_DEFAULT | wxICON_QUESTION;
+    auto dlg = new OCPN_TimedHTMLMessageDialog(0,
+                                               LAST_RUN_ERROR_MSG,
+                                               _("Safe restart"),
+                                               TIMEOUT_SECONDS,
+                                               style,
+                                               false,
+                                               wxDefaultPosition);
     int reply = dlg->ShowModal();
     safe_mode = reply == wxID_YES;
     dlg->Destroy();
