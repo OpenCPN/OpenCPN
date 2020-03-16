0abdcb1b44d869a7c6406af2613d25b71de4dda4
dsr <bdbcat@yahoo.com>
2020-03-15 22:53:48 -0400

    Adjust some preprocessor #defines to avoid warning message

--- libs/gdal/include/gdal/cpl_port.h
+++ libs/gdal/include/gdal/cpl_port.h
@@ -255,8 +255,8 @@ typedef unsigned long    GUIntBig;
 #endif
 
 #ifndef MAX
-#  define MIN(a,b)      ((a<b) ? a : b)
-#  define MAX(a,b)      ((a>b) ? a : b)
+#  define MIN(a,b)      a < b ? a : b
+#  define MAX(a,b)      a > b ? a : b
 #endif
 
 #ifndef ABS
