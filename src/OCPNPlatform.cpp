OpenCPN/OpenCPN

commit 5b74a16bfc672fd125f8c279ad42e7d541769d7a

flatpak: Use separate configuration dir (#1852).
This moves the flatpak configuration dir from ~/.opencpn to
$XDG_CONFIG_HOME/opencpn, defaulting to
~./var/app/org.opencpn.OpenCPN/config/opencpn.

src/OCPNPlatform.cpp
@@ -1487,6 +1487,15 @@ wxString &OCPNPlatform::GetPrivateDataDir()
         
 #ifdef __WXMSW__
         m_PrivateDataDir = GetHomeDir();                     // should be {Documents and Settings}\......
+#elif defined FLATPAK
+        // ~/.var/app/org.opencpn.OpenCPN/config
+        const char* config_home = getenv("XDG_CONFIG_HOME");
+        if (!config_home {
+            config_home = std::string(getenv("HOME"))
+                + "/.var/app/org.opencpn.OpenCPN/config";
+        }
+        m_PrivateDataDir = std::string(config_home) + "/opencpn";
+
 #elif defined __WXOSX__
         m_PrivateDataDir = std_path.GetUserConfigDir();     // should be ~/Library/Preferences
         appendOSDirSlash(&m_PrivateDataDir);

commit 1885c8f1fdfc7c523a38c94d54cc33606c00e50e

Flatpak: fix FTBS caused after 5b74a16bf.
My bad: I checked the regular build instead of the flatpak one
before making the PR. This should clean up the situation.

src/OCPNPlatform.cpp
@@ -1488,13 +1488,15 @@ wxString &OCPNPlatform::GetPrivateDataDir()
 #ifdef __WXMSW__
         m_PrivateDataDir = GetHomeDir();                     // should be {Documents and Settings}\......
 #elif defined FLATPAK
-        // ~/.var/app/org.opencpn.OpenCPN/config
-        const char* config_home = getenv("XDG_CONFIG_HOME");
-        if (!config_home {
-            config_home = std::string(getenv("HOME"))
-                + "/.var/app/org.opencpn.OpenCPN/config";
+        std::string config_home;
+        if (getenv("XDG_CONFIG_HOME")) {
+            config_home = getenv("XDG_CONFIG_HOME");
         }
-        m_PrivateDataDir = std::string(config_home) + "/opencpn";
+        else {
+          config_home = getenv("HOME");
+          config_home += "/.var/app/org.opencpn.OpenCPN/config";
+        }
+        m_PrivateDataDir = config_home + "/opencpn";
 
 #elif defined __WXOSX__
         m_PrivateDataDir = std_path.GetUserConfigDir();     // should be ~/Library/Preferences