f158c1fcfb6df9f081dc5eac71b0800f2496ea47
dsr <bdbcat@yahoo.com>
2020-04-20 23:49:01 -0400

    Correct FTBS on gnu gcc 7.5.0

--- libs/glu/libutil/error.c
+++ libs/glu/libutil/error.c
@@ -57,7 +57,7 @@ static const struct token_string Errors[] = {
    { GLU_OUT_OF_MEMORY, "out of memory" },
    { GLU_INCOMPATIBLE_GL_VERSION, "incompatible gl version" },
    { GLU_INVALID_OPERATION, "invalid operation" },
-   { ~0, NULL } /* end of list indicator */
+   { ~0, 0 } /* end of list indicator */
 };
 
 
