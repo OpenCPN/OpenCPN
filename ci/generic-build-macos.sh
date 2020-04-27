9461cff637f99555c521a33d44f7f60bb95392c4
dsr <bdbcat@yahoo.com>
2020-04-26 20:10:35 -0400

    Correct missing MacOS library "pixman", #1

--- ci/generic-build-macos.sh
+++ ci/generic-build-macos.sh
@@ -30,7 +30,7 @@ if [ -n "$CI" ]; then
 fi
 
 set -o pipefail
-for pkg in cairo cmake libexif python3 wget xz; do
+for pkg in pixman cairo cmake libexif python3 wget xz; do
     brew list $pkg 2>/dev/null | head -10 || brew install $pkg
 done
 
