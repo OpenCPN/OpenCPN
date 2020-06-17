05cc6107613b7838d53719ea4e08ea5ddaea9031
dsr <bdbcat@yahoo.com>
2020-06-17 11:28:26 -0400

    Correctly update MUIBar chart scale report on initial startup.

--- src/chcanv.cpp
+++ src/chcanv.cpp
@@ -6661,6 +6661,7 @@ void ChartCanvas::CreateMUIBar()
     if(m_muiBar){
         SetMUIBarPosition();
         UpdateFollowButtonState();
+        m_muiBar->UpdateDynamicValues();
         m_muiBar->SetCanvasENCAvailable( m_bENCGroup );
         m_muiBar->Raise();
     }
