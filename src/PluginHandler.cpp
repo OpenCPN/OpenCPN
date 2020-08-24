a02334132f6aa8d1022c2387b1fe44843d9a9690
dsr <bdbcat@yahoo.com>
2020-08-23 21:33:00 -0400

    Harden archive explode error code logic.

--- src/PluginHandler.cpp
+++ src/PluginHandler.cpp
@@ -374,10 +374,15 @@ static int copy_data(struct archive* ar, struct archive* aw)
 
     while (true) {
         r = archive_read_data_block(ar, &buff, &size, &offset);
-        if (r == ARCHIVE_EOF) return (ARCHIVE_OK);
-        if (r < ARCHIVE_OK) return (r);
+        if (r == ARCHIVE_EOF)
+            return (ARCHIVE_OK);
+        if (r < ARCHIVE_OK){
+            std::string s(archive_error_string(ar));
+            return (r);
+        }
         r = archive_write_data_block(aw, buff, size, offset);
         if (r < ARCHIVE_OK) {
+            std::string s(archive_error_string(aw));
             wxLogWarning("Error copying install data: %s",
                          archive_error_string(aw));
             return (r);
@@ -656,9 +661,9 @@ bool PluginHandler::archive_check(int r, const char* msg, struct archive* a)
 {
     if (r < ARCHIVE_OK) {
         std::string s(msg);
-#ifndef __OCPN__ANDROID__
-        s = s + ": " + archive_error_string(a);
-#endif
+        
+        if(archive_error_string(a))
+            s = s + ": " + archive_error_string(a);
         wxLogMessage(s.c_str());
         last_error_msg = s;
     }
