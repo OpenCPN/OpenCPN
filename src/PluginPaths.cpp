16de2c7b7a0afb67ac3f3a39c5a227b237fa4815
dropenmind <74968848+dropenmind@users.noreply.github.com>
2020-11-28 20:09:53 +0100

    Update PluginPaths.cpp

--- src/PluginPaths.cpp
+++ src/PluginPaths.cpp
@@ -214,7 +214,7 @@ PluginPaths::PluginPaths()
     else if (g_Platform->isFlatpacked()) {
         initFlatpackPaths();
     }
-    else if (osSystemId & wxOS_UNIX_LINUX) {
+    else if (osSystemId & wxOS_UNIX_LINUX || osSystemId & wxOS_UNIX_FREEBSD) {
 #ifdef __OCPN__ANDROID__
         initAndroidPaths();
 #else
