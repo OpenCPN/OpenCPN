e85f76adb43d8c8bdfdfb9828e22f47f7130ed5a
dsr <bdbcat@yahoo.com>
2020-12-08 08:01:05 -0500

    Correct changed CDN build resource links.

--- ci/generic-build-macos.sh
+++ ci/generic-build-macos.sh
@@ -48,7 +48,8 @@ make
 make install
 cd ..
 
-wget -q http://opencpn.navnux.org/build_deps/wx312B_opencpn50_macos109.tar.xz
+# download build_deps/wx312B_opencpn50_macos109.tar.xz
+wget -q https://download.opencpn.org/s/rwoCNGzx6G34tbC/download
 tar xJf wx312B_opencpn50_macos109.tar.xz -C /tmp
 export PATH="/usr/local/opt/gettext/bin:$PATH"
 echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.bash_profile
