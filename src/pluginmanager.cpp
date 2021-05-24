0f40f76853927a183d85d6d00be2a74b34cfccdc
dsr <bdbcat@yahoo.com>
2021-05-24 00:04:56 -0400

    Allow time for plugin unload to process, Windows.

--- src/pluginmanager.cpp
+++ src/pluginmanager.cpp
@@ -1887,7 +1887,12 @@ bool PlugInManager::UnLoadPlugIn(size_t ix)
     if(pic->m_pplugin){
         pic->m_destroy_fn(pic->m_pplugin);
     }
-   
+
+#ifdef __WXMSW__
+    wxSleep(1);
+    pic->m_library.Unload();
+    wxSleep(1);
+#endif    
     delete pic;            // This will unload the PlugIn via DTOR of pic->m_library
     plugin_array.RemoveAt(ix);
     return true;
