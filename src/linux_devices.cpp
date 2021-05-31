6ffaf1ffc75a1b5c1910827f561d2db1bdf4d301
Alec Leamas <leamas.alec@nowhere.nt>
2021-05-31 19:52:39 +0200

    Implement is_dongle_permissions_wrong() and is_device_pemissions_ok()

--- src/linux_devices.cpp
+++ src/linux_devices.cpp
@@ -22,9 +22,90 @@
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
  **************************************************************************/
+#include "config.h"
 
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#include <wx/log.h>
+
+#include "logger.h"
 #include "linux_devices.h"
 
-bool is_dongle_permissions_wrong() { return true; }
-bool is_device_permissions_ok(const char* dev_name) { return true; }
+#ifndef HAVE_LIBUSB_10
 
+bool is_dongle_permissions_wrong() { return false; }
+
+#else
+
+#include <libusb-1.0/libusb.h>
+
+static const int dongle_vendor = 0x1547;
+static const int dongle_product = 0x1000;
+
+
+static int try_open(int vendorId, int productId)
+{
+    libusb_context* ctx = 0;
+    int r = libusb_init(&ctx);
+    if (r != 0) {
+        WARNING_LOG << "Cannot initialize libusb: " << libusb_strerror(r);
+        return LIBUSB_ERROR_NOT_SUPPORTED;
+    }
+    libusb_device** device_list;
+    ssize_t size = libusb_get_device_list(ctx, &device_list);
+    if  (size < 0) {
+        DEBUG_LOG << "Cannot get usb devices list: " << libusb_strerror(size);
+        return LIBUSB_ERROR_NOT_SUPPORTED;
+    }
+    r = LIBUSB_ERROR_INVALID_PARAM;
+    for (auto dev = device_list; *dev; dev++)  {
+        struct libusb_device_descriptor desc;
+        libusb_get_device_descriptor(*dev, &desc);
+        if (desc.idVendor != vendorId || desc.idProduct != productId) {
+            continue;
+        }
+        libusb_device_handle* dev_handle;
+        r =  libusb_open(*dev, &dev_handle);
+        if (r >= 0) {
+            libusb_close(dev_handle);
+        }
+        break;
+    }
+    libusb_free_device_list(device_list, 1);
+    DEBUG_LOG << "Nothing found for " << vendorId << ":" << productId;
+    libusb_exit(0);
+    return r;
+}
+
+
+bool is_dongle_permissions_wrong()
+{
+    int rc = try_open(dongle_vendor, dongle_product);
+    DEBUG_LOG << "Probing dongle permissions, result: " << rc;
+    return rc == LIBUSB_ERROR_ACCESS;
+}
+
+#endif
+
+#ifdef HAVE_UNISTD_H
+
+bool is_device_permissions_ok(const char* path) {
+    int r = access(path, R_OK & W_OK);
+    if (r < 0) {
+        INFO_LOG << "access(3) fails on: " << path << ": " << strerror(errno);
+        return true;
+    }
+    return r == 0;
+}
+
+#else
+
+bool is_device_permissions_ok(const char* path)
+{
+    WARNING_LOG << "Invoking linux-only function in non-linux context";
+    return true;
+}
+
+#endif
