1ff0766cb6ae4bc33f48ea621a1932f674b6e0e0
Alec Leamas <leamas.alec@nowhere.nt>
2021-08-05 09:23:20 +0200

    .cmake, .txt files: Remove trailing whitespace.

--- buildwin/Toolchain-mingw32.cmake
+++ buildwin/Toolchain-mingw32.cmake
@@ -12,7 +12,7 @@ SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32 /usr/local/i686-w64-mingw32 )
 SET(CMAKE_INSTALL_PREFIX /usr/local/i686-w64-mingw32)
 
 # adjust the default behaviour of the FIND_XXX() commands:
-# search headers and libraries in the target environment, search 
+# search headers and libraries in the target environment, search
 # programs in the host environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
