b3b056f635a1114a06b9004c398f978554f47eda
dsr <bdbcat@yahoo.com>
2021-10-09 17:43:15 -0400

    Various, fix TLS #6

--- ci/generic-build-macos.sh
+++ ci/generic-build-macos.sh
@@ -18,10 +18,10 @@ sudo ln -s ${HOME}/project/opt_local_cache /opt/local
 ls ${HOME}/project/opt_local_cache || echo "OK"
 ls ${HOME}/project/opt_local_cache/bin || echo "OK"
 
-sudo mkdir -p /opt/local/share/curl
-sudo cp buildosx/cacert.pem /opt/local/share/curl/curl-ca-bundle.crt
-sudo mkdir -p /opt/local/etc/openssl
-sudo ln -s /opt/local/share/curl/curl-ca-bundle.crt /opt/local/etc/openssl/cert.pem
+#sudo mkdir -p /opt/local/share/curl
+#sudo cp buildosx/cacert.pem /opt/local/share/curl/curl-ca-bundle.crt
+#sudo mkdir -p /opt/local/etc/openssl
+#sudo ln -s /opt/local/share/curl/curl-ca-bundle.crt /opt/local/etc/openssl/cert.pem
 
 #openssl x509 -in /opt/local/share/curl/curl-ca-bundle.crt -out mycert.pem -outform PEM
 #sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "mycert.pem"
@@ -54,6 +54,26 @@ sudo port deactivate OCPN_curl || {
 
 # Install curl to get the TLS certificate bundle
 # then immediately deactivate curl to make room for OCPN_curl later
+sudo port -q install curl
+sudo port deactivate curl
+
+sudo port deactivate openssl
+
+
+# install the local port libraries
+#  n.b.  ORDER IS IMPORTANT
+
+sudo port -q install OCPN_openssl
+sudo port -q install OCPN_curl
+sudo port -q install OCPN_libpixman
+sudo port -q install OCPN_cairo
+sudo port -q install zstd
+sudo port -q install OCPN_libarchive
+
+sudo port -q -f install OCPN_libpng
+
+# Install curl to get the TLS certificate bundle
+# then immediately deactivate curl to make room for OCPN_curl later
 #sudo port -q install curl
 #sudo port -N deactivate curl
 
@@ -65,18 +85,18 @@ sudo port deactivate OCPN_curl || {
 # install the local port libraries
 #  n.b.  ORDER IS IMPORTANT
 
-sudo port -q install OCPN_openssl
-sudo port -q install OCPN_cairo
+#sudo port -q install OCPN_openssl
+#sudo port -q install OCPN_cairo
 
-sudo port -N deactivate libpixman
-sudo port -q install OCPN_libpixman
+#sudo port -N deactivate libpixman
+#sudo port -q install OCPN_libpixman
 
-sudo port -q install zstd
-sudo port -q install OCPN_libarchive
-sudo port -q -f install OCPN_libpng
+#sudo port -q install zstd
+#sudo port -q install OCPN_libarchive
+#sudo port -q -f install OCPN_libpng
 
-sudo port -N deactivate curl
-sudo port -q install OCPN_curl
+#sudo port -N deactivate curl
+#sudo port -q install OCPN_curl
 
 # Return latest installed brew version of given package
 pkg_version() { brew list --versions $2 $1 | tail -1 | awk '{print $2}'; }
