90654748e5553721723973cdde192f78f6f4c8e2
Alec Leamas <leamas.alec@nowhere.net>
2022-02-04 00:24:38 +0100

    PluginHandler: Drop support for Xenial/16.04

--- src/PluginHandler.cpp
+++ src/PluginHandler.cpp
@@ -194,7 +194,6 @@ public:
   // Debian version. To be removed, see  #2512.
   bool is_ubuntu_plugin_compatible(const Plugin& plugin) const {
     static const std::vector<std::string> debian_versions = {
-        "9;ubuntu-x86_64;16.04",
         // Assuming Debian 10 users sticks to gtk2:
         "10;ubuntu-x86_64;18.04",
         "11;ubuntu-gtk3-x86_64;20.04",
@@ -217,7 +216,6 @@ public:
   bool is_debian_plugin_compatible(const Plugin& plugin) const {
     if (!ocpn::startswith(m_abi, "ubuntu")) return false;
     static const std::vector<std::string> compat_versions = {
-        "9;ubuntu-x86_64;16.04",
         // Assuming Debian 10 users sticks to gtk2:
         "10;ubuntu-x86_64;18.04",
         "11;ubuntu-gtk3-x86_64;20.04",
