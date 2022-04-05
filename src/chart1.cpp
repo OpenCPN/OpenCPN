568151048ef6c7dbf060550b1236bd04c00a358a
dsr <bdbcat@yahoo.com>
2022-04-05 19:43:14 -0400

    Correct Status bar SOG/COG report logic.  #2601

--- src/chart1.cpp
+++ src/chart1.cpp
@@ -9118,27 +9118,24 @@ void MyFrame::PostProcessNMEA(bool pos_valid, bool sog_valid,
     }
 
     wxString sogcog;
-    if(sog_valid){
-      if (!std::isnan(gSog))
-        sogcog.Printf(_T("SOG %2.2f ") + getUsrSpeedUnit() + _T("  "),
-                    toUsrSpeed(gSog));
-      else
-        sogcog.Printf(_T("SOG --- ")) ;
-    }
+    if (!std::isnan(gSog))
+      sogcog.Printf(_T("SOG %2.2f ") + getUsrSpeedUnit() + _T("  "),
+                  toUsrSpeed(gSog));
+    else
+      sogcog.Printf(_T("SOG --- ")) ;
 
     wxString cogs;
-    if (cog_valid){
       // We show COG only if SOG is > 0
-      if (!std::isnan(gCog) && !std::isnan(gSog) && (gSog > 0)){
-        if (g_bShowTrue)
-          cogs << wxString::Format(wxString("COG %03d%c  "), (int)gCog, 0x00B0);
-        if (g_bShowMag)
-          cogs << wxString::Format(wxString("COG %03d%c(M)  "),
-                                 (int)GetMag(gCog), 0x00B0);
-        }
-      else
-        cogs.Printf(("COG ---%c"), 0x00B0);
-    }
+    if (!std::isnan(gCog) && !std::isnan(gSog) && (gSog > 0)){
+      if (g_bShowTrue)
+        cogs << wxString::Format(wxString("COG %03d%c  "), (int)gCog, 0x00B0);
+      if (g_bShowMag)
+        cogs << wxString::Format(wxString("COG %03d%c(M)  "),
+                               (int)GetMag(gCog), 0x00B0);
+      }
+    else
+      cogs.Printf(("COG ---%c"), 0x00B0);
+
 
     sogcog.Append(cogs);
     SetStatusText(sogcog, STAT_FIELD_SOGCOG);
