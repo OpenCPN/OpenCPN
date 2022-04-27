cb07dd70d0ed0d6367fe04e1d3a9d412e4ed7885
Pavel Kalian <pavel@kalian.cz>
2022-04-27 07:07:15 -0300

    Revert "Fix CMake compiler extension checking to be more reliable."
    This reverts commit 347e4ea272d457853e182ee8ddf2c44fa16d810d.

--- cmake/CompilerSupport.cmake
+++ cmake/CompilerSupport.cmake
@@ -2,10 +2,50 @@
 # Check for supported compiler flags
 #
 
-include(CheckSymbolExists)
+if (_COMPILER_FLAGS_PROBED)
+  return ()
+endif ()
+set(_COMPILER_FLAGS_PROBED 1 CACHE INTERNAL "Multiple run guard")
 
-check_symbol_exists(__SSE__ xmmintrin.h HAVE_MSSE)
-check_symbol_exists(__SSE2__ emmintrin.h HAVE_MSSE2)
-check_symbol_exists(__SSE3__ pmmintrin.h HAVE_MSSE3)
-check_symbol_exists(__AVX2__ immintrin.h HAVE_MAVX2)
-check_symbol_exists(__ARM_NEON arm_neon.h HAVE_MFPU_NEON)
+function (print_ok_fail flag)
+  if (${flag})
+      message(STATUS "OK")
+  else ()
+      message(STATUS "Fail")
+  endif ()
+endfunction (print_ok_fail)
+
+function (print_no_nl message)
+  execute_process(COMMAND ${CMAKE_COMMAND} -E echo_append "--" ${message})
+endfunction (print_no_nl)
+
+print_no_nl("Probing for compiler -msse support ")
+try_compile(HAVE_MSSE ${CMAKE_BINARY_DIR}/msse
+            SOURCES  ${CMAKE_SOURCE_DIR}/cmake/dummy.c
+            COMPILE_DEFINITIONS -msse)
+print_ok_fail(${HAVE_MSSE})
+
+print_no_nl("Probing for compiler -msse2 support ")
+try_compile(HAVE_MSSE2 ${CMAKE_BINARY_DIR}/msse2
+            SOURCES  ${CMAKE_SOURCE_DIR}/cmake/dummy.c
+            COMPILE_DEFINITIONS -msse2)
+print_ok_fail(${HAVE_MSSE2})
+
+print_no_nl("Probing for compiler -msse3 support ")
+try_compile(HAVE_MSSE3 ${CMAKE_BINARY_DIR}/msse3
+            SOURCES  ${CMAKE_SOURCE_DIR}/cmake/dummy.c
+            COMPILE_DEFINITIONS -msse3)
+print_ok_fail(${HAVE_MSSE3})
+
+print_no_nl("Probing for compiler -mavx2 support ")
+try_compile(HAVE_MAVX2 ${CMAKE_BINARY_DIR}/mavx2
+            SOURCES  ${CMAKE_SOURCE_DIR}/cmake/dummy.c
+            COMPILE_DEFINITIONS -mavx2)
+print_ok_fail(${HAVE_MAVX2})
+
+print_no_nl("Probing for compiler -mfpu=neon support ")
+try_compile(HAVE_MFPU_NEON ${CMAKE_BINARY_DIR}/fpu_neon
+            SOURCES  ${CMAKE_SOURCE_DIR}/cmake/dummy.c
+            COMPILE_DEFINITIONS -mfpu=neon)
+print_ok_fail(${HAVE_MFPU_NEON})
+
