47cccb014a1d5627fd5feca3dc809d7466fe1fef
Alec Leamas <leamas.alec@nowhere.net>
2022-04-28 21:24:08 +0200

    ci: flatpak: New deployment host.

--- ci/circleci-build-flatpak.sh
+++ ci/circleci-build-flatpak.sh
@@ -47,8 +47,9 @@ flatpak --user install --noninteractive org.freedesktop.Platform//$runtime
 flatpak --user install --noninteractive org.freedesktop.Sdk//$runtime
 
 cd flatpak
+
 # By default, script packages master branch from main github repo, as a
-# proper packagoing branch should do. Setting FP_BUILD_ORIGINAL_BRANCH
+# proper packaging branch should do. Setting FP_BUILD_ORIGINAL_BRANCH
 # makes it build same branch as the script instead, for testing.
 if [ -z "$FP_BUILD_ORIGINAL_BRANCH" ]; then
     sed -i -e '/url:/s|\.\.|https://github.com/OpenCPN/OpenCPN.git|' \
@@ -59,10 +60,7 @@ fi
 # The build heavy lifting
 test -d ../build || mkdir ../build
 cd ../build
-flatpak-builder --repo=repo --force-clean --default-branch=devel \
-    --state-dir=../cache  --delete-build-dirs \
-    app ../flatpak/org.opencpn.OpenCPN.yaml
-flatpak install -y --user --reinstall repo org.opencpn.OpenCPN
+make -f ../flatpak/Makefile build
 flatpak list
 
 # Decrypt and unpack gpg keys, sign and install into website/
@@ -73,42 +71,25 @@ make -f ../flatpak/Makefile install
 make GPG_HOMEDIR=opencpn-gpg -f ../flatpak/Makefile sign
 rm -rf gpg.tar.gz opencpn-gpg
 
+# Deploy website/ to deployment server.
+cp ../ci/id_opencpn.tar.cpt .
+ccdecrypt --envvar FLATPAK_KEY id_opencpn.tar.cpt
+tar -xf id_opencpn.tar
+chmod 600 .ssh/id_opencpn
+rsync -a --info=stats --delete-after \
+    --rsh="ssh -o 'StrictHostKeyChecking no' -i .ssh/id_opencpn" \
+    website/  opencpnci@ocpnci.kalian.cz:web/flatpak-repo
+rm -f .ssh/id_opencpn*
+
+
+# Restore the patched file so the caching works.
+git checkout ../flatpak/org.opencpn.OpenCPN.yaml
+
 # Debug: show version in local repo.
 flatpak remote-add  \
     --user --gpg-import=website/opencpn.key local $PWD/website/repo
 flatpak update --appstream local
 flatpak remote-ls local
-
-# Deploy website/ to deployment servers.
-cp ../ci/id_opencpn.tar.cpt .
-ccdecrypt --envvar FLATPAK_KEY id_opencpn.tar.cpt
-tar -xf id_opencpn.tar
-chmod 600 .ssh/id_opencpn
-
-# Old, published channel
-rsync -a --info=stats --delete-after \
-    --rsh="ssh -o 'StrictHostKeyChecking no' -i .ssh/id_opencpn" \
-    website/ opencpn@mumin.crabdance.com:/var/www/ocpn-flatpak/website
-
-# Seed the two masters in the opencpn cloud with new build
-if [ -n "$OCPN_CLOUD_TEST" ]; then
-    rsync -a --info=stats --delete-after \
-        --rsh="ssh -o 'StrictHostKeyChecking no' -i .ssh/id_opencpn" \
-        website/ rsync@mumin.crabdance.com:/home/rsync/flatpak/website
-    rsync -a --info=stats --delete-after \
-        --rsh="ssh -p 2222 -o 'StrictHostKeyChecking no' -i .ssh/id_opencpn" \
-        website/ rsync@gafsan.crabdance.com:/home/rsync/flatpak/website
-
-    rm -f .ssh/id_opencpn*
-fi
-
-# Restore the patched file so the caching works.
-git checkout ../flatpak/org.opencpn.OpenCPN.yaml
-
-# Debug: show version in remote repo.
-flatpak remote-add --user --no-gpg-verify opencpn $PWD/website/repo
-flatpak update --appstream opencpn
-flatpak remote-ls opencpn
 
 # Validate the appstream data:
 appstreamcli validate app/files/share/appdata/org.opencpn.OpenCPN.appdata.xml || :
