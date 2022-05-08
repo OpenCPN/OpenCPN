6f1ad588010a8669a2ca627c8438c346819e87cb
Owen Anderson <resistor@mac.com>
2022-05-07 23:56:48 -0700

    Check for the compiler flag -mfpu=neon in addition to check for headers.

--- cmake/CompilerSupport.cmake
+++ cmake/CompilerSupport.cmake
@@ -3,9 +3,13 @@
 #
 
 include(CheckSymbolExists)
+include(CheckCCompilerFlag)
 
 check_symbol_exists(__SSE__ xmmintrin.h HAVE_MSSE)
 check_symbol_exists(__SSE2__ emmintrin.h HAVE_MSSE2)
 check_symbol_exists(__SSE3__ pmmintrin.h HAVE_MSSE3)
 check_symbol_exists(__AVX2__ immintrin.h HAVE_MAVX2)
-check_symbol_exists(__ARM_NEON arm_neon.h HAVE_MFPU_NEON)
+check_symbol_exists(__ARM_NEON arm_neon.h HAVE_ARM_NEON)
+if (HAVE_ARM_NEON)
+    check_c_compiler_flag(-mfpu=neon HAVE_FPU_NEON)
+endif()
