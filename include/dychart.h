367f914780f80a1aef81c810dc98dd09b9f13dd7
dsr <bdbcat@yahoo.com>
2022-05-11 11:15:46 -0400

    Revise GL header inclusion rules

--- include/dychart.h
+++ include/dychart.h
@@ -177,6 +177,34 @@
  #include <GL/glu.h>
 #endif
 
+/*
+#if defined(_WIN32) || defined(_WIN64)
+#  include <gl/glew.h>
+#  include <GL/gl.h>
+#  include <GL/glu.h>
+#elif __APPLE__
+#  include "TargetConditionals.h"
+#  if (TARGET_OS_IPHONE && TARGET_IPHONE_SIMULATOR) || TARGET_OS_IPHONE
+#    include <OpenGLES/ES2/gl.h>
+#    include <OpenGLES/ES2/glext.h>
+#  else
+#    include <OpenGL/gl.h>
+#    include <OpenGL/glu.h>
+#    include <OpenGL/glext.h>
+#  endif
+#elif defined(__ANDROID__) || defined(ANDROID)
+#  include <GLES2/gl2.h>
+#  include <GLES2/gl2ext.h>
+#elif defined(__linux__) || defined(__unix__) || defined(__posix__)
+#  include <GL/gl.h>
+#  include <GL/glu.h>
+#  include <GL/glext.h>
+#else
+#  error platform not supported.
+#endif
+*/
+
+
 #ifdef __OCPN__ANDROID__
 #include "qdebug.h"
 #endif
