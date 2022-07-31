2ee31d46ed5ed3aa795f7086d6ae0dfd6e9fbdb9
Alec Leamas <leamas.alec@nowhere.net>
2022-07-31 22:30:46 +0200

    test: refactor first test.

--- test/test1.cpp
+++ test/test1.cpp
@@ -10,49 +10,44 @@ wxDEFINE_EVENT(EVT_BAR, wxCommandEvent);
 
 std::string s_result;
 
-class MsgSink:  public wxEvtHandler {
-private:
-  ObservedVarListener listener;
+class MsgCliApp : public wxAppConsole {
 public:
-  MsgSink() {
-    ObservableMsg observable("1234");
-    listener = observable.get_listener(this, EVT_BAR);
-    Bind(EVT_BAR, [&](wxCommandEvent ev) {
-      auto msg = get_navmsg_ptr(ev);
-      auto n2000_msg = std::dynamic_pointer_cast<const Nmea2000Msg>(msg);
-      std::string s(n2000_msg->payload.begin(), n2000_msg->payload.end());
-      s_result = s;
-    });
-  }
-};
+  class Sink:  public wxEvtHandler {
+  private:
+    ObservedVarListener listener;
+  public:
+    Sink() {
+      ObservableMsg observable("1234");
+      listener = observable.get_listener(this, EVT_BAR);
+      Bind(EVT_BAR, [&](wxCommandEvent ev) {
+        auto msg = get_navmsg_ptr(ev);
+        auto n2000_msg = std::dynamic_pointer_cast<const Nmea2000Msg>(msg);
+        std::string s(n2000_msg->payload.begin(), n2000_msg->payload.end());
+        s_result = s;
+      });
+    }
+  };
+  
+  class Source {
+  public:
+    Source() {
+      std::string s("payload data");
+      auto payload = std::vector<unsigned char>(s.begin(), s.end());
+      auto id = static_cast<uint64_t>(1234);
+      auto n2k_msg = std::make_shared<const Nmea2000Msg>(id, payload);
+      ObservableMsg observable("1234");
+      observable.notify(n2k_msg);
+    }
+  };
 
-
-class MsgSource {
-public:
-  MsgSource(wxEvtHandler& sink) {
-    std::string s("payload data");
-    auto payload = std::vector<unsigned char>(s.begin(), s.end());
-    auto id = static_cast<uint64_t>(1234);
-    auto n2k_msg = std::make_shared<const Nmea2000Msg>(id, payload);
-    ObservableMsg observable("1234");
-    observable.notify(n2k_msg);
-  }
-};
-
-class CliApp1 : public wxAppConsole {
-public:
-  CliApp1() : wxAppConsole() {
-    CheckBuildOptions(WX_BUILD_OPTIONS_SIGNATURE, "program");
-    SetAppName("opencpn");
-    MsgSink sink;
-    MsgSource source(sink);
+  MsgCliApp() : wxAppConsole() {
+    Sink sink;
+    Source source;
     ProcessPendingEvents();
   }
 };
 
-
 TEST(Messaging, ObservableMsg) {
-  CliApp1 app;
+  MsgCliApp app;
   EXPECT_STREQ(s_result.c_str(), "payload data");
 }
- 
