096611eef6defa422380e8bfed12b425dc4d5756
Alec Leamas <leamas.alec@nowhere.net>
2022-08-16 23:58:37 +0200

    observable: Remove useless implementation files.

--- src/observable_msg.cpp
+++
@@ -1,7 +0,0 @@
-#include "observable_msg.h"
-
-/* ObservableMsg implementation */
-
-void ObservableMsg::notify(std::shared_ptr<const NavMsg> msg) {
-  ObservedVar::notify(std::dynamic_pointer_cast<const void>(msg));
-}
