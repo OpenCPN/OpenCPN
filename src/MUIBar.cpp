0fb1f3870b8adfb696a7ae9701656b782dd3e8be
dsr <bdbcat@yahoo.com>
2022-10-07 22:40:12 -0400

    Correct top-level-window management, Windows.

--- src/MUIBar.cpp
+++ src/MUIBar.cpp
@@ -536,7 +536,11 @@ MUIBar::MUIBar(ChartCanvas* parent, int orientation, float size_factor,
   // wxWindow::Create(parent, id, pos, size, style, name);
   // long mstyle = wxSIMPLE_BORDER;
   long mstyle = wxNO_BORDER | wxFRAME_NO_TASKBAR | wxFRAME_SHAPED |
-                wxFRAME_FLOAT_ON_PARENT | wxFRAME_TOOL_WINDOW;
+                wxFRAME_FLOAT_ON_PARENT;
+
+#ifdef __WXOSX__
+  mstyle |= wxFRAME_TOOL_WINDOW;
+#endif
 
   m_scaleFactor = size_factor;
   m_cs = (ColorScheme)-1;
