bdd175e6256d222f5ba3a882b64ead15477dfd54
dsr <bdbcat@yahoo.com>
2022-11-08 10:28:12 -0500

    Change "python3.6" to "python36"

--- ci/appveyor-upload.sh
+++ ci/appveyor-upload.sh
@@ -6,8 +6,8 @@ if [ -z "$CLOUDSMITH_API_KEY" ]; then
     echo 'Cannot deploy to cloudsmith: missing $CLOUDSMITH_API_KEY'
 else
     echo 'Deploying to cloudsmith'
-    python3.6 -m ensurepip
-    python3.6 -m pip install cloudsmith-cli
+    python36 -m ensurepip
+    python36 -m pip install cloudsmith-cli
     sed -i '/#!/s|/usr|/c/cygwin|' /c/cygwin/bin/cloudsmith
     export LC_ALL=C.UTF-8
     export LANG=C.UTF-8
