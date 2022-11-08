dd4f04f8103566196351689ce63ea9b87be5dbf5
dsr <bdbcat@yahoo.com>
2022-11-08 12:07:21 -0500

    Try default python installation

--- ci/appveyor-upload.sh
+++ ci/appveyor-upload.sh
@@ -6,8 +6,8 @@ if [ -z "$CLOUDSMITH_API_KEY" ]; then
     echo 'Cannot deploy to cloudsmith: missing $CLOUDSMITH_API_KEY'
 else
     echo 'Deploying to cloudsmith'
-    C:/Python36/python -m ensurepip
-    C:/Python36/python -m pip install cloudsmith-cli
+    python -m ensurepip
+    python -m pip install cloudsmith-cli
     sed -i '/#!/s|/usr|/c/cygwin|' /c/cygwin/bin/cloudsmith
     export LC_ALL=C.UTF-8
     export LANG=C.UTF-8
