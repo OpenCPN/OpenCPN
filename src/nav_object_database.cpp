6ff5bf4abef80fce30414f8160361ef237723a59
dsr <bdbcat@yahoo.com>
2022-12-17 23:58:32 -0500

    Refactor navobj XML file handling.

--- src/nav_object_database.cpp
+++ src/nav_object_database.cpp
@@ -1294,7 +1294,11 @@ bool NavObjectCollection1::CreateNavObjGPXPoints(void) {
     pr = node->GetData();
 
     if ((pr->m_bIsolatedMark) && !(pr->m_bIsInLayer) && !(pr->m_btemp)) {
-      GPXCreateWpt(m_gpx_root.append_child("wpt"), pr, OPT_WPT);
+      pugi::xml_node doc = root();
+      pugi::xml_node gpx = doc.first_child();
+      pugi::xml_node new_node = gpx.append_child("wpt");
+
+      GPXCreateWpt(new_node, pr, OPT_WPT);
     }
     node = node->GetNext();
   }
@@ -1310,8 +1314,14 @@ bool NavObjectCollection1::CreateNavObjGPXRoutes(void) {
   while (node1) {
     Route *pRoute = node1->GetData();
 
-    if (!pRoute->m_bIsInLayer && !pRoute->m_btemp)
-      GPXCreateRoute(m_gpx_root.append_child("rte"), pRoute);
+    if (!pRoute->m_bIsInLayer && !pRoute->m_btemp){
+      pugi::xml_node doc = root();
+      pugi::xml_node gpx = doc.first_child();
+      pugi::xml_node new_node = gpx.append_child("rte");
+
+      GPXCreateRoute(new_node, pRoute);
+    }
+
     node1 = node1->GetNext();
   }
 
@@ -1322,8 +1332,13 @@ bool NavObjectCollection1::CreateNavObjGPXTracks(void) {
   // Tracks
   for (Track *pTrack : g_TrackList) {
     if (pTrack->GetnPoints()) {
-      if (!pTrack->m_bIsInLayer && !pTrack->m_btemp)
-        GPXCreateTrk(m_gpx_root.append_child("trk"), pTrack, 0);
+      if (!pTrack->m_bIsInLayer && !pTrack->m_btemp){
+        pugi::xml_node doc = root();
+        pugi::xml_node gpx = doc.first_child();
+        pugi::xml_node new_node = gpx.append_child("rte");
+
+        GPXCreateTrk(new_node, pTrack, 0);
+      }
     }
   }
 
@@ -1342,19 +1357,19 @@ bool NavObjectCollection1::CreateAllGPXObjects() {
 
 bool NavObjectCollection1::AddGPXRoute(Route *pRoute) {
   SetRootGPXNode();
-  GPXCreateRoute(m_gpx_root.append_child("rte"), pRoute);
+  GPXCreateRoute(root().append_child("rte"), pRoute);
   return true;
 }
 
 bool NavObjectCollection1::AddGPXTrack(Track *pTrk) {
   SetRootGPXNode();
-  GPXCreateTrk(m_gpx_root.append_child("trk"), pTrk, 0);
+  GPXCreateTrk(root().append_child("trk"), pTrk, 0);
   return true;
 }
 
 bool NavObjectCollection1::AddGPXWaypoint(RoutePoint *pWP) {
   SetRootGPXNode();
-  GPXCreateWpt(m_gpx_root.append_child("wpt"), pWP, OPT_WPT);
+  GPXCreateWpt(root().append_child("wpt"), pWP, OPT_WPT);
   return true;
 }
 
@@ -1391,21 +1406,21 @@ bool NavObjectCollection1::AddGPXPointsList(RoutePointList *pRoutePoints) {
 }
 
 void NavObjectCollection1::SetRootGPXNode(void) {
-  if (!strlen(m_gpx_root.name())) {
-    m_gpx_root = append_child("gpx");
-    m_gpx_root.append_attribute("version") = "1.1";
-    m_gpx_root.append_attribute("creator") = "OpenCPN";
-    m_gpx_root.append_attribute("xmlns:xsi") =
+  if (!strlen(root().name())) {
+    pugi::xml_node gpx_root = append_child("gpx");
+    gpx_root.append_attribute("version") = "1.1";
+    gpx_root.append_attribute("creator") = "OpenCPN";
+    gpx_root.append_attribute("xmlns:xsi") =
         "http://www.w3.org/2001/XMLSchema-instance";
-    m_gpx_root.append_attribute("xmlns") = "http://www.topografix.com/GPX/1/1";
-    m_gpx_root.append_attribute("xmlns:gpxx") =
+    gpx_root.append_attribute("xmlns") = "http://www.topografix.com/GPX/1/1";
+    gpx_root.append_attribute("xmlns:gpxx") =
         "http://www.garmin.com/xmlschemas/GpxExtensions/v3";
-    m_gpx_root.append_attribute("xsi:schemaLocation") =
+    gpx_root.append_attribute("xsi:schemaLocation") =
         "http://www.topografix.com/GPX/1/1 "
         "http://www.topografix.com/GPX/1/1/gpx.xsd "
         "http://www.garmin.com/xmlschemas/GpxExtensions/v3 "
         "http://www8.garmin.com/xmlschemas/GpxExtensionsv3.xsd";
-    m_gpx_root.append_attribute("xmlns:opencpn") = "http://www.opencpn.org";
+    gpx_root.append_attribute("xmlns:opencpn") = "http://www.opencpn.org";
   }
 }
 
@@ -1517,7 +1532,7 @@ NavObjectChanges::~NavObjectChanges() {
 void NavObjectChanges::AddRoute(Route *pr, const char *action) {
   SetRootGPXNode();
 
-  pugi::xml_node object = m_gpx_root.append_child("rte");
+  pugi::xml_node object = root().append_child("rte");
   GPXCreateRoute(object, pr);
 
   pugi::xml_node xchild = object.child("extensions");
@@ -1534,7 +1549,7 @@ void NavObjectChanges::AddRoute(Route *pr, const char *action) {
 void NavObjectChanges::AddTrack(Track *pr, const char *action) {
   SetRootGPXNode();
 
-  pugi::xml_node object = m_gpx_root.append_child("trk");
+  pugi::xml_node object = root().append_child("trk");
   GPXCreateTrk(object, pr, RT_OUT_NO_RTPTS);  // emit a void track, no waypoints
 
   pugi::xml_node xchild = object.child("extensions");
@@ -1550,7 +1565,7 @@ void NavObjectChanges::AddTrack(Track *pr, const char *action) {
 void NavObjectChanges::AddWP(RoutePoint *pWP, const char *action) {
   SetRootGPXNode();
 
-  pugi::xml_node object = m_gpx_root.append_child("wpt");
+  pugi::xml_node object = root().append_child("wpt");
   GPXCreateWpt(object, pWP, OPT_WPT);
 
   pugi::xml_node xchild = object.child("extensions");
@@ -1567,7 +1582,7 @@ void NavObjectChanges::AddTrackPoint(TrackPoint *pWP, const char *action,
                                      const wxString &parent_GUID) {
   SetRootGPXNode();
 
-  pugi::xml_node object = m_gpx_root.append_child("tkpt");
+  pugi::xml_node object = root().append_child("tkpt");
   GPXCreateTrkpt(object, pWP, OPT_TRACKPT);
 
   pugi::xml_node xchild = object.append_child("extensions");
