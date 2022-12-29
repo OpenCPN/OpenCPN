510bbcd5ce4ff1ae525512c3380b2778d5f24c00
dsr <bdbcat@yahoo.com>
2022-12-29 11:19:47 -0500

    Correct piano chart selector size logic for scaled displays (Retina)

--- src/piano.cpp
+++ src/piano.cpp
@@ -77,7 +77,6 @@ END_EVENT_TABLE()
 // Define a constructor
 Piano::Piano(ChartCanvas *parent) {
   m_parentCanvas = parent;
-  ;
 
   m_index_last = -1;
   m_iactive = -1;
@@ -359,7 +358,9 @@ void Piano::BuildGLTexture() {
 
 void Piano::DrawGL(int off) {
 #ifdef ocpnUSE_GL
-  unsigned int w = m_parentCanvas->GetClientSize().x, h = GetHeight(), endx = 0;
+  unsigned int w = m_parentCanvas->GetClientSize().x * m_parentCanvas->GetContentScaleFactor();
+  int h = GetHeight();
+  int endx = 0;
 
   if (m_tex_piano_height != h) BuildGLTexture();
 
@@ -685,7 +686,8 @@ wxString &Piano::GetStoredHash() { return m_hash; }
 
 void Piano::FormatKeys(void) {
   ocpnStyle::Style *style = g_StyleManager->GetCurrentStyle();
-  int width = m_parentCanvas->GetClientSize().x, height = GetHeight();
+  int width = m_parentCanvas->GetClientSize().x * m_parentCanvas->GetContentScaleFactor();
+  int height = GetHeight();
   width *= g_btouch ? 0.98f : 0.6f;
 
   int nKeys = m_key_array.size();
@@ -693,7 +695,7 @@ void Piano::FormatKeys(void) {
   if (nKeys) {
     if (!kw) kw = width / nKeys;
 
-    kw = wxMin(kw, (m_parentCanvas->GetClientSize().x * 3 / 4) / nKeys);
+    kw = wxMin(kw, (width * 3 / 4) / nKeys);
     kw = wxMax(kw, 6);
 
     //    Build the Key Regions
@@ -815,7 +817,7 @@ void Piano::ResetRollover(void) {
 }
 
 int Piano::GetHeight() {
-  int height = 22;  // default desktop value
+  int height = 22 * m_parentCanvas->GetContentScaleFactor();  // default desktop value
   if (g_btouch) {
     double size_mult = exp(g_GUIScaleFactor * 0.0953101798043);  // ln(1.1)
     height *= size_mult;
