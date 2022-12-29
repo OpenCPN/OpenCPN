e27413cbd47ea54eaef84671ce940b263e9ff9f7
dsr <bdbcat@yahoo.com>
2022-12-28 22:13:47 -0500

    Correct another FTBS

--- test/tests.cpp
+++ test/tests.cpp
@@ -502,7 +502,7 @@ public:
 
     auto base_ptr = message.get();
     auto n2k_msg = dynamic_cast<const Nmea2000Msg*>(base_ptr);
-    s_result3 = n2k_msg->name.to_string();
+    s_result3 = n2k_msg->PGN.to_string();
 
     stringstream ss;
     std::for_each(n2k_msg->payload.begin(), n2k_msg->payload.end(),
