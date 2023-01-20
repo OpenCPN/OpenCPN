c40c45e3eb0b065dc151c0eeb1e287cdd27b1572
dsr <bdbcat@yahoo.com>
2023-01-19 22:16:32 -0500

    Add local build of liblame to MacOS package

--- ci/generic-build-macos.sh
+++ ci/generic-build-macos.sh
@@ -47,12 +47,21 @@ make
 sudo make install
 cd ..
 
+curl -k -o lame-3.100.tar.gz https://sourceforge.net/projects/lame/files/latest/download/lame-3.100.tar.gz
+tar zxf lame-3.100.tar.gz
+cd lame-3.100
+export MACOSX_DEPLOYMENT_TARGET=10.13
+./configure --disable-dependency-tracking --disable-debug --prefix=/usr/local --enable-nasm
+make
+sudo make install
+cd ..
+
 
 brew install cairo
 brew install freetype
-brew install lame
+#brew install lame
 brew install lz4
-brew install mpg123
+#brew install mpg123
 brew install xz
 brew install zstd
 
