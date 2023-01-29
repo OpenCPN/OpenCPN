5617c404e11c263ec78fe49d3aeeae911ab92733
dsr <bdbcat@yahoo.com>
2023-01-28 22:53:18 -0500

    Correct "pencil" cursor size for Windows scaled display

--- src/chcanv.cpp
+++ src/chcanv.cpp
@@ -909,22 +909,13 @@ void ChartCanvas::RebuildCursors() {
   ocpnStyle::Style *style = g_StyleManager->GetCurrentStyle();
   double cursorScale = exp(g_GUIScaleFactor * (0.693 / 5.0));
 
-  // On MSW, custom cursors created from images are automatically scaled to maximum size of 32x32.
-  // On a high def display, this renders the "pencil" cursor too small to easily use.
-  // Detect this configuration, and avoid using "pencil" cursor in this case.
-  // TODO  Investigate alternative means of defining custom cursors on MSW
-  bool bUsePencil = true;
-#ifdef __WXMSW__
-  wxSize ds = g_Platform->getDisplaySize();
-  if (ds.x > 3000)        // somewhat arbitrary detection of hi-def display
-    bUsePencil = false;
-#endif
+  double pencilScale = 1.0 / g_Platform->GetDisplayDIPMult(gFrame);
 
   wxImage ICursorLeft = style->GetIcon(_T("left")).ConvertToImage();
   wxImage ICursorRight = style->GetIcon(_T("right")).ConvertToImage();
   wxImage ICursorUp = style->GetIcon(_T("up")).ConvertToImage();
   wxImage ICursorDown = style->GetIcon(_T("down")).ConvertToImage();
-  wxImage ICursorPencil = style->GetIconScaled(_T("pencil"), cursorScale).ConvertToImage();
+  wxImage ICursorPencil = style->GetIconScaled(_T("pencil"), pencilScale).ConvertToImage();
   wxImage ICursorCross = style->GetIcon(_T("cross")).ConvertToImage();
 
 #if !defined(__WXMSW__) && !defined(__WXQT__)
@@ -964,9 +955,9 @@ void ChartCanvas::RebuildCursors() {
   } else
     pCursorDown = new wxCursor(wxCURSOR_ARROW);
 
-  if (ICursorPencil.Ok() && bUsePencil) {
-    ICursorPencil.SetOption(wxIMAGE_OPTION_CUR_HOTSPOT_X, 0 * cursorScale);
-    ICursorPencil.SetOption(wxIMAGE_OPTION_CUR_HOTSPOT_Y, 16 * cursorScale);
+  if (ICursorPencil.Ok()) {
+    ICursorPencil.SetOption(wxIMAGE_OPTION_CUR_HOTSPOT_X, 0 * pencilScale);
+    ICursorPencil.SetOption(wxIMAGE_OPTION_CUR_HOTSPOT_Y, 16 * pencilScale);
     pCursorPencil = new wxCursor(ICursorPencil);
   } else
     pCursorPencil = new wxCursor(wxCURSOR_ARROW);
