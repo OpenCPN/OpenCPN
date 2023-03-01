07d7d1a68bbc8f2b1781adfababf71c1290b13bb
dsr <bdbcat@yahoo.com>
2023-02-28 21:20:48 -0500

    Path debug #2

--- ci/generic-build-macos.sh
+++ ci/generic-build-macos.sh
@@ -67,8 +67,7 @@ curl -k -o /tmp/wx322-2_opencpn50_macos1010.tar.bz2  \
     https://download.opencpn.org/s/N7WP2yjNYZ7qsZp/download
 tar -C /tmp -xJf /tmp/wx322-2_opencpn50_macos1010.tar.bz2
 
-ls -l /tmp
-ls -l private/tmp
+ls -l /tmp/wx322.2
 
 export PATH="/usr/local/opt/gettext/bin:$PATH"
 echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.bash_profile
@@ -82,7 +81,7 @@ cmake -DOCPN_CI_BUILD=$CI_BUILD \
   -DOCPN_USE_LIBCPP=ON \
   -DOCPN_USE_SYSTEM_LIBARCHIVE=OFF \
   -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx322.2/lib/wx/config/osx_cocoa-unicode-3.2 \
-  -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx321_opencpn50_macos1010" \
+  -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx322-2_opencpn50_macos1010" \
   -DCMAKE_INSTALL_PREFIX=/tmp/opencpn -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
   -DOCPN_BUILD_TEST=OFF \
   ..
