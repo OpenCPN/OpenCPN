a84ac58e74179d03861c1f9cb471e68207cfb431
Alec Leamas <leamas.alec@nowhere.net>
2023-03-08 10:24:10 +0100

    plugin_loader/manager: Fix horrible hack (#2744)
    When deactivating a plugin, plugin_loader has emitted an event handled
    by pluginmanager. However, since event handling is asynchronous this has
    implies problem when the plugin to handle in pluginmanager has already
    been removed by plugin_loader.
    
    The hack has been a read-only, malloc'ed copy of the plugin, a
    construction the compiler rightfully warns about since the plugin
    container does not have a default ctor. This hack is horrible and
    fragile.
    
    Instead, retire the event and use a callback functin instead. This is
    executed synchronously before the plugin is actually deleted and thus
    solves the problem

--- src/plugin_loader.cpp
+++ src/plugin_loader.cpp
@@ -214,7 +214,9 @@ PluginLoader* PluginLoader::getInstance() {
 }
 
 PluginLoader::PluginLoader()
-    : m_blacklist(blacklist_factory()), m_default_plugin_icon(0) {}
+    : m_blacklist(blacklist_factory()),
+      m_default_plugin_icon(0),
+      m_on_deactivate_cb([](const PlugInContainer* pic) {}) {}
 
 bool PluginLoader::IsPlugInAvailable(wxString commonName) {
   for (unsigned int i = 0; i < plugin_array.GetCount(); i++) {
@@ -580,6 +582,7 @@ bool PluginLoader::DeactivatePlugIn(PlugInContainer* pic) {
     wxString msg("PluginLoader: Deactivating PlugIn: ");
     wxLogMessage(msg + pic->m_plugin_file);
 
+    m_on_deactivate_cb(pic);
 #ifndef CLIAPP
     // if this plugin is responsible for any charts, then unload chart cache
     if ((pic->m_cap_flag & INSTALLS_PLUGIN_CHART) ||
@@ -590,14 +593,6 @@ bool PluginLoader::DeactivatePlugIn(PlugInContainer* pic) {
 
     pic->m_bInitState = false;
     pic->m_pplugin->DeInit();
-    // pic is doomed and will be deleted. Make a copy to handler which
-    // can be used to look up items in toolbar etc.
-    // FIXME (leamas): Correct solution is to use a shared_ptr instead,
-    // expanding to a large patch covering many areas.
-    auto pic_copy =
-        static_cast<PlugInContainer*>(malloc(sizeof(PlugInContainer)));
-    memcpy(pic_copy, pic, sizeof(PlugInContainer));
-    evt_deactivate_plugin.Notify(pic_copy);
   }
   return true;
 }
