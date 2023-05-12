1ab7f7527dda8834faa1b3eceb90478cde185a2b
Pavel Kalian <pavel@kalian.cz>
2023-05-12 06:54:39 -0300

    Correct the show name checkbox incorrectly overriding minimal scale default for waypoints. Fixes #3209

--- src/MarkInfo.cpp
+++ src/MarkInfo.cpp
@@ -93,6 +93,7 @@ extern wxColour g_colourWaypointRangeRingsColour;
 
 extern int g_iWpt_ScaMin;
 extern bool g_bUseWptScaMin;
+extern bool g_bShowWptName;
 
 WX_DECLARE_LIST(wxBitmap, BitmapList);
 #include <wx/listimpl.cpp>
@@ -1395,7 +1396,7 @@ void MarkInfoDlg::DefautlBtnClicked(wxCommandEvent& event) {
         g_bUseWptScaMin = m_checkBoxScaMin->GetValue();
       }
       if (m_SaveDefaultDlg->NameCB->GetValue()) {
-        g_iWpt_ScaMin = m_checkBoxShowName->GetValue();
+        g_bShowWptName = m_checkBoxShowName->GetValue();
       }
     }
     m_SaveDefaultDlg = NULL;
