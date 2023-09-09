3e6231d5010e28edf00f1d6085b69cae5c476fff
AvirSaam <den.demchenko@gmail.com>
2023-09-09 15:46:02 +0000

    correct relative to true vector conversion implemented

--- src/ais_decoder.cpp
+++ src/ais_decoder.cpp
@@ -1609,17 +1609,33 @@ AisError AisDecoder::DecodeN0183(const wxString &str) {
     token.ToDouble(&arpa_cog);
     arpa_cogunit = tkz.GetNextToken();  // 7) Course Units
     if (arpa_cogunit == _T("R")) {
-        double course;
-        if (!std::isnan(gHdt))
-          course = gHdt;
+      double course;
+      if (!std::isnan(gHdt))
+        course = gHdt;
+      else
+        course = gCog;
+
+      double rel_bearing = 180 + course - arpa_cog;
+      if (rel_bearing > 180)
+        rel_bearing -= 360;
+
+      double new_arpa_speed = sqrt( gSog * gSog + arpa_sog * arpa_sog - 2 * gSog * arpa_sog * cos(rel_bearing * PI / 180.) );
+
+      if (new_arpa_speed != 0 && gSog != 0){
+        double cos_val = (new_arpa_speed * new_arpa_speed + gSog * gSog - arpa_sog * arpa_sog) / (2 * new_arpa_speed * gSog);
+        if (cos_val > 1)
+          cos_val = 1;
+        else if (cos_val < -1)
+          cos_val = -1;
+
+        arpa_cog = acos( cos_val  ) * 180. / PI;
+        if (rel_bearing < 0)
+          arpa_cog = course - arpa_cog;
         else
-          course = gCog;
+          arpa_cog = course + arpa_cog;
+      }
 
-        double new_arpa_speed = sqrt( gSog * gSog + arpa_sog * arpa_sog - 2 * gSog * arpa_sog * cos( (arpa_cog-course-180) * PI / 180.) );
-        double new_arpa_cog = acos( (new_arpa_speed * new_arpa_speed + gSog * gSog - arpa_sog * arpa_sog) / (2 * new_arpa_speed * gSog) ) * 180. / PI + course;
-
-        arpa_sog = new_arpa_speed;
-        arpa_cog = new_arpa_cog;
+      arpa_sog = new_arpa_speed;
 
       if (arpa_cog >= 360.) arpa_cog -= 360.;
     }
