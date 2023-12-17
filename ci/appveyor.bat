465960f85298b5ef8f4752fb9fdff41e20d31a14
Alec Leamas <leamas.alec@nowhere.net>
2023-12-17 06:09:54 +0100

    ci: windows: Build test, check that results are valid

--- ci/appveyor.bat
+++ ci/appveyor.bat
@@ -52,7 +52,7 @@ cmake -A Win32 -G "Visual Studio 17 2022" ^
     -DOCPN_CI_BUILD=ON ^
     -DOCPN_BUNDLE_WXDLLS=ON ^
     -DOCPN_RELEASE=0 ^
-    -DOCPN_BUILD_TEST=OFF ^
+    -DOCPN_BUILD_TEST=ON ^
     ..
 
 cmake --build . --target package --config %CONFIGURATION%
