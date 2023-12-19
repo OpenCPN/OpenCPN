bb760e8be60ecbc7d6384a1251d6e3893efdb458
Alec Leamas <leamas.alec@nowhere.net>
2023-12-20 00:34:39 +0100

    tests: MacOS fixes.

--- test/tests.cpp
+++ test/tests.cpp
@@ -41,7 +41,23 @@
 #include "std_instance_chk.h"
 #include "wx_instance_chk.h"
 
+// Macos up to 10.13
+#if defined(__clang_major__) && (__clang_major__ < 15)
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+
+// Ubuntu Bionic:
+#elif !defined(__clang_major__) && defined(__GNUC__) && (__GNUC__ < 8)
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+
+#else
+#include <filesystem>
+#include <utility>
 namespace fs = std::filesystem;
+#endif
+
+
 using namespace std::literals::chrono_literals;
 
 class AISTargetAlertDialog;
