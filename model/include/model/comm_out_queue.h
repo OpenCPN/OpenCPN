450a14c41769026ea80650c8da9e7ac0434c83f9
Pavel Kalian <pavel@kalian.cz>
2024-01-25 16:54:05 -0300

    Implement performance counters and improve hashing of message ID

--- model/include/model/comm_out_queue.h
+++ model/include/model/comm_out_queue.h
@@ -2,6 +2,51 @@
 #include <mutex>
 #include <string>
 #include <vector>
+#include <chrono>
+#include <unordered_map>
+
+
+class PerfCounter {
+  public:
+    PerfCounter() : msgs_in(0), msgs_out(0), bytes_in(0), bytes_out(0), bps_in(0), mps_in(0), bps_out(0), mps_out(0), in_out_delay_us(0), overflow_msgs(0) {}
+    void in(const size_t bytes, bool ok) {
+      auto t1 = std::chrono::steady_clock::now();
+      std::chrono::duration<double, std::micro> us_time = t1 - last_in;
+      bps_in = 0.95 * bps_in + 0.05 * bytes * 1000000 / us_time.count();
+      mps_in = 0.95 * bps_in + 0.05 * 1000000 / us_time.count();
+      msgs_in++;
+      bytes_in += bytes;
+      last_in = t1;
+      if(!ok) {
+        overflow_msgs++;
+      }
+    }
+
+    void out(const size_t bytes, std::chrono::time_point<std::chrono::steady_clock> in_ts) {
+      auto t1 = std::chrono::steady_clock::now();
+      std::chrono::duration<double, std::micro> us_time = t1 - last_in;
+      bps_out = 0.95 * bps_out + 0.05 * bytes * 1000000 / us_time.count();
+      mps_out = 0.95 * bps_out + 0.05 * 1000000 / us_time.count();
+      us_time = t1 - in_ts;
+      in_out_delay_us = 0.95 * in_out_delay_us + 0.05 * us_time.count();
+      msgs_out++;
+      bytes_out += bytes;
+      last_out = t1;
+    }
+
+    size_t msgs_in;
+    size_t msgs_out;
+    size_t bytes_in;
+    size_t bytes_out;
+    uint32_t bps_in;
+    double mps_in;
+    uint32_t bps_out;
+    double mps_out;
+    size_t in_out_delay_us;
+    size_t overflow_msgs;
+    std::chrono::time_point<std::chrono::steady_clock> last_in;
+    std::chrono::time_point<std::chrono::steady_clock> last_out;
+};
 
 /**
  *  Queue of NMEA0183 messages which only holds a limited amount
@@ -19,7 +64,7 @@ public:
    * Return  next line to send and remove it from buffer,
    * throws exception if empty.
    */
-  std::string pop();
+  virtual std::string pop();
 
   /** Return number of lines in queue. */
   int size() const;
@@ -42,6 +87,7 @@ protected:
     std::string line;
     BufferItem(const std::string& line);
     BufferItem(const BufferItem& other);
+    std::chrono::time_point<std::chrono::steady_clock> ts;
   };
 
   std::vector<BufferItem> m_buffer;
@@ -55,15 +101,21 @@ public:
   CommOutQueueSingle() : CommOutQueue(1) {}
 
   /** Insert line of NMEA0183 data in buffer. */
-  bool push_back(const std::string& line);
+  bool push_back(const std::string& line) override;
 };
 
 /** Add unit test measurements to CommOutQueue. */
+#include <unordered_map>
+
 class MeasuredCommOutQueue : public CommOutQueue {
 public:
-  MeasuredCommOutQueue(int size) : CommOutQueue(size), push_time(0) {}
+  MeasuredCommOutQueue(int size) : CommOutQueue(size), push_time(0), pop_time(0) {}
 
-  bool push_back(const std::string& line);
+  bool push_back(const std::string& line) override;
+  std::string pop() override;
 
+  std::unordered_map<unsigned long, PerfCounter> msg_perf;
+  PerfCounter perf;
   double push_time;
+  double pop_time;
 };
--- model/src/comm_out_queue.cpp
+++ model/src/comm_out_queue.cpp
@@ -13,19 +13,38 @@ static const uint64_t kFirstFiveBytes = 0x000000ffffffffff;
 static const uint64_t kFirstFiveBytes = 0xffffffffff000000;
 #endif
 
+#define PUBX 190459303248 //"PUBX,"
+#define STALK 323401897043 //"STALK"
 
-/** Return bytes 1..5 in line as an uint64_t. */
+/** Return bytes 1..5 in line as an uint64_t with esceptions for u-blox GNSS and converted Seatalk.
+ * Note: Some vendor extension messages also do not have 5 character talker ID + message ID, but 6 (PMGNST, PRWIZCH, PSMDST).
+ * This is probably not critical as the last character seems to not be making any difference in producing a uinique ID for the messages commonly seen.
+*/
 static inline uint64_t GetNmeaType(const std::string& line) {
-  uint64_t result = *reinterpret_cast<const uint64_t*>(&line[1]);
-  result &= kFirstFiveBytes;
-  return result;
+  size_t skipchars = 1;
+  if (line[0] == 0x5c) { //Starts with the tag block '\', we need to skip it and then also the start delimiter
+    skipchars = line.find(',', 1);
+    if(skipchars == std::string::npos) {
+      skipchars = 1; // This should never happen, there is no end of the tag block, but just in case...
+    }
+  }
+  uint64_t result = *reinterpret_cast<const uint64_t*>(&line[skipchars]);
+  uint64_t result5 = result & kFirstFiveBytes;
+  if(result5 == PUBX || result5 == STALK) {
+    /* PUBX from possibly high-speed u-blox GNSS receivers that are sure to overload slow connections has a 2 digit zero-padded numerical message ID in the first field
+       Similar with STALK, the two digit Seatalk message ID is in the first field
+       Both fit nicely into 8 bytes though... */
+    return result;
+  } else {
+    return result5;
+  }
 }
 
 CommOutQueue::BufferItem::BufferItem(const std::string& _line)
-    : type(GetNmeaType(_line)), line(_line) {}
+    : type(GetNmeaType(_line)), line(_line), ts(std::chrono::steady_clock::now()) {}
 
 CommOutQueue::BufferItem::BufferItem(const BufferItem& other)
-    : type(other.type), line(other.line) {}
+    : type(other.type), line(other.line), ts(std::chrono::steady_clock::now()) {}
 
 CommOutQueue::CommOutQueue(int size) : m_size(size - 1) {
   assert(size >= 1 && "Illegal buffer size");
@@ -83,13 +102,36 @@ bool CommOutQueueSingle::push_back(const std::string& line) {
 
 bool MeasuredCommOutQueue::push_back(const std::string& line) {
   using std::chrono::duration;
-  using std::chrono::high_resolution_clock;
+  using std::chrono::steady_clock;
 
-  auto t1 = high_resolution_clock::now();
+  auto t1 = steady_clock::now();
   bool ok = CommOutQueue::push_back(line);
-  auto t2 = high_resolution_clock::now();
+  msg_perf[GetNmeaType(line)].in(line.size(), ok);
+  perf.in(line.size(), ok);
+  auto t2 = steady_clock::now();
   duration<double, std::micro> us_time = t2 - t1;
 
   push_time = 0.95 * push_time + 0.05 * us_time.count();  // LP filter.
   return ok;
+}
+
+std::string  MeasuredCommOutQueue::pop() {
+  using std::chrono::duration;
+  using std::chrono::steady_clock;
+
+  auto t1 = steady_clock::now();
+  //auto msg = CommOutQueue::pop(); // We need to update the perf counters, can't just pop() here
+  std::lock_guard<std::mutex> lock(m_mutex);
+  if (m_buffer.size() <= 0)
+    throw std::underflow_error("Attempt to pop() from empty buffer");
+  auto item = m_buffer.back();
+  m_buffer.pop_back();
+  perf.out(item.line.size(), item.ts);
+  msg_perf[item.type].out(item.line.size(), item.ts);
+  auto t2 = steady_clock::now();
+  duration<double, std::micro>us_time = t2 - t1;
+  us_time = t2 - t1;
+
+  pop_time = 0.95 * pop_time + 0.05 * us_time.count();  // LP filter.
+  return item.line;
 }
