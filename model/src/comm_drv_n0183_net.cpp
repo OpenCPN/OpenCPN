6179265b60f2d55f4c4bf1b2f996619f0b4a6630
Alec Leamas <leamas.alec@nowhere.net>
2024-01-27 22:39:03 +0100

    comm_drv_n0183_net.cpp: clang-format

--- model/src/comm_drv_n0183_net.cpp
+++ model/src/comm_drv_n0183_net.cpp
@@ -70,7 +70,7 @@
 #define N_DOG_TIMEOUT 5
 
 // FIXME (dave)  This should be in some more "common" space, but where?
-bool CheckSumCheck(const std::string &sentence) {
+bool CheckSumCheck(const std::string& sentence) {
   size_t check_start = sentence.find('*');
   if (check_start == wxString::npos || check_start > sentence.size() - 3)
     return false;  // * not found, or it didn't have 2 characters following it.
@@ -87,7 +87,7 @@ bool CheckSumCheck(const std::string &sentence) {
   return calculated_checksum == checksum;
 }
 
-class MrqContainer{
+class MrqContainer {
 public:
   struct ip_mreq m_mrq;
   void SetMrqAddr(unsigned int addr) {
@@ -163,7 +163,7 @@ CommDriverN0183Net::CommDriverN0183Net(const ConnectionParams* params,
   m_socketread_watchdog_timer.SetOwner(this, TIMER_SOCKET + 1);
   this->attributes["netAddress"] = params->NetworkAddress.ToStdString();
   char port_char[10];
-  sprintf(port_char, "%d",params->NetworkPort);
+  sprintf(port_char, "%d", params->NetworkPort);
   this->attributes["netPort"] = std::string(port_char);
 
   // Prepare the wxEventHandler to accept events from the actual hardware thread
@@ -249,7 +249,8 @@ void CommDriverN0183Net::OpenNetworkUDP(unsigned int addr) {
     if ((ntohl(addr) & 0xf0000000) == 0xe0000000) {
       SetMulticast(true);
       m_mrq_container->SetMrqAddr(addr);
-      GetSock()->SetOption(IPPROTO_IP, IP_ADD_MEMBERSHIP, &m_mrq_container->m_mrq,
+      GetSock()->SetOption(IPPROTO_IP, IP_ADD_MEMBERSHIP,
+                           &m_mrq_container->m_mrq,
                            sizeof(m_mrq_container->m_mrq));
     }
 
@@ -331,11 +332,13 @@ void CommDriverN0183Net::OpenNetworkGPSD() {
 void CommDriverN0183Net::OnSocketReadWatchdogTimer(wxTimerEvent& event) {
   m_dog_value--;
   if (m_dog_value <= 0) {  // No receive in n seconds, assume connection lost
-    wxString log = wxString::Format(_T("    TCP NetworkDataStream watchdog timeout: %s."),
-      GetPort().c_str());
+    wxString log =
+        wxString::Format(_T("    TCP NetworkDataStream watchdog timeout: %s."),
+                         GetPort().c_str());
     if (!GetParams().NoDataReconnect) {
-      log.Append(wxString::Format(_T(" Reconnection is disabled, waiting another %d seconds."),
-        N_DOG_TIMEOUT));
+      log.Append(wxString::Format(
+          _T(" Reconnection is disabled, waiting another %d seconds."),
+          N_DOG_TIMEOUT));
       m_dog_value = N_DOG_TIMEOUT;
       wxLogMessage(log);
       return;
@@ -372,9 +375,8 @@ bool CommDriverN0183Net::SendMessage(std::shared_ptr<const NavMsg> msg,
   return SendSentenceNetwork(msg_0183->payload.c_str());
 }
 
-
 void CommDriverN0183Net::OnSocketEvent(wxSocketEvent& event) {
-  //#define RD_BUF_SIZE    200
+  // #define RD_BUF_SIZE    200
 #define RD_BUF_SIZE \
   4096  // Allows handling of high volume data streams, such as a National AIS
         // stream with 100s of msgs a second.
