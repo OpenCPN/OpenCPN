8bc3692bdeced9b557f6c92248759a2178ddc7dd
Alec Leamas <leamas.alec@nowhere.net>
2024-01-31 09:09:15 +0100

    sound: Move configuration to library

--- libs/sound/cmake/FindLibSndfile.cmake
+++ libs/sound/cmake/FindLibSndfile.cmake
@@ -12,34 +12,31 @@
 #  Copyright (c) 2009-2015 Jörg Müller. All rights reserved.
 #  License: Apache 2.0 (not distributed in binary packages).
 
-if(APPLE AND OCPN_USE_DEPS_BUNDLE)
+if (APPLE AND OCPN_USE_DEPS_BUNDLE)
   set(LIBSNDFILE_INCLUDE_DIR "${OCPN_DEPS_BUNDLE_PATH}/include")
   set(LIBSNDFILE_LIBRARY "${OCPN_DEPS_BUNDLE_PATH}/lib/libsndfile.dylib")
-else()
+else ()
   # Use pkg-config to get hints about paths
   find_package(PkgConfig QUIET)
-  if(PKG_CONFIG_FOUND)
-	pkg_check_modules(LIBSNDFILE_PKGCONF sndfile)
-  endif(PKG_CONFIG_FOUND)
+  if (PKG_CONFIG_FOUND)
+	  pkg_check_modules(LIBSNDFILE_PKGCONF sndfile)
+  endif ()
 
   # Include dir
-  find_path(LIBSNDFILE_INCLUDE_DIR
-	NAMES sndfile.h
-  )
-
+  find_path(LIBSNDFILE_INCLUDE_DIR NAMES sndfile.h)
   # Library
   find_library(LIBSNDFILE_LIBRARY
-	NAMES sndfile libsndfile-1
-	PATHS ${LIBSNDFILE_PKGCONF_LIBRARY_DIRS}
+  	NAMES sndfile libsndfile-1
+  	PATHS ${LIBSNDFILE_PKGCONF_LIBRARY_DIRS}
   )
 endif()
 
 find_package(PackageHandleStandardArgs)
-find_package_handle_standard_args(LibSndFile  DEFAULT_MSG  LIBSNDFILE_LIBRARY LIBSNDFILE_INCLUDE_DIR)
+find_package_handle_standard_args(LibSndfile  DEFAULT_MSG  LIBSNDFILE_LIBRARY LIBSNDFILE_INCLUDE_DIR)
 
-if(LIBSNDFILE_FOUND)
+if (LIBSNDFILE_FOUND)
   set(LIBSNDFILE_LIBRARIES ${LIBSNDFILE_LIBRARY})
   set(LIBSNDFILE_INCLUDE_DIRS ${LIBSNDFILE_INCLUDE_DIR})
-endif(LIBSNDFILE_FOUND)
+endif ()
 
 mark_as_advanced(LIBSNDFILE_LIBRARY LIBSNDFILE_LIBRARIES LIBSNDFILE_INCLUDE_DIR LIBSNDFILE_INCLUDE_DIRS)
--- libs/sound/src/SoundFactory.cpp
+++ libs/sound/src/SoundFactory.cpp
@@ -21,7 +21,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
  ***************************************************************************
  */
-#include "config.h"
+#include "snd_config.h"
 #include "SoundFactory.h"
 
 #if defined(__OCPN__ANDROID__)
