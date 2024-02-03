4fc459d47bcf5cb86af837ffad7c21858494a7ff
Dave <bdbcat@yahoo.com>
2024-02-03 16:25:56 -0500

    Correct FTBS on older linux builds

--- model/src/comm_can_util.cpp
+++ model/src/comm_can_util.cpp
@@ -23,7 +23,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
  **************************************************************************/
 
-
+#include <algorithm>
 #include <vector>
 
 #include "model/comm_can_util.h"
