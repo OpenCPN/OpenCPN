1cdf9a50019b5dc1ca4d8d6a822133cb45c754c9
Alec Leamas <leamas.alec@nowhere.net>
2024-02-21 22:15:04 +0100

    comm_plugin_api.cpp -> plugin_api.cpp
    Last changes makes the file more like a general "implement
    ocpn_plugin.h" thing then something communication specific.

--- model/src/plugin_api.cpp
+++ model/src/plugin_api.cpp
@@ -22,6 +22,9 @@
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
  **************************************************************************/
+
+/** \file plugin_api.cpp Implement various ocpn_plugin.h methods. */
+
 #include <memory>
 #include <sstream>
 #include <vector>
