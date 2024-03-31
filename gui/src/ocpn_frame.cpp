0189695de4ccac79072c18c1ad1b3389b8893866
Dave <bdbcat@yahoo.com>
2024-03-31 10:56:52 -0400

    Adjust toolbar scale for Win scaled display, also refine tooltip positions #3593

--- gui/src/ocpn_frame.cpp
+++ gui/src/ocpn_frame.cpp
@@ -1325,10 +1325,12 @@ void MyFrame::UpdateAllToolbars(ColorScheme cs) {
 
 void MyFrame::SetAllToolbarScale() {
   g_toolbar_scalefactor = g_Platform->GetToolbarScaleFactor(g_GUIScaleFactor);
+  g_toolbar_scalefactor *= OCPN_GetWinDIPScaleFactor();
 }
 
 void MyFrame::SetGPSCompassScale() {
   g_compass_scalefactor = g_Platform->GetCompassScaleFactor(g_GUIScaleFactor);
+  g_compass_scalefactor *= OCPN_GetWinDIPScaleFactor();
 }
 
 ChartCanvas *MyFrame::GetCanvasUnderMouse() {
