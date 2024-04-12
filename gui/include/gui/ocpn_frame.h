a2d8240b342f71f9d04045248197d0143860a04e
Alec Leamas <leamas.alec@nowhere.net>
2024-04-12 17:35:17 +0200

    Add overflow dialog

--- gui/include/gui/ocpn_frame.h
+++ gui/include/gui/ocpn_frame.h
@@ -37,6 +37,7 @@
 #include "model/ocpn_types.h"
 #include "model/comm_appmsg_bus.h"
 #include "bbox.h"
+#include "comm_overflow_dlg.h"
 #include "color_handler.h"
 #include "gui_lib.h"
 #include "load_errors_dlg.h"
@@ -405,6 +406,8 @@ private:
   ObsListener m_on_raise_listener;
   ObsListener m_on_quit_listener;
   ObsListener m_routes_update_listener;
+
+  CommOverflowDlg comm_overflow_dlg;
 
   DECLARE_EVENT_TABLE()
 };
