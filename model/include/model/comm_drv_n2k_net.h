111b66e370cd6ffac8cb795b6318e1acb451b617
Alec Leamas <leamas.alec@gmail.com>
2024-06-30 14:36:33 +0200

    New upstream version 5.9.2-beta2+dfsg

--- model/include/model/comm_drv_n2k_net.h
+++ model/include/model/comm_drv_n2k_net.h
@@ -73,7 +73,9 @@ typedef enum
   N2KFormat_Actisense_N2K_ASCII,
   N2KFormat_Actisense_N2K,
   N2KFormat_Actisense_RAW,
-  N2KFormat_Actisense_NGT
+  N2KFormat_Actisense_NGT,
+  N2KFormat_SeaSmart,
+  N2KFormat_MiniPlex
 } N2K_Format;
 
 typedef enum
@@ -185,6 +187,8 @@ private:
   bool ProcessActisense_N2K(std::vector<unsigned char> packet);
   bool ProcessActisense_RAW(std::vector<unsigned char> packet);
   bool ProcessActisense_NGT(std::vector<unsigned char> packet);
+  bool ProcessSeaSmart(std::vector<unsigned char> packet);
+  bool ProcessMiniPlex(std::vector<unsigned char> packet);
 
 
   bool SendN2KNetwork(std::shared_ptr<const Nmea2000Msg> &msg,
