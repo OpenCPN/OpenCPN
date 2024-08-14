0deda588e40d9c6dbecdf9a097b8d8e8e6e2fe66
Alec Leamas <leamas.alec@gmail.com>
2024-08-14 21:36:17 +0200

    build: Remove .deb package generation
    First part of alternative debian package: Remove now obsolete
    debian/ directory and related "make package" call in the
    build script

--- ci/generic-build-debian.sh
+++ ci/generic-build-debian.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-# Build the Travis Debian artifacts
+# Build the Debian artifacts
 #
 set -xe
 src_tree_root="$(dirname $(readlink -f $0))/.."
@@ -9,20 +9,11 @@ sudo apt-get -qq update
 sudo apt-get install --yes --force-yes -q devscripts equivs
 
 mk-build-deps "${src_tree_root}/ci/control" \
-    --install --root-cmd=sudo\
+    --install \
+    --root-cmd=sudo \
     --remove \
     --tool="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes --force-yes"
 sudo apt-get --allow-unauthenticated --yes --force-yes install -f
-
-# Xenial finds webview header but not the library:
-if [ "$OCPN_TARGET" = "xenial" ]; then
-    WEBVIEW_OPT="-DOCPN_USE_WEBVIEW:BOOL=OFF"
-fi
-
-if [[ "$EXTRA_BUILD_OPTS" == *OCPN_FORCE_GTK3=ON* ]]; then
-    sudo update-alternatives --set wx-config \
-        /usr/lib/*-linux-*/wx/config/gtk3-unicode-3.0
-fi
 
 pushd "${src_tree_root}"
 rm -rf build && mkdir build && cd build
@@ -34,7 +25,6 @@ cmake $WEBVIEW_OPT  $EXTRA_BUILD_OPTS\
     ..
 make -sj2
 dbus-run-session make run-tests || :
-make package
 popd
 
 sudo apt-get --yes --force-yes install python3-pip python3-setuptools
