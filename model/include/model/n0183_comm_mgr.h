a16a500141e917cb0a23d65d4ba4681c68da6a0f
Alec Leamas <leamas.alec@gmail.com>
2024-09-26 19:04:25 +0200

    comm_drv_serial: Send message direct (#3116).
    In the serial driver thread, send the message from outside directly
    without an extra event passing round to the main driver. In the end
    this is much simpler and should be faster and easier to maintain.

--- model/include/model/n0183_comm_mgr.h
+++ model/include/model/n0183_comm_mgr.h
@@ -26,6 +26,8 @@
 #include "config.h"
 
 #include <atomic>
+#include <memory>
+#include <functional>
 
 #include <wx/string.h>
 
@@ -35,13 +37,14 @@
 #ifndef __ANDROID__
 #include "serial/serial.h"
 #endif
-
+using SendMsgFunc = std::function<void(const std::vector<unsigned char>&)>;
 class CommDriverN0183Serial;
+
 
 /** Nmea0183 serial IO thread. */
 class CommDriverN0183SerialThread : public ThreadCtrl {
 public:
-  CommDriverN0183SerialThread(CommDriverN0183Serial* launcher);
+  CommDriverN0183SerialThread(SendMsgFunc send_msg_func);
 
   ~CommDriverN0183SerialThread();
 
@@ -53,10 +56,6 @@ public:
   /** Send a message to remote peer. */
   bool SetOutMsg(const wxString& msg);
 
-  /** Unset thread "keep going" flag i. e., initiate stop sequence. */
-
-  /** Return true if/when thread has stopped. */
-
 private:
 #ifndef __ANDROID__
   serial::Serial m_serial;
@@ -66,10 +65,10 @@ private:
   void CloseComPortPhysical();
   ssize_t WriteComPortPhysical(const char* msg);
 
-  CommDriverN0183Serial* m_parent_driver;
   wxString m_portname;
   int m_baud;
   OutputBuffer m_out_que;
+  SendMsgFunc m_send_msg_func;
 };
 
 #endif  //    _N0183_PROTOL_MGR__
