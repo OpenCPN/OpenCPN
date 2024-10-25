052fbabfd59b8862e7a7a7cd38da58ea20699eee
Alec Leamas <leamas.alec@gmail.com>
2024-10-25 23:05:12 +0200

    comm_drv_n0183_net: Move checksum utility to ocpn_utils

--- model/src/ocpn_utils.cpp
+++ model/src/ocpn_utils.cpp
@@ -145,4 +145,21 @@ void copy_file(const std::string& src_path, const std::string& dest_path) {
   dest.close();
 }
 
+bool N0183CheckSumOk(const std::string& sentence) {
+  size_t check_start = sentence.find('*');
+  if (check_start == std::string::npos || check_start > sentence.size() - 3)
+    return false;  // * not found, or it didn't have 2 characters following it.
+
+  std::string check_str = sentence.substr(check_start + 1, 2);
+  unsigned long checksum = strtol(check_str.c_str(), 0, 16);
+  if (checksum == 0L && check_str != "00") return false;
+
+  unsigned char calculated_checksum = 0;
+  for (std::string::const_iterator i = sentence.begin() + 1;
+       i != sentence.end() && *i != '*'; ++i)
+    calculated_checksum ^= static_cast<unsigned char>(*i);
+
+  return calculated_checksum == checksum;
+}
+
 }  // namespace ocpn
