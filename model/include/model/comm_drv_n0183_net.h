5df19a265a1708f6f3f0163c18513356348d9190
Alec Leamas <leamas.alec@gmail.com>
2024-10-25 22:43:55 +0200

    comm_drv_n0183_net: Clean up comments

--- model/include/model/comm_drv_n0183_net.h
+++ model/include/model/comm_drv_n0183_net.h
@@ -1,11 +1,6 @@
-/***************************************************************************
- *
- * Project:  OpenCPN
- * Purpose:
- * Author:   David Register, Alec Leamas
- *
- ***************************************************************************
- *   Copyright (C) 2022 by David Register, Alec Leamas                     *
+/**************************************************************************
+ *   Copyright (C) 2022  David Register                                    *
+ *   Copyright (C) 2022  Alec Leamas                                       *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -22,6 +17,8 @@
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
  **************************************************************************/
+
+/** \file comm_drv_n0183_net.h  NMEA0183 over IP driver. */
 
 #ifndef _COMMDRIVERN0183NET_H
 #define _COMMDRIVERN0183NET_H
