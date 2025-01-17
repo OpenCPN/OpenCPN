db55f6ffb58f602d3f53b52877e5f2c4eafef8e8
Alec Leamas <leamas.alec@gmail.com>
2025-01-17 16:36:19 +0100

    Add  Data Monitor skeletons

---
+++ gui/include/gui/svg_icons.h
@@ -0,0 +1,36 @@
+/**
+ * \file
+ * Various svg icons, so far from Icon Library at
+ * https://gitlab.gnome.org/World/design/icon-library/, available
+ * at Flathub as org.gnome.design.IconLibrary and
+ * https://teams.pages.gitlab.gnome.org/Design/icon-development-kit-www/
+ *
+ * License: GPL v3 or later.
+ */
+const char* kFunnelSvg = R"--(
+  <svg xmlns="http://www.w3.org/2000/svg"
+       height="16px" viewBox="0 0 16 16" width="16px">
+  <path d="m 0 1 h 15 l -6 7 v 6 l -3 2 v -8 z m 0 0" fill="#222222"/>
+  </svg>
+)--";
+
+const char* kNoFunnelSvg = R"--(
+<svg
+   height="16px" viewBox="0 0 16 16" width="16px" version="1.1"
+   id="svg1"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <defs id="defs1" />
+  <path
+     d="m 0 1 h 15 l -6 7 v 6 l -3 2 v -8 z m 0 0"
+     fill="#222222"
+     id="path1" />
+  <path
+     style="fill:none;stroke:#f80000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+     d="M 0.88888889,12.565657 14.969697,4.969697"
+     id="path2" />
+  <path
+     style="fill:none;stroke:#f80000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+     d="m 1.010101,5.2121212 13.49495,7.1515148"
+     id="path3" />
+</svg>
+)--";
