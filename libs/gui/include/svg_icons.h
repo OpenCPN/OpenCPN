a1b7136b5681cb85b129c4b7daeb1cd3860ff220
Alec Leamas <leamas.alec@gmail.com>
2025-04-05 06:53:49 +0200

    gui_lib: Add new EditButton component
    See images in #4442

---
+++ libs/gui/include/svg_icons.h
@@ -0,0 +1,16 @@
+/**
+ * \file
+ * Various svg icons, so far from Icon Library at
+ * https://gitlab.gnome.org/World/design/icon-library/, available
+ * at Flathub as org.gnome.design.IconLibrary and
+ * https://teams.pages.gitlab.gnome.org/Design/icon-development-kit-www/
+ *
+ * License: GPL v3 or later.
+ */
+
+namespace gui_icons {
+
+extern const char* kEditPen;
+extern const char* kCheckmark;
+
+}  // namespace gui_icons
