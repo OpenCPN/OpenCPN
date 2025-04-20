6b04f59ec58cefa597888f92053f6f3922d556e1
Dave <bdbcat@yahoo.com>
2025-04-19 22:55:57 -0400

    Improve N2K message payload print format, for DataMonitor.

--- model/src/comm_navmsg.cpp
+++ model/src/comm_navmsg.cpp
@@ -90,9 +90,36 @@ static std::string CharToString(char c) {
 }
 
 std::string Nmea2000Msg::to_string() const {
-  std::string s;
-  for (auto c : payload) s.append(CharToString(c));
-  return PGN.to_string() + " " + s;
+  // Arrange to "pretty-print" the N2K message payload
+
+  // extract PGN from payload
+  uint64_t pgn = 0;
+  unsigned char* c = (unsigned char*)&pgn;
+  *c++ = payload.at(3);
+  *c++ = payload.at(4);
+  *c++ = payload.at(5);
+
+  std::string st;
+  size_t i = 0;
+  while (i < payload.size() - 1) {
+    if (i > 12) {
+      for (int j = 0; j < 8; j++) {
+        st.append(CharToString(payload[i]));
+        i++;
+        if (i >= payload.size() - 1) {
+          break;
+        }
+      }
+      st.append(" ");
+    } else
+      i++;
+  }
+
+  std::stringstream ss1;
+  std::string spgn = "    ";
+  if (PGN.pgn == 1) spgn = "ALL ";
+  ss1 << "n2000-" << spgn << "PGN: " << pgn << " [ " << st << " ]";
+  return ss1.str();
 }
 
 std::string Nmea2000Msg::to_vdr() const {
@@ -104,6 +131,7 @@ std::string Nmea2000Msg::to_vdr() const {
 std::string Nmea0183Msg::to_string() const {
   std::stringstream ss;
   ss << key() << " " << talker << type << " " << ocpn::printable(payload);
+  printf("%s\n", ss.str().c_str());
   return ss.str();
 }
 
