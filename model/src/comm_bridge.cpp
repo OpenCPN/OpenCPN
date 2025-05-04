8b1ebce30db149ad5a29344720db5f8dc0fa7fa7
Dave <bdbcat@yahoo.com>
2025-05-04 12:14:44 -0400

    Improve comm priority source identifier for N2k messages.

--- model/src/comm_bridge.cpp
+++ model/src/comm_bridge.cpp
@@ -1228,38 +1228,39 @@ bool CommBridge::SaveConfig(void) {
 }
 
 std::string CommBridge::GetPriorityKey(std::shared_ptr<const NavMsg> msg) {
-  std::string source = msg->source->to_string();
-  std::string listener_key = msg->key();
+  std::string key;
 
   std::string this_identifier;
   std::string this_address("0");
   if (msg->bus == NavAddr::Bus::N0183) {
     auto msg_0183 = std::dynamic_pointer_cast<const Nmea0183Msg>(msg);
     if (msg_0183) {
+      std::string source = msg->source->to_string();
       this_identifier = msg_0183->talker;
       this_identifier += msg_0183->type;
+      key = source + ":" + this_address + ";" + this_identifier;
     }
   } else if (msg->bus == NavAddr::Bus::N2000) {
     auto msg_n2k = std::dynamic_pointer_cast<const Nmea2000Msg>(msg);
     if (msg_n2k) {
       this_identifier = msg_n2k->PGN.to_string();
       unsigned char n_source = msg_n2k->payload.at(7);
-      char ss[4];
-      sprintf(ss, "%d", n_source);
-      this_address = std::string(ss);
+      wxString km = wxString::Format("N2k device address: %d", n_source);
+      key = km.ToStdString() + " ; " + "PGN: " + this_identifier;
     }
   } else if (msg->bus == NavAddr::Bus::Signalk) {
     auto msg_sk = std::dynamic_pointer_cast<const SignalkMsg>(msg);
     if (msg_sk) {
       auto addr_sk =
           std::static_pointer_cast<const NavAddrSignalK>(msg->source);
-      source = addr_sk->to_string();
+      std::string source = addr_sk->to_string();
       this_identifier = "signalK";
       this_address = msg->source->iface;
+      key = source + ":" + this_address + ";" + this_identifier;
     }
   }
 
-  return source + ":" + this_address + ";" + this_identifier;
+  return key;
 }
 
 bool CommBridge::EvalPriority(
