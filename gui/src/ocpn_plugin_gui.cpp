78d3fa444ed8da7da528df6b9124aaf3cd487dbc
Dave <bdbcat@yahoo.com>
2025-07-10 20:49:25 -0400

    Restore legacy plugin API behavior on OCPNGetFont() API call.

--- gui/src/ocpn_plugin_gui.cpp
+++ gui/src/ocpn_plugin_gui.cpp
@@ -317,7 +317,7 @@ bool GetGlobalColor(wxString colorName, wxColour* pcolour) {
 }
 
 wxFont* OCPNGetFont(wxString TextElement, int default_size) {
-  return FontMgr::Get().GetFont(TextElement, default_size);
+  return FontMgr::Get().GetFontLegacy(TextElement, default_size);
 }
 
 wxFont* GetOCPNScaledFont_PlugIn(wxString TextElement, int default_size) {
