6fd9cfec69654ff1e7641015d6f9f3a5edaba826
Alec Leamas <leamas.alec@gmail.com>
2025-08-11 10:22:28 +0200

    d/repo: build.sh help update

--- debian/repo/tools/build.sh
+++ debian/repo/tools/build.sh
@@ -3,9 +3,11 @@ if [[ "$1" == "-h" || "$1" == "--help" ]]; then
  cat << "EOF"
 
 test -d /tmp/pkg-amd64 || mkdir /tmp/pkg-amd64
+# Initial run:
 podman run -v $HOME/opencpn-gpg:/root/.gnupg --platform linux/amd64 \
      -v=$PWD:/build -v=/tmp/pkg-amd64:/output -e DOCKER_BUILD=1 \
-     --name debian_13-amd64 debian:trixie-backports
+     --name debian_13-amd64 -ti debian:trixie-backports /bin/bash
+# Subsequent runs:
 podman start debian_13-amd64
 podman cp ~/.devscripts debian_13-amd64:/root
 podman exec -ti debian_13-amd64 /bin/bash
@@ -13,9 +15,11 @@ podman exec -ti debian_13-amd64 /bin/bash
 
 
 test -d /tmp/pkg-aarch64 || mkdir /tmp/pkg-aarch64
+# Initial run:
 podman run -v $HOME/opencpn-gpg:/root/.gnupg --platform linux/aarch64 \
      -v=$PWD:/build -v=/tmp/pkg-aarch64:/output -e DOCKER_BUILD=1 \
-     --name debian_13-aarch64 debian:trixie-backports
+     --name debian_13-aarch64 -ti debian:trixie-backports /bin/bash
+# Subsequent runs:
 podman start debian_13-aarch64
 podman cp ~/.devscripts debian_13-aarch64:/root
 podman exec -ti debian_13-aarch64 /bin/bash
