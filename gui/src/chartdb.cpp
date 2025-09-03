d3fc990686a4b56b9c46e1f47225db867a839dce
Alec Leamas <leamas.alec@gmail.com>
2025-09-03 15:52:53 +0200

    chartdb: file clean up checklist

--- gui/src/chartdb.cpp
+++ gui/src/chartdb.cpp
@@ -1,10 +1,4 @@
-/***************************************************************************
- *
- * Project:  OpenCPN
- * Purpose:  Chart Database Object
- * Author:   David Register, Mark A Sikes
- *
- ***************************************************************************
+/**************************************************************************
  *   Copyright (C) 2010 by David S. Register   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -18,10 +12,14 @@
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ *   along with this program; if not, see <https://www.gnu.org/licenses/>. *
  **************************************************************************/
+
+/**
+ * \file
+ *
+ * Implement chartdb.h -- chart database management
+ */
 
 // For compilers that support precompilation, includes "wx.h".
 #include <wx/wxprec.h>
