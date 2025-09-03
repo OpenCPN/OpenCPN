b5000412358487356f9028e9c745e45c268165fd
Alec Leamas <leamas.alec@gmail.com>
2025-09-03 15:42:58 +0200

    cat_settings: file clean up checklist

--- gui/include/gui/cat_settings.h
+++ gui/include/gui/cat_settings.h
@@ -1,8 +1,4 @@
-/******************************************************************************
- *
- * Project:  OpenCPN
- *
- ***************************************************************************
+/**************************************************************************
  *   Copyright (C) 2019 Alec Leamas                                        *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -16,17 +12,19 @@
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
- ***************************************************************************
+ *   along with this program; if not, see <https://www.gnu.org/licenses/>. *
+ **************************************************************************/
+
+/**
+ * \file
+ *
+ * Plugin catalog settings dialog
  */
 
 #ifndef PLUG_SETTINGS_H_
 #define PLUG_SETTINGS_H_
 
 #include <wx/dialog.h>
-#include <wx/window.h>
 
 /**
  * Modal dialog for plugin catalog settings. Implements a dialog that displays
