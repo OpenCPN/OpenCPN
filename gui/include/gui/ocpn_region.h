028807d0d6db6edc3a5f4647f5475855b9d116c0
Alec Leamas <leamas.alec@gmail.com>
2025-09-16 22:48:18 +0200

    ocpn_region: file clean up checklist

--- gui/include/gui/ocpn_region.h
+++ gui/include/gui/ocpn_region.h
@@ -1,32 +1,15 @@
-/***************************************************************************
- *
- * Project:  OpenCPN
- *
- ***************************************************************************
- *   Portions Copyright (C) 2010 by David S. Register                      *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
- ***************************************************************************
- */
-
 /////////////////////////////////////////////////////////////////////////////
 // Author:      Robert Roebling
 // Copyright:   (c) 1998 Robert Roebling
+// Copyright:   (c) 2010 David S Register
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
+
+/**
+ * \file
+ *
+ * OpenCPN region handling
+ */
 
 #ifndef _OCPN_REGION_H_
 #define _OCPN_REGION_H_
@@ -35,11 +18,9 @@
 
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
-#endif  // precompiled headers
+#endif
 
-// #if defined(__WXOSX__)
 #define USE_NEW_REGION
-// #endif
 
 /**
  * A wrapper class for wxRegion with additional functionality.
