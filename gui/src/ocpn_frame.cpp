fac714f6d14a9b10e444e5dda87702b8836ad158
Alec Leamas <leamas.alec@gmail.com>
2025-09-18 08:35:27 +0200

    filenames: S57QueryDialog -> s57_query_dlg (Google GL)D

--- gui/src/ocpn_frame.cpp
+++ gui/src/ocpn_frame.cpp
@@ -139,7 +139,7 @@
 #include "route_prop_dlg_impl.h"
 #include "s52plib.h"
 #include "s57chart.h"
-#include "S57QueryDialog.h"
+#include "s57_query_dlg.h"
 #include "SoundFactory.h"
 #include "SystemCmdSound.h"
 #include "tcmgr.h"
