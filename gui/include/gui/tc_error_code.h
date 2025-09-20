e597d1eaa133e6745a6fd6e25447a4348701e610
Alec Leamas <leamas.alec@gmail.com>
2025-09-20 11:15:26 +0200

    tc_error_code: file clean up checklist

--- gui/include/gui/tc_error_code.h
+++ gui/include/gui/tc_error_code.h
@@ -1,8 +1,4 @@
-/******************************************************************************
- *
- * Project:  OpenCPN
- *
- ***************************************************************************
+/**************************************************************************
  *   Copyright (C) 2013 by David S. Register                               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -16,14 +12,11 @@
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
- ***************************************************************************
- */
+ *   along with this program; if not, see <https://www.gnu.org/licenses/>. *
+ **************************************************************************/
 
-#ifndef __TC_ERROR_CODE_H__
-#define __TC_ERROR_CODE_H__
+#ifndef TC_ERROR_CODE_H_
+#define TC_ERROR_CODE_H_
 
 typedef enum {
   TC_NO_ERROR,
