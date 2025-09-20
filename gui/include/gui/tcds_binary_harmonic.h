0788c32c57bcc8e7d6a6b82c2ee813b4b9f1cc1c
Alec Leamas <leamas.alec@gmail.com>
2025-09-20 11:04:46 +0200

    tcds_binary_harmonic: file clean up checklist

--- gui/include/gui/tcds_binary_harmonic.h
+++ gui/include/gui/tcds_binary_harmonic.h
@@ -1,8 +1,4 @@
 /***************************************************************************
- *
- * Project:  OpenCPN
- *
- ***************************************************************************
  *   Copyright (C) 2013 by David S. Register                               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -16,13 +12,17 @@
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ *   along with this program; if not, see <https://www.gnu.org/licenses/>. *
  **************************************************************************/
 
-#ifndef __TCDS_BINARY_HARMONIC_H__
-#define __TCDS_BINARY_HARMONIC_H__
+/**
+ * \file
+ *
+ * TCDataFactory loading data from binary file
+ */
+
+#ifndef TCDS_BINARY_HARMONIC_H_
+#define TCDS_BINARY_HARMONIC_H_
 
 #include <wx/string.h>
 
@@ -37,7 +37,7 @@ public:
 
   TC_Error_Code LoadData(const wxString &data_file_path);
 
-  int GetMaxIndex(void) { return num_IDX; };
+  int GetMaxIndex() { return num_IDX; };
   IDX_entry *GetIndexEntry(int n_index);
   TC_Error_Code LoadHarmonicData(IDX_entry *pIDX);
 
