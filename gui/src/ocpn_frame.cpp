a8e5285105d321b397e30e56e0d63266ac741088
Alec Leamas <leamas.alec@gmail.com>
2025-12-26 17:40:23 +0100

    routeman et al: Move on_routes_update to GuiEvents
    Having most  gui event variables in a common place makes
    things easier to navigate and avoids checks for non-existing
    evnt var owner.

--- gui/src/ocpn_frame.cpp
+++ gui/src/ocpn_frame.cpp
@@ -707,7 +707,7 @@ MyFrame::MyFrame(wxFrame *frame, const wxString &title, const wxPoint &pos,
   m_recaptureTimer.SetOwner(this, RECAPTURE_TIMER);
   m_tick_idx = 0;
   assert(g_pRouteMan != 0 && "g_pRouteMan not available");
-  m_routes_update_listener.Init(g_pRouteMan->on_routes_update,
+  m_routes_update_listener.Init(GuiEvents::GetInstance().on_routes_update,
                                 [&](wxCommandEvent) { Refresh(); });
   m_evt_drv_msg_listener.Init(CommDriverRegistry::GetInstance().evt_driver_msg,
                               [&](ObservedEvt &ev) { OnDriverMsg(ev); });
