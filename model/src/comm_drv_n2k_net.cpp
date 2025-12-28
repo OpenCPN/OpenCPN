a7357a7fcf8daef668af58577fb0f1b8f28d0a66
Alec Leamas <leamas.alec@gmail.com>
2025-12-28 19:27:46 +0100

    drivers: n2k: Don't update obsoleted all channel - #4990

--- model/src/comm_drv_n2k_net.cpp
+++ model/src/comm_drv_n2k_net.cpp
@@ -1919,15 +1919,10 @@ bool CommDriverN2KNet::SendN2KNetwork(std::shared_ptr<const Nmea2000Msg>& msg,
   SendSentenceNetwork(out_data);
   m_driver_stats.tx_count += msg->payload.size();
 
-  // Create the internal message for all N2K listeners
+  // Create internal message and notify upper layers
   std::vector<unsigned char> msg_payload = PrepareLogPayload(msg, addr);
-  auto msg_one =
-      std::make_shared<const Nmea2000Msg>(msg->PGN.pgn, msg_payload, addr);
-  auto msg_all = std::make_shared<const Nmea2000Msg>(1, msg_payload, addr);
-
-  // Notify listeners
-  m_listener.Notify(std::move(msg_one));
-  m_listener.Notify(std::move(msg_all));
+  m_listener.Notify(
+      std::make_shared<const Nmea2000Msg>(msg->PGN.pgn, msg_payload, addr));
 
   return true;
 };
