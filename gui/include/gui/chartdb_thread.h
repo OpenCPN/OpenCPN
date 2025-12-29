74a83d91430ed19b8bd9868ead5dd0f54c09d8b0
Dave <bdbcat@yahoo.com>
2025-12-28 20:57:22 -0500

    Implement threaded chart database creation

---
+++ gui/include/gui/chartdb_thread.h
@@ -0,0 +1,34 @@
+/**************************************************************************
+ *   Copyright (C) 2026 by David S. Register                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <https://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+/**
+ * \file
+ *
+ * Define threaded chart database classes
+ */
+
+#ifndef __CHARTDBTHREAD_H__
+#define __CHARTDBTHREAD_H__
+
+#include <wx/xml/xml.h>
+
+#include "chartbase.h"
+#include "chartbase.h"
+#include "chartdbs.h"
+#include "chartimg.h"
+
+#endif
---
+++ gui/src/chartdb_thread.cpp
@@ -0,0 +1,52 @@
+/**************************************************************************
+ *   Copyright (C) 2026 by David S. Register                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <https://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+/**
+ * \file
+ *
+ * Implement threaded chart database creation
+ */
+
+#include <stdint.h>
+
+#include <wx/wxprec.h>
+#include <wx/tokenzr.h>
+#include <wx/filename.h>
+#include <wx/wx.h>
+
+#include "config.h"
+
+#include "mipmap/mipmap.h"
+#include "model/base_platform.h"
+#include "model/config_vars.h"
+#include "model/gui_vars.h"
+
+#include "chartbase.h"
+#include "chartdb.h"
+#include "chartimg.h"
+#include "chcanv.h"
+#include "dychart.h"
+#include "gl_chart_canvas.h"
+#include "gl_tex_cache.h"
+#include "gl_texture_descr.h"
+#include "lz4.h"
+#include "lz4hc.h"
+#include "ocpn_platform.h"
+#include "quilt.h"
+#include "squish.h"
+#include "viewport.h"
+#include "chartdb_thread.h"
