8e460178bde4f4d9d0f9b3d5e3b7fb3aeab5ae4c
Dave <bdbcat@yahoo.com>
2026-01-28 22:05:31 -0500

    Honor config file "ToolbarY=n", #5043

--- gui/src/toolbar.cpp
+++ gui/src/toolbar.cpp
@@ -182,7 +182,7 @@ ocpnFloatingToolbarDialog::ocpnFloatingToolbarDialog(wxWindow *parent,
   m_cs = (ColorScheme)-1;
 
   m_style = g_StyleManager->GetCurrentStyle();
-  SetULDockPosition(wxPoint(4, 4));
+  SetULDockPosition(wxPoint(4, g_maintoolbar_y));
 
   SetGeometry(false, wxRect());
 
