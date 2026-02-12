e6e47a071cdda903a7cf1bb53fea1d6d2ec551d8
Alec Leamas <leamas.alec@gmail.com>
2026-02-12 16:17:20 +0100

    tty_scroll: Fix empty lines rendering - #5070 item 1

--- gui/src/tty_scroll.cpp
+++ gui/src/tty_scroll.cpp
@@ -123,7 +123,9 @@ wxColor UserColorsByState::operator()(NavmsgStatus ns) {
 void TtyScroll::DrawLine(wxDC& dc, const Logline& ll, int data_pos, int y) {
   wxString ws;
   if (!ll.message.empty()) ws << Timestamp(ll.navmsg->created_at) << " ";
-  if (ll.state.direction == NavmsgStatus::Direction::kOutput)
+  if (ll.message.empty())
+    ;
+  else if (ll.state.direction == NavmsgStatus::Direction::kOutput)
     ws << " " << kUtfRightArrow << " ";
   else if (ll.state.direction == NavmsgStatus::Direction::kInput)
     ws << " " << kUtfLeftwardsArrowToBar << " ";
@@ -132,7 +134,9 @@ void TtyScroll::DrawLine(wxDC& dc, const Logline& ll, int data_pos, int y) {
   else
     ws << " " << kUtfLeftArrow << " ";
 
-  if (ll.state.status != NavmsgStatus::State::kOk)
+  if (ll.message.empty())
+    ;
+  else if (ll.state.status != NavmsgStatus::State::kOk)
     ws << kUtfMultiplicationX;
   else if (ll.state.accepted == NavmsgStatus::Accepted::kFilteredNoOutput)
     ws << kUtfFallingDiagonal;
