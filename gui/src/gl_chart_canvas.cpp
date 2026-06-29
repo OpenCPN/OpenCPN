6f7fa800f0599a77b81242cb94ad41d50b2ac2f8
Dave <bdbcat@yahoo.com>
2026-06-28 20:40:57 -0400

    Enable VBO on Android, to support ANGLE/Vulcan graphics stack.

--- gui/src/gl_chart_canvas.cpp
+++ gui/src/gl_chart_canvas.cpp
@@ -1090,10 +1090,6 @@ void glChartCanvas::SetupOpenGL() {
   // VBO??
   g_b_EnableVBO = true;
 
-#ifdef __ANDROID__
-  g_b_EnableVBO = false;
-#endif
-
   if (g_b_EnableVBO)
     wxLogMessage("OpenGL-> Using Vertexbuffer Objects");
   else
