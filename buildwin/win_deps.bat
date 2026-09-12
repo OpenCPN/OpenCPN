5242c47470c569dbe113828a9cac13a1bbc803f1
transmitterdan <10525699+transmitterdan@users.noreply.github.com>
2026-09-12 03:33:31 -0500

    Update runtime Windows DLLs to latest version from host build tools
    Overwrites the MS runtime DLLs found in https://github.com/OpenCPN/OCPNWindowsCoreBuildSupport using latest versions found in host build environment

--- buildwin/win_deps.bat
+++ buildwin/win_deps.bat
@@ -68,6 +68,24 @@ if not exist %CACHE_DIR%\buildwin\libcurl.dll (
    -O %CACHE_DIR%\buildwin\iphlpapi.lib
 )
 
+:: For VS2022 builds we need to use the V143 runtime, which is in a different location than the V140 runtime used by VS2017 builds.
+:: The following code finds the correct location of the V143 runtime and copies it to the buildwin directory. It overwrites any
+:: existing files in the buildwin directory, which is fine since we want to ensure we have the correct version of the runtime dlls.
+
+for /F "delims=" %%A in ('dir /S /B "%VSINSTALLDIR%\api-ms-win-core-console-l1-1-0.dll" 2^>nul ^| findstr /R "Common7\\IDE\\Remote Debugger\\x86"') do (
+    set "DBGRUNTIME_DIR=%%~dpA"
+)
+echo %DBGRUNTIME_DIR%
+
+copy "%DBGRUNTIME_DIR%\api-ms-win*.dll" !CACHE_DIR!\buildwintemp\OCPNWindowsCoreBuildSupport-0.5\buildwin\vc /Y
+
+for /F "delims=" %%A in ('dir /S /B "%VSINSTALLDIR%\vccorlib140.dll" 2^>nul ^| findstr /R "onecore\\x86\\Microsoft.VC143.CRT"') do (
+    set "MSVCRUNTIME_DIR=%%~dpA"
+)
+echo %MSVCRUNTIME_DIR%
+
+copy "%MSVCRUNTIME_DIR%\*.dll" !CACHE_DIR!\buildwintemp\OCPNWindowsCoreBuildSupport-0.5\buildwin\vc /Y
+
 wget -nv -O !CACHE_DIR!\QuickStartGuide.zip ^
        https://dl.cloudsmith.io/public/david-register/opencpn-docs/raw/files/QuickStartGuide-v0.4.zip
 if not exist %CACHE_DIR%\..\data\doc\local (mkdir %CACHE_DIR%\..\data\doc\local)
