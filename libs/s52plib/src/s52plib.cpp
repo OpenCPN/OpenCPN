bb5e80c44a1e44f44acf5fc4c4b6a86706c07e5d
Dave <bdbcat@yahoo.com>
2026-09-13 16:08:27 -0400

    Correct FTBS

--- libs/s52plib/src/s52plib.cpp
+++ libs/s52plib/src/s52plib.cpp
@@ -5734,7 +5734,6 @@ int s52plib::RenderLC(ObjRazRules *rzRules, Rules *rules) {
       }
 
       ls = ls->next;
-      ils++;
     }
 
     free(ptp);
@@ -6538,7 +6537,7 @@ void s52plib::draw_lc_poly(wxDC *pdc, wxColor &color, int width, wxPoint *ptp,
 
   wxPoint2DDouble r;
 
-  cw = false;   // Winding is always CCW
+  bool cw = false;   // Winding is always CCW
 
   //    Get a true pixel clipping/bounding box from the vp
   wxPoint pbb = GetPixFromLL(vp_plib.clat, vp_plib.clon);
