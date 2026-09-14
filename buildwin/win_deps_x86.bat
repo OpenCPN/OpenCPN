d58ac017b9af103852a401a5346dcc3cea23ddf3
Dave <bdbcat@yahoo.com>
2026-09-14 15:36:36 -0400

    Implement GA build of windows-x86 model #4

--- buildwin/win_deps_x86.bat
+++ buildwin/win_deps_x86.bat
@@ -22,9 +22,11 @@ wget --version >nul 2>&1 || choco install wget -y --no-progress
 
 git clone https://github.com/microsoft/vcpkg N:\code\vcpkg
 set "VCPKG_DISABLE_METRICS=1"
-N:\code\vcpkg\bootstrap-vcpkg.bat -disableMetrics
+echo on
+call N:\code\vcpkg\bootstrap-vcpkg.bat -disableMetrics
+echo off
 echo After Bootstrap
-N:\code\vcpkg\vcpkg install openssl:x64-windows curl:x64-windows libarchive:x64-windows glew:x64-windows --debug
+call N:\code\vcpkg\vcpkg install openssl:x64-windows curl:x64-windows libarchive:x64-windows glew:x64-windows --debug
 echo After vcpkg intall...
 
 :: If needed, download wxWidgets binary build.
