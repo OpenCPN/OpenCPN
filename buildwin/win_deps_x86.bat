f66fa2ae3e25d8c9618360c2fec6e8166bfb24ab
Dave <bdbcat@yahoo.com>
2026-09-14 09:54:34 -0400

    Implement GA build of windows-x86 model

---
+++ buildwin/win_deps_x86.bat
@@ -0,0 +1,78 @@
+:: Download OpenCPN prebuilt dependencies
+::
+@echo on
+setlocal enabledelayedexpansion
+
+:: Install Poedit if required
+msgmerge --version >nul 2>&1
+if errorlevel 1 (
+  choco install -y poedit
+  set "PATH=%PATH%;C:\Program Files (x86)\Poedit\Gettexttools\bin"
+)
+
+:: Install git if required.
+git --version >nul 2>&1
+if errorlevel 1 (
+  choco install -y git
+  set "PATH=%PATH%;C:\Program Files\Git\bin"
+)
+
+:: install wget as required
+wget --version >nul 2>&1 || choco install -y wget
+
+git clone https://github.com/microsoft/vcpkg N:\code\vcpkg
+N:\code\vcpkg\bootstrap-vcpkg.bat
+N:\code\vcpkg\vcpkg install openssl:x64-windows curl:x64-windows libarchive:x64-windows glew:x64-windows
+
+:: If needed, download wxWidgets binary build.
+set "CACHE_DIR=%~dp0..\cache"
+if not exist !CACHE_DIR! (mkdir !CACHE_DIR!)
+set "GITHUB_DL=https://github.com/wxWidgets/wxWidgets/releases/download"
+if not exist cache\wxWidgets-3.2.9 (
+::  wget -nv %GITHUB_DL%/v3.2.1/wxMSW-3.2.1_vc14x_Dev.7z
+::  7z x -y -o%CACHE_DIR%\wxWidgets-3.2.1 wxMSW-3.2.1_vc14x_Dev.7z
+::  wget -nv %GITHUB_DL%/v3.2.1/wxWidgets-3.2.1-headers.7z
+::  7z x -y -o%CACHE_DIR%\wxWidgets-3.2.1 wxWidgets-3.2.1-headers.7z
+::  wget -nv %GITHUB_DL%/v3.2.1/wxMSW-3.2.1_vc14x_ReleaseDLL.7z
+::  7z x -y -o%CACHE_DIR%\wxWidgets-3.2.1 wxMSW-3.2.1_vc14x_ReleaseDLL.7z
+  wget -nv %GITHUB_DL%/v3.2.9/wxMSW-3.2.9_vc14x_Dev.7z
+  7z x -y -o%CACHE_DIR%\wxWidgets-3.2.9 wxMSW-3.2.9_vc14x_Dev.7z
+  wget -nv %GITHUB_DL%/v3.2.9/wxWidgets-3.2.9-headers.7z
+  7z x -y -o%CACHE_DIR%\wxWidgets-3.2.9 wxWidgets-3.2.9-headers.7z
+  wget -nv %GITHUB_DL%/v3.2.9/wxMSW-3.2.9_vc14x_ReleaseDLL.7z
+  7z x -y -o%CACHE_DIR%\wxWidgets-3.2.9 wxMSW-3.2.9_vc14x_ReleaseDLL.7z
+)
+:: Create cache\wx-config.bat, paths to downloaded wxWidgets.
+set "WXWIN=!CACHE_DIR!\wxWidgets-3.2.9"
+echo set "wxWidgets_ROOT_DIR=%WXWIN%" > %CACHE_DIR%\wx-config.bat
+echo set "wxWidgets_LIB_DIR=%WXWIN%\lib\vc14x_dll" >> %CACHE_DIR%\wx-config.bat
+
+
+if not exist C:\ProgramData\chocolatey\lib\nsis (
+  echo Installing nsis tools using choco
+  choco install -y nsis
+)
+
+:: Make sure the pre-compiled libraries are in place
+set "GH_DL_BASE=https://github.com/OpenCPN/OCPNWindowsCoreBuildSupport"
+set "opencpn_support_base=https://dl.cloudsmith.io/public/alec-leamas"
+if not exist %CACHE_DIR%\buildwin\libcurl.dll (
+  wget -nv -O !CACHE_DIR!\OCPNWindowsCoreBuildSupport.zip ^
+      %GH_DL_BASE%/archive/refs/tags/v0.5.zip
+  7z x -y !CACHE_DIR!\OCPNWindowsCoreBuildSupport.zip ^
+      -o%CACHE_DIR%\buildwintemp
+  if not exist !CACHE_DIR!\buildwin (mkdir !CACHE_DIR!\buildwin)
+  xcopy ^
+    !CACHE_DIR!\buildwintemp\OCPNWindowsCoreBuildSupport-0.5\buildwin ^
+    !CACHE_DIR!\buildwin /s /y /q
+  if exist !CACHE_DIR!\buildwin\wxWidgets (
+    del !CACHE_DIR!\buildwin\wxWidgets\*.dll /q
+  )
+  wget !opencpn_support_base!/opencpn-support/raw/files/iphlpapi.lib ^
+   -O %CACHE_DIR%\buildwin\iphlpapi.lib
+)
+
+wget -nv -O !CACHE_DIR!\QuickStartGuide.zip ^
+       https://dl.cloudsmith.io/public/david-register/opencpn-docs/raw/files/QuickStartGuide-v0.4.zip
+if not exist %CACHE_DIR%\..\data\doc\local (mkdir %CACHE_DIR%\..\data\doc\local)
+7z x -y !CACHE_DIR!\QuickStartGuide.zip  -o%CACHE_DIR%\..\data\doc\local
---
+++ ci/windows-x86.bat
@@ -0,0 +1,78 @@
+:: Build script for appveyor, https://www.appveyor.com
+:: Builds one version linked against wxWidgets 3.2
+
+@echo off
+setlocal enabledelayedexpansion
+
+set "SCRIPTDIR=%~dp0"
+
+:: %CONFIGURATION% comes from environment, set a default if invoked elsewise.
+if "%CONFIGURATION%" == "" set "CONFIGURATION=Release"
+
+:: On GA, set up N:, see below
+if exist D:\a\OpenCPN\OpenCPN (subst N: D:\a\OpenCPN\OpenCPN)
+
+:: If N: exists it is used as base dir to sanitize source file paths
+:: in pdb.
+if exist N:\ (
+  n:
+  cd \
+  echo "Using virtual drive N: as base path"
+)
+
+if not defined VCINSTALLDIR (
+  for /f "tokens=* USEBACKQ" %%p in (
+    `"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere" ^
+    -latest -property installationPath`
+  ) do call "%%p\Common7\Tools\vsDevCmd.bat"
+)
+
+call %SCRIPTDIR%..\buildwin\win_deps_x86.bat wx32
+call %SCRIPTDIR%..\cache\wx-config.bat
+echo USING wxWidgets_LIB_DIR: !wxWidgets_LIB_DIR!
+echo USING wxWidgets_ROOT_DIR: !wxWidgets_ROOT_DIR!
+
+where wxmsw32u_qa_vc14x.dll >nul 2>&1
+if errorlevel 1 (
+  set "PATH=%PATH%;%wxWidgets_LIB_DIR%"
+  echo Appending %wxWidgets_LIB_DIR% to PATH
+)
+
+
+if exist build (rmdir /s /q build)
+mkdir build && cd build
+
+:: This id done in win_deps.bat, but for some reason stopped
+:: working since 2024-02-28, so work it around here
+set "PATH=%PATH%;%PROGRAMFILES%\Poedit\Gettexttools\bin"
+
+cmake -A Win32 -G "Visual Studio 17 2022" ^
+    -DCMAKE_GENERATOR_PLATFORM=x86 ^
+    -DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
+    -DwxWidgets_LIB_DIR=!wxWidgets_LIB_DIR! ^
+    -DwxWidgets_ROOT_DIR=!wxWidgets_ROOT_DIR! ^
+    -DwxWidgets_CONFIGURATION=mswu ^
+    -DOCPN_TARGET_TUPLE=msvc-wx32;10;x86_64 ^
+    -DOCPN_CI_BUILD=ON ^
+    -DOCPN_BUNDLE_WXDLLS=ON ^
+    -DOCPN_RELEASE=0 ^
+    -DCMAKE_INSTALL_PREFIX="%cd%/test/%CONFIGURATION%" ^
+    -DOCPN_BUILD_TEST=ON ^
+
+    ..
+
+cmake --build . --target package --config %CONFIGURATION%
+
+type N:\build\_CPack_Packages\win32\NSIS\NSISOutput.log
+
+:: Compress pdb and mark with git hash
+@echo on
+"C:\Program Files\Git\bin\bash" -c ^
+  "tar czf opencpn+%GITHUB_SHA:~0,8%.pdb.tar.gz %CONFIGURATION%/opencpn.pdb"
+@echo off
+
+:: Display dependencies debug info
+echo import glob; import subprocess > ldd.py
+echo lib = glob.glob("%CONFIGURATION%/opencpn.exe")[0] >> ldd.py
+echo subprocess.call(['dumpbin', '/dependents', lib], shell=True) >> ldd.py
+python ldd.py
