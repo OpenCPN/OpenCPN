499bcb436c04b16207bb4101fdb656c4f841b19c
Dave <bdbcat@yahoo.com>
2026-09-16 23:56:00 -0400

    Implement GA build of windows-x86 model #27

--- ci/windows-x86.bat
+++ ci/windows-x86.bat
@@ -75,8 +75,7 @@ cmake -A x64 -G "Visual Studio 17 2022" ^
     -DOCPN_BUNDLE_WXDLLS=ON ^
     -DOCPN_RELEASE=0 ^
     -DCMAKE_INSTALL_PREFIX="%cd%/test/%CONFIGURATION%" ^
-    -DOCPN_BUILD_TEST=ON ^
-
+    -DOCPN_BUILD_TEST=OFF ^
     ..
 
 cmake --build . --target package --config %CONFIGURATION%
