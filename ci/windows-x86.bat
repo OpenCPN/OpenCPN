c41ce0c9687c3979ce5813856be83a2b1129f573
Dave <bdbcat@yahoo.com>
2026-09-18 14:59:39 -0400

    Implement GA build of windows-x86 model #43

--- ci/windows-x86.bat
+++ ci/windows-x86.bat
@@ -79,10 +79,12 @@ cmake -A x64 -G "Visual Studio 17 2022" ^
     ..
 
 cmake --build . --target package --config %CONFIGURATION%
+
+dir D:\a\OpenCPN\OpenCPN\build\_CPack_Packages\win64\NSIS\opencpn_5.14.0-0+beb4502_setup_x64.exe
+
 echo Build complete.
 
-::type N:\build\_CPack_Packages\win32\NSIS\NSISOutput.log
-type D:\a\OpenCPN\OpenCPN\build\_CPack_Packages\win64\NSIS\project.nsi
+::type D:\a\OpenCPN\OpenCPN\build\_CPack_Packages\win64\NSIS\project.nsi
 type D:\a\OpenCPN\OpenCPN\build\_CPack_Packages\win64\NSIS\NSISOutput.log
 
 :: Compress pdb and mark with git hash
